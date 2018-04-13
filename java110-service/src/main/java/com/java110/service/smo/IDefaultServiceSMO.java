3de85d01308c5efa0bc77d4a02fdc9a16d5aa9fb
wuxw7 <wuxw7@asiainfo.com>
2018-04-13 13:09:56 +0800

    2018-04-13 2018基础版

---
+++ java110-service/src/main/java/com/java110/service/smo/IDefaultServiceSMO.java
@@ -0,0 +1,19 @@
+package com.java110.service.smo;
+
+/**
+ *
+ * 默认服务业务处理类
+ *
+ * table
+ *
+ * colmuns
+ *
+ * table_attr
+ *
+ * service 表
+ *
+ *
+ * Created by wuxw on 2017/9/30.
+ */
+public interface IDefaultServiceSMO {
+}
