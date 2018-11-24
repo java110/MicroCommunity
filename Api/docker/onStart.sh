d7717df0f80aac77eec9ba0345ea7f4487143f75
wuxw7 <wuxw7@asiainfo.com>
2018-11-25 01:05:24 +0800

    修改域名bug

--- Api/docker/onStart.sh
+++ Api/docker/onStart.sh
@@ -6,6 +6,6 @@ cp  -r ../target .
 
 docker build -t java110/api .
 
-docker run -ti --name api_test -p8001:8001 -idt java110/api:latest
+docker run -ti --name api_test -p8008:8008 -idt java110/api:latest
 
 docker logs -f api_test
