5b77063ee1cd33b7b758174d2c8190c025d9e026
wuxw7 <wuxw7@asiainfo.com>
2018-11-28 23:11:39 +0800

    加入docker-componse

--- reBuildAll.sh
+++ reBuildAll.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
-mvn clean install;
-mvn clean package;
-docker-compose -f /data/qhcrm/app-assist/docker-compose.yml       up -d --force-recreate;
+
+git pull origin master
+
+mvn clean install
+mvn package;
+
+docker-compose -f ./app/Api/docker/docker-compose.yml       up -d --force-recreate;
+docker-compose -f ./app/CenterService/docker/docker-compose.yml       up -d --force-recreate;
+docker-compose -f ./app/ShopService/docker/docker-compose.yml       up -d --force-recreate;
+docker-compose -f ./app/StoreService/docker/docker-compose.yml       up -d --force-recreate;
+docker-compose -f ./app/UserService/docker/docker-compose.yml       up -d --force-recreate;
+
