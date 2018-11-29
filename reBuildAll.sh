1f1b1160f5bdb19a8febd9b77286fe48d7d72e9d
wuxw7 <wuxw7@asiainfo.com>
2018-11-29 14:00:56 +0800

    修改启动脚本

--- reBuildAll.sh
+++ reBuildAll.sh
@@ -5,9 +5,9 @@ git pull origin master
 mvn clean install -Dmaven.test.skip=true
 mvn package -Dmaven.test.skip=true
 
-docker-compose -f ./Api/docker/docker-compose.yml       up -d --force-recreate;
-docker-compose -f ./CenterService/docker/docker-compose.yml       up -d --force-recreate;
-docker-compose -f ./ShopService/docker/docker-compose.yml       up -d --force-recreate;
-docker-compose -f ./StoreService/docker/docker-compose.yml       up -d --force-recreate;
-docker-compose -f ./UserService/docker/docker-compose.yml       up -d --force-recreate;
+docker-compose -f ./Api/docker/docker-compose.yml       up -d --build --force-recreate;
+docker-compose -f ./CenterService/docker/docker-compose.yml       up -d --build --force-recreate;
+docker-compose -f ./ShopService/docker/docker-compose.yml       up -d --build --force-recreate;
+docker-compose -f ./StoreService/docker/docker-compose.yml       up -d --build --force-recreate;
+docker-compose -f ./UserService/docker/docker-compose.yml       up -d --build --force-recreate;
 
