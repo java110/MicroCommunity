2091449209227a1a653f2f42e5d5c80b50068cfe
吴学文 <wuxuewen@wuxuewendeMacBook-Pro.local>
2019-04-10 01:03:56 +0800

    员工权限开发

---
+++ WebService/src/main/resources/components/privilege-staffInfo/privilegeStaffInfo.js
@@ -0,0 +1,21 @@
+/**
+    权限组
+**/
+(function(vc){
+
+    vc.extends({
+        data:{
+
+        },
+        _initMethod:function(){
+
+        },
+        _initEvent:function(){
+
+        },
+        methods:{
+
+        }
+    });
+
+})(window.vc);
