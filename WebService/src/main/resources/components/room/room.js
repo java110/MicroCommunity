f5986a355fda8d5df98fe01127bcd7f490f7cdf5
wuxw <928255095@qq.com>
2019-04-19 17:24:55 +0800

    开始开发房屋管理

---
+++ WebService/src/main/resources/components/room/room.js
@@ -0,0 +1,46 @@
+/**
+    入驻小区
+**/
+(function(vc){
+    vc.extends({
+        data:{
+            roomInfo:{
+                roomDataInfo:[],
+            }
+        },
+        _initMethod:function(){
+            vc.component.listRoom();
+        },
+        _initEvent:function(){
+            vc.on('room','listRoom',function(_param){
+                  vc.component.listRoom();
+            });
+        },
+        methods:{
+            listRoom:function(){
+                var param = {
+                    params:{
+                        msg:this.message
+                    }
+
+               }
+               //发送get请求
+               vc.http.get('room',
+                            'listRoom',
+                             param,
+                             function(json,res){
+                                vc.component.roomInfo.roomDataInfo=JSON.parse(json);
+                             },function(errInfo,error){
+                                console.log('请求失败处理');
+                             }
+                           );
+            },
+            _openAddRoomModal:function(){
+                //vc.emit('storeEnterCommunity','openStoreEnterCommunity',{});
+            },
+            _openDelRoomModel:function(_community){
+                //vc.emit('storeExitCommunity','openStoreExitCommunityModal',_community);
+            }
+        }
+    });
+})(window.vc);
