ee62e0c82f29b934680a95044f25dafd212749a5
吴学文 <wuxuewen@wuxuewendeMacBook-Pro.local>
2019-04-27 01:24:12 +0800

    添加小区楼取消审核功能

--- Api/src/main/java/com/java110/api/listener/floor/SaveFloorListener.java
+++ Api/src/main/java/com/java110/api/listener/floor/SaveFloorListener.java
@@ -3,16 +3,15 @@ package com.java110.api.listener.floor;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.java110.api.listener.AbstractServiceApiDataFlowListener;
-import com.java110.common.constant.BusinessTypeConstant;
-import com.java110.common.constant.CommonConstant;
-import com.java110.common.constant.CommunityMemberTypeConstant;
-import com.java110.common.constant.ServiceCodeConstant;
+import com.java110.common.cache.MappingCache;
+import com.java110.common.constant.*;
 import com.java110.common.util.Assert;
 import com.java110.core.annotation.Java110Listener;
 import com.java110.core.context.DataFlowContext;
 import com.java110.core.factory.GenerateCodeFactory;
 import com.java110.entity.center.AppService;
 import com.java110.event.service.api.ServiceDataFlowEvent;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -142,6 +141,7 @@ public class SaveFloorListener extends AbstractServiceApiDataFlowListener {
         businessCommunityMember.put("communityId", paramInJson.getString("communityId"));
         businessCommunityMember.put("memberId", paramInJson.getString("floorId"));
         businessCommunityMember.put("memberTypeCd", CommunityMemberTypeConstant.FLOOR);
+        businessCommunityMember.put("auditStatusCd", StatusConstant.STATUS_CD_AUDIT_COMPLETE);
         business.getJSONObject(CommonConstant.HTTP_BUSINESS_DATAS).put("businessCommunityMember", businessCommunityMember);
 
         return business;
