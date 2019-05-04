c8419251e827cb024f6a1344f69d8d1157cc452f
吴学文 <wuxuewen@wuxuewendeMacBook-Pro.local>
2019-05-04 22:56:35 +0800

    加入房屋建模语句

--- java110-config/db/CommunityService/create_room.sql
+++ java110-config/db/CommunityService/create_room.sql
@@ -65,3 +65,46 @@ CREATE TABLE building_unit(
 CREATE INDEX idx_unit_b_id ON building_unit(b_id);
 CREATE UNIQUE INDEX idx_unit_id ON building_unit(unit_id);
 
+
+
+-- 单元信息 building 楼宇管理
+CREATE TABLE business_building_room(
+  room_id VARCHAR(30) NOT NULL COMMENT '房屋ID',
+  b_id VARCHAR(30) NOT NULL COMMENT '业务Id',
+  room_num VARCHAR(12) NOT NULL COMMENT '房屋编号',
+  unit_id VARCHAR(30) NOT NULL COMMENT '单元ID',
+  layer int NOT NULL COMMENT '层数',
+  section varchar(4) NOT NULL COMMENT '室',
+  apartment varchar(4) NOT NULL COMMENT '户型',
+  built_up_area varchar(4) NOT NULL COMMENT '建筑面积',
+  unit_price varchar(4) NOT NULL COMMENT '每平米单价',
+  user_id VARCHAR(30) NOT NULL COMMENT '用户ID',
+  remark VARCHAR(200) NOT NULL COMMENT '备注',
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  operate VARCHAR(4) NOT NULL COMMENT '数据状态，添加ADD，修改MOD 删除DEL'
+);
+
+
+CREATE INDEX idx_business_room_id ON business_building_room(room_id);
+CREATE INDEX idx_business_room_b_id ON business_building_room(b_id);
+
+
+CREATE TABLE building_room(
+  room_id VARCHAR(30) NOT NULL COMMENT '房屋ID',
+  b_id VARCHAR(30) NOT NULL COMMENT '业务Id',
+  room_num VARCHAR(12) NOT NULL COMMENT '房屋编号',
+  unit_id VARCHAR(30) NOT NULL COMMENT '单元ID',
+  layer int NOT NULL COMMENT '层数',
+  section varchar(4) NOT NULL COMMENT '室',
+  apartment varchar(4) NOT NULL COMMENT '户型',
+  built_up_area varchar(4) NOT NULL COMMENT '建筑面积',
+  unit_price varchar(4) NOT NULL COMMENT '每平米单价',
+  user_id VARCHAR(30) NOT NULL COMMENT '用户ID',
+  remark VARCHAR(200) NOT NULL COMMENT '备注',
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  status_cd VARCHAR(2) NOT NULL DEFAULT '0' COMMENT '数据状态，详细参考c_status表，S 保存，0, 在用 1失效',
+  UNIQUE KEY (room_id)
+);
+CREATE INDEX idx_room_b_id ON building_room(b_id);
+CREATE UNIQUE INDEX idx_room_id ON building_room(room_id);
+
