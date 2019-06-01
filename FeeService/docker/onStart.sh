da90c84a73396585cfbf1a59fd6a16c908162c34
wuxw <928255095@qq.com>
2019-06-01 12:20:40 +0800

    修改端口冲突 bug

--- FeeService/docker/onStart.sh
+++ FeeService/docker/onStart.sh
@@ -6,6 +6,6 @@ cp  -r ../target .
 
 docker build -t java110/fee .
 
-docker run -ti --name fee_test -p8006:8006 -idt java110/fee:latest
+docker run -ti --name fee_test -p8010:8010 -idt java110/fee:latest
 
 docker logs -f fee_test
