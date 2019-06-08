97b9b43748e2a349651d3fa91870233b939339aa
吴学文 <wuxuewen@wuxuewendeMacBook-Pro.local>
2019-06-08 23:23:38 +0800

    修复业主车辆管理 服务名错误处理

--- java110-core/src/main/java/com/java110/core/smo/owner/IOwnerCarInnerServiceSMO.java
+++ java110-core/src/main/java/com/java110/core/smo/owner/IOwnerCarInnerServiceSMO.java
@@ -17,7 +17,7 @@ import java.util.List;
  * @Version 1.0
  * add by wuxw 2019/4/24
  **/
-@FeignClient(name = "community-service", configuration = {FeignConfiguration.class})
+@FeignClient(name = "user-service", configuration = {FeignConfiguration.class})
 @RequestMapping("/ownerCarApi")
 public interface IOwnerCarInnerServiceSMO {
 
