0711c93acc5ed0cbd96199384028686f13b5194d
wuxw <928255095@qq.com>
2019-06-13 14:55:54 +0800

    首页开发中

---
+++ WebService/src/main/resources/components/index-context/indexContext.js
@@ -0,0 +1,58 @@
+(function(vc){
+    var DEFAULT_PAGE = 1;
+    var DEFAULT_ROWS = 10;
+    vc.extends({
+        data:{
+            indexContextInfo:{
+                owners:[],
+                total:0,
+                records:1,
+                errorInfo:""
+            }
+        },
+        _initMethod:function(){
+            //vc.component._listOwnerData(DEFAULT_PAGE,DEFAULT_ROWS);
+        },
+        _initEvent:function(){
+            vc.on('listOwner','listOwnerData',function(){
+                vc.component._listOwnerData(DEFAULT_PAGE,DEFAULT_ROWS);
+            });
+            vc.on('pagination','page_event',function(_currentPage){
+                vc.component._listOwnerData(_currentPage,DEFAULT_ROWS);
+            });
+        },
+        methods:{
+            _listOwnerData:function(_page,_row){
+                var param = {
+                    params:{
+                        page:_page,
+                        row:_row,
+                        communityId:vc.getCurrentCommunity().communityId,
+                        ownerTypeCd:'1001'
+                    }
+                }
+
+               //发送get请求
+               vc.http.get('listOwner',
+                            'list',
+                             param,
+                             function(json,res){
+                                var listOwnerData =JSON.parse(json);
+
+                                vc.component.listOwnerInfo.total = listOwnerData.total;
+                                vc.component.listOwnerInfo.records = listOwnerData.records;
+                                vc.component.listOwnerInfo.owners = listOwnerData.owners;
+
+                                vc.emit('pagination','init',{
+                                    total:vc.component.listOwnerInfo.records,
+                                    currentPage:_page
+                                });
+                             },function(errInfo,error){
+                                console.log('请求失败处理');
+                             }
+                           );
+
+            }
+        }
+    })
+})(window.vc);
