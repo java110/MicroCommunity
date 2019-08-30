8a459e1aa9bd957adb9c141d6c1a743caa5e5b5e
wuxw <928255095@qq.com>
2019-08-30 18:29:24 +0800

    加入菜单编辑和删除功能

--- Api/src/main/java/com/java110/api/listener/menu/UpdateMenuListener.java
+++ Api/src/main/java/com/java110/api/listener/menu/UpdateMenuListener.java
@@ -2,12 +2,21 @@ package com.java110.api.listener.menu;
 
 import com.alibaba.fastjson.JSONObject;
 import com.java110.api.listener.AbstractServiceApiListener;
+import com.java110.common.constant.ResponseConstant;
 import com.java110.common.constant.ServiceCodeMenuConstant;
+import com.java110.common.exception.ListenerExecuteException;
 import com.java110.common.util.Assert;
+import com.java110.common.util.BeanConvertUtil;
 import com.java110.core.annotation.Java110Listener;
 import com.java110.core.context.DataFlowContext;
+import com.java110.core.smo.menu.IMenuInnerServiceSMO;
+import com.java110.dto.app.AppDto;
+import com.java110.dto.menu.MenuDto;
 import com.java110.event.service.api.ServiceDataFlowEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 /**
  * 编辑菜单侦听
@@ -15,6 +24,9 @@ import org.springframework.http.HttpMethod;
  */
 @Java110Listener("updateMenuListener")
 public class UpdateMenuListener extends AbstractServiceApiListener {
+
+    @Autowired
+    private IMenuInnerServiceSMO menuInnerServiceSMOImpl;
     @Override
     protected void validate(ServiceDataFlowEvent event, JSONObject reqJson) {
 
@@ -29,6 +41,18 @@ public class UpdateMenuListener extends AbstractServiceApiListener {
     @Override
     protected void doSoService(ServiceDataFlowEvent event, DataFlowContext context, JSONObject reqJson) {
 
+        MenuDto menuDto = BeanConvertUtil.covertBean(reqJson, MenuDto.class);
+
+
+        int count = menuInnerServiceSMOImpl.updateMenu(menuDto);
+
+        if (count < 1) {
+            throw new ListenerExecuteException(ResponseConstant.RESULT_CODE_ERROR, "修改数据失败");
+        }
+
+        ResponseEntity<String> responseEntity = new ResponseEntity<String>("", HttpStatus.OK);
+
+        context.setResponseEntity(responseEntity);
 
     }
 
@@ -48,4 +72,11 @@ public class UpdateMenuListener extends AbstractServiceApiListener {
     }
 
 
+    public IMenuInnerServiceSMO getMenuInnerServiceSMOImpl() {
+        return menuInnerServiceSMOImpl;
+    }
+
+    public void setMenuInnerServiceSMOImpl(IMenuInnerServiceSMO menuInnerServiceSMOImpl) {
+        this.menuInnerServiceSMOImpl = menuInnerServiceSMOImpl;
+    }
 }
