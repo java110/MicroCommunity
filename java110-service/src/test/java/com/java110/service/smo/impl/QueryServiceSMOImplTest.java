7da179c0002df53fffe72221ac17cf87b3482708
wuxw <928255095@qq.com>
2019-09-06 18:37:28 +0800

    测试 javassist 成功

--- java110-service/src/test/java/com/java110/service/smo/impl/QueryServiceSMOImplTest.java
+++ java110-service/src/test/java/com/java110/service/smo/impl/QueryServiceSMOImplTest.java
@@ -17,9 +17,11 @@ public class QueryServiceSMOImplTest extends TestCase {
 
 
     public void testJava() throws CannotCompileException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        String javaCode = "public static void testJava2() {        System.out.println(\"123213\");\n}\n";
-                String    javaCode2 ="public static void testJava1() {     testJava2();   System.out.println(\"223213\");\n}";
+        String javaCode = "public static void testJava2() {       DataQuery dataQuery = new DataQuery();\n dataQuery.setServiceCode(\"服务编码\");  System.out.println(dataQuery.getServiceCode());\n}\n";
+                String    javaCode2 ="public static void testJava1() {     testJava2(); ServiceSql serviceSql = new ServiceSql();  System.out.println(\"623213\");\n}";
         ClassPool classPool = ClassPool.getDefault();
+        classPool.importPackage("com.java110.entity.service.DataQuery");
+        classPool.importPackage("com.java110.entity.service.ServiceSql");
         CtClass ctClass = classPool.makeClass("com.java110.service.smo.WuxwTest");
         CtMethod helloM = CtNewMethod.make(javaCode, ctClass);
         ctClass.addMethod(helloM);
@@ -32,6 +34,9 @@ public class QueryServiceSMOImplTest extends TestCase {
         Constructor<?> con=pc.getConstructor(new Class[]{});
 
         move.invoke(con);
+
+
+
     }
 
 
