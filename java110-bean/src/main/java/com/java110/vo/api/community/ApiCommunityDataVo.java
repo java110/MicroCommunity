fb91efe81dc6b0298b53d27fb2458acab090106d
wuxw <928255095@qq.com>
2019-09-17 17:26:35 +0800

    优化小区信息菜单信息

--- java110-bean/src/main/java/com/java110/vo/api/community/ApiCommunityDataVo.java
+++ java110-bean/src/main/java/com/java110/vo/api/community/ApiCommunityDataVo.java
@@ -12,49 +12,78 @@ public class ApiCommunityDataVo implements Serializable {
     private String cityCode;
     private String mapX;
     private String mapY;
+    private String state;
+    private String stateName;
+
     public String getCommunityId() {
         return communityId;
     }
+
     public void setCommunityId(String communityId) {
         this.communityId = communityId;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public String getNearbyLandmarks() {
         return nearbyLandmarks;
     }
+
     public void setNearbyLandmarks(String nearbyLandmarks) {
         this.nearbyLandmarks = nearbyLandmarks;
     }
+
     public String getCityCode() {
         return cityCode;
     }
+
     public void setCityCode(String cityCode) {
         this.cityCode = cityCode;
     }
+
     public String getMapX() {
         return mapX;
     }
+
     public void setMapX(String mapX) {
         this.mapX = mapX;
     }
+
     public String getMapY() {
         return mapY;
     }
+
     public void setMapY(String mapY) {
         this.mapY = mapY;
     }
 
+    public String getState() {
+        return state;
+    }
 
+    public void setState(String state) {
+        this.state = state;
+    }
 
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
 }
