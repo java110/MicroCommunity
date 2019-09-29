2d6ee3420f15e0fa75b1ea0c4e5bb2b00b61c7ee
吴学文 <wuxuewen@wuxuewendeMacBook-Pro.local>
2019-09-29 20:03:26 +0800

    加入公告详情页面

---
+++ WebService/src/main/resources/components/noticePackage/noticeDetail/noticeDetail.js
@@ -0,0 +1,60 @@
+/**
+    入驻小区
+**/
+(function(vc){
+    var DEFAULT_PAGE = 1;
+    var DEFAULT_ROWS = 1;
+    vc.extends({
+        data:{
+            noticeDetailInfo:{
+                noticeId:'',
+                title:'',
+                context:'',
+                startTime:'',
+                endTime:'',
+                createTime:''
+            }
+        },
+        _initMethod:function(){
+            var tmpNoticeId = vc.getParam('noticeId');
+            if(!vc.notNull(tmpNoticeId)){
+                return ;
+            }
+            vc.component.noticeDetailInfo.noticeId = tmpNoticeId;
+            vc.component._listNotices(DEFAULT_PAGE, DEFAULT_ROWS);
+        },
+        _initEvent:function(){
+
+        },
+        methods:{
+            _listNotices:function(_page, _rows){
+
+                var param = {
+                    params:{
+                        page:_page,
+                        row:_rows,
+                        communityId:vc.getCurrentCommunity().communityId,
+                        noticeId:vc.component.noticeDetailInfo.noticeId
+                    }
+               };
+
+               //发送get请求
+               vc.http.get('noticeDetail',
+                            'get',
+                             param,
+                             function(json,res){
+                                var _noticeDetailInfo=JSON.parse(json);
+
+                                var _notices = _noticeManageInfo.notices;
+                                if(_notices.length >0){
+                                    vc.copyObject(_notices[0], vc.component.noticeDetailInfo);
+                                }
+
+                             },function(errInfo,error){
+                                console.log('请求失败处理');
+                             }
+                           );
+            }
+        }
+    });
+})(window.vc);
