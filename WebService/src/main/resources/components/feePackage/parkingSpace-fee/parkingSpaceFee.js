092f5685989af4a556a7da144dc96776abbff453
wuxw <928255095@qq.com>
2019-09-29 16:57:06 +0800

    交费页面 日期组件优化

--- WebService/src/main/resources/components/feePackage/parkingSpace-fee/parkingSpaceFee.js
+++ WebService/src/main/resources/components/feePackage/parkingSpace-fee/parkingSpaceFee.js
@@ -32,18 +32,30 @@
         },
         methods:{
             initDate:function(){
-                $(".start_time").datetimepicker({format: 'yyyy-mm-dd', language: 'zh-CN',minView: "day"});
-                $(".end_time").datetimepicker({format: 'yyyy-mm-dd', language: 'zh-CN',minView: "hour"});
+                $('.start_time').datetimepicker({
+                    language: 'zh-CN',
+                    format: 'yyyy-mm-dd',
+                    autoClose: 1,
+                    minView: "month",
+                    todayBtn: true
+                });
                 $('.start_time').datetimepicker()
-                          .on('changeDate', function (ev) {
-                            var value = $(".start_time").val();
-                            vc.component.feeDetailInfo.startTime = value ;
-                          });
+                    .on('changeDate', function (ev) {
+                        var value = $(".start_time").val();
+                        vc.component.feeDetailInfo.startTime = value;
+                    });
+                $('.end_time').datetimepicker({
+                    language: 'zh-CN',
+                    format: 'yyyy-mm-dd',
+                    autoClose: 1,
+                    minView: "month",
+                    todayBtn: true
+                });
                 $('.end_time').datetimepicker()
-                              .on('changeDate', function (ev) {
-                                var value = $(".end_time").val();
-                                vc.component.feeDetailInfo.endTime = value ;
-                              });
+                  .on('changeDate', function (ev) {
+                    var value = $(".end_time").val();
+                    vc.component.feeDetailInfo.endTime = value ;
+                  });
             },
             listFeeDetail:function(_page,_row){
                 var param = {
