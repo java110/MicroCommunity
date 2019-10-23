eb14fbb25c1e3a36e82104c71043f6af88d4c5d5
wuxw <928255095@qq.com>
2019-10-23 20:17:37 +0800

    加入 侦听处理类

---
+++ CommonService/src/main/java/com/java110/common/activity/ResourceEntryListener.java
@@ -0,0 +1,11 @@
+package com.java110.common.activity;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+public class ResourceEntryListener implements TaskListener {
+    @Override
+    public void notify(DelegateTask delegateTask) {
+
+    }
+}
