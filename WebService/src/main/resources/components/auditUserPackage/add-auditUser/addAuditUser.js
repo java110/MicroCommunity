d924a1e9736f487b58bdfc3cdf1660275d10c674
wuxw <928255095@qq.com>
2019-10-26 10:20:17 +0800

    取消审核人员编辑功能

--- WebService/src/main/resources/components/auditUserPackage/add-auditUser/addAuditUser.js
+++ WebService/src/main/resources/components/auditUserPackage/add-auditUser/addAuditUser.js
@@ -1,100 +1,98 @@
-(function(vc){
+(function (vc) {
 
     vc.extends({
         propTypes: {
-               callBackListener:vc.propTypes.string, //父组件名称
-               callBackFunction:vc.propTypes.string //父组件监听方法
+            callBackListener: vc.propTypes.string, //父组件名称
+            callBackFunction: vc.propTypes.string //父组件监听方法
         },
-        data:{
-            addAuditUserInfo:{
-                auditUserId:'',
-                userId:'',
-userName:'',
-auditLink:'',
-objCode:'',
+        data: {
+            addAuditUserInfo: {
+                auditUserId: '',
+                userId: '',
+                userName: '',
+                auditLink: '',
+                objCode: '',
 
             }
         },
-         _initMethod:function(){
+        _initMethod: function () {
 
-         },
-         _initEvent:function(){
-            vc.on('addAuditUser','openAddAuditUserModal',function(){
+        },
+        _initEvent: function () {
+            vc.on('addAuditUser', 'openAddAuditUserModal', function () {
                 $('#addAuditUserModel').modal('show');
             });
         },
-        methods:{
-            addAuditUserValidate(){
+        methods: {
+            addAuditUserValidate() {
                 return vc.validate.validate({
-                    addAuditUserInfo:vc.component.addAuditUserInfo
-                },{
-                    'addAuditUserInfo.userId':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"用户ID不能为空"
+                    addAuditUserInfo: vc.component.addAuditUserInfo
+                }, {
+                    'addAuditUserInfo.userId': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "用户ID不能为空"
                         },
- {
-                            limit:"num",
-                            param:"",
-                            errInfo:"用户ID必须为数字"
-                        },
-                    ],
-'addAuditUserInfo.userName':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"用户名称不能为空"
-                        },
- {
-                            limit:"maxin",
-                            param:"2,50",
-                            errInfo:"用户名称必须在2至50字符之间"
+                        {
+                            limit: "num",
+                            param: "",
+                            errInfo: "用户ID必须为数字"
                         },
                     ],
-'addAuditUserInfo.auditLink':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"审核环节不能为空"
+                    'addAuditUserInfo.userName': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "用户名称不能为空"
                         },
- {
-                            limit:"num",
-                            param:"",
-                            errInfo:"审核环节格式错误"
-                        },
-                    ],
-'addAuditUserInfo.objCode':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"流程对象不能为空"
-                        },
- {
-                            limit:"maxLength",
-                            param:"64",
-                            errInfo:"物品库存不能大于64位"
+                        {
+                            limit: "maxin",
+                            param: "2,50",
+                            errInfo: "用户名称必须在2至50字符之间"
                         },
                     ],
-
-
+                    'addAuditUserInfo.auditLink': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "审核环节不能为空"
+                        },
+                        {
+                            limit: "num",
+                            param: "",
+                            errInfo: "审核环节格式错误"
+                        },
+                    ],
+                    'addAuditUserInfo.objCode': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "流程对象不能为空"
+                        },
+                        {
+                            limit: "maxLength",
+                            param: "64",
+                            errInfo: "物品库存不能大于64位"
+                        },
+                    ],
 
 
                 });
             },
-            saveAuditUserInfo:function(){
-                if(!vc.component.addAuditUserValidate()){
+            saveAuditUserInfo: function () {
+                if (!vc.component.addAuditUserValidate()) {
                     vc.message(vc.validate.errInfo);
 
-                    return ;
+                    return;
                 }
 
                 vc.component.addAuditUserInfo.communityId = vc.getCurrentCommunity().communityId;
                 //不提交数据将数据 回调给侦听处理
-                if(vc.notNull($props.callBackListener)){
-                    vc.emit($props.callBackListener,$props.callBackFunction,vc.component.addAuditUserInfo);
+                if (vc.notNull($props.callBackListener)) {
+                    vc.emit($props.callBackListener, $props.callBackFunction, vc.component.addAuditUserInfo);
                     $('#addAuditUserModel').modal('hide');
-                    return ;
+                    return;
                 }
 
                 vc.http.post(
@@ -102,36 +100,36 @@ objCode:'',
                     'save',
                     JSON.stringify(vc.component.addAuditUserInfo),
                     {
-                        emulateJSON:true
-                     },
-                     function(json,res){
+                        emulateJSON: true
+                    },
+                    function (json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
-                        if(res.status == 200){
+                        if (res.status == 200) {
                             //关闭model
                             $('#addAuditUserModel').modal('hide');
                             vc.component.clearAddAuditUserInfo();
-                            vc.emit('auditUserManage','listAuditUser',{});
+                            vc.emit('auditUserManage', 'listAuditUser', {});
 
-                            return ;
+                            return;
                         }
                         vc.message(json);
 
-                     },
-                     function(errInfo,error){
+                    },
+                    function (errInfo, error) {
                         console.log('请求失败处理');
 
                         vc.message(errInfo);
 
-                     });
+                    });
             },
-            clearAddAuditUserInfo:function(){
+            clearAddAuditUserInfo: function () {
                 vc.component.addAuditUserInfo = {
-                                            userId:'',
-userName:'',
-auditLink:'',
-objCode:'',
+                    userId: '',
+                    userName: '',
+                    auditLink: '',
+                    objCode: '',
 
-                                        };
+                };
             }
         }
     });
--- WebService/src/main/resources/components/auditUserPackage/delete-auditUser/deleteAuditUser.js
+++ WebService/src/main/resources/components/auditUserPackage/delete-auditUser/deleteAuditUser.js
@@ -1,52 +1,50 @@
-(function(vc,vm){
+(function (vc, vm) {
 
     vc.extends({
-        data:{
-            deleteAuditUserInfo:{
-
-            }
+        data: {
+            deleteAuditUserInfo: {}
         },
-         _initMethod:function(){
+        _initMethod: function () {
 
-         },
-         _initEvent:function(){
-             vc.on('deleteAuditUser','openDeleteAuditUserModal',function(_params){
+        },
+        _initEvent: function () {
+            vc.on('deleteAuditUser', 'openDeleteAuditUserModal', function (_params) {
 
                 vc.component.deleteAuditUserInfo = _params;
                 $('#deleteAuditUserModel').modal('show');
 
             });
         },
-        methods:{
-            deleteAuditUser:function(){
-                vc.component.deleteAuditUserInfo.communityId=vc.getCurrentCommunity().communityId;
+        methods: {
+            deleteAuditUser: function () {
+                vc.component.deleteAuditUserInfo.communityId = vc.getCurrentCommunity().communityId;
                 vc.http.post(
                     'deleteAuditUser',
                     'delete',
                     JSON.stringify(vc.component.deleteAuditUserInfo),
                     {
-                        emulateJSON:true
-                     },
-                     function(json,res){
+                        emulateJSON: true
+                    },
+                    function (json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
-                        if(res.status == 200){
+                        if (res.status == 200) {
                             //关闭model
                             $('#deleteAuditUserModel').modal('hide');
-                            vc.emit('auditUserManage','listAuditUser',{});
-                            return ;
+                            vc.emit('auditUserManage', 'listAuditUser', {});
+                            return;
                         }
                         vc.message(json);
-                     },
-                     function(errInfo,error){
+                    },
+                    function (errInfo, error) {
                         console.log('请求失败处理');
                         vc.message(json);
 
-                     });
+                    });
             },
-            closeDeleteAuditUserModel:function(){
+            closeDeleteAuditUserModel: function () {
                 $('#deleteAuditUserModel').modal('hide');
             }
         }
     });
 
-})(window.vc,window.vc.component);
+})(window.vc, window.vc.component);
