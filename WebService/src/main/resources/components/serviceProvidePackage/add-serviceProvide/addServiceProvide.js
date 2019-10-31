1c80cfa2d799407715ac4f72e57d95966bd6e781
wuxw <928255095@qq.com>
2019-10-31 11:02:39 +0800

    取消sql 大小限制

--- WebService/src/main/resources/components/serviceProvidePackage/add-serviceProvide/addServiceProvide.js
+++ WebService/src/main/resources/components/serviceProvidePackage/add-serviceProvide/addServiceProvide.js
@@ -1,140 +1,110 @@
-(function(vc){
+(function (vc) {
 
     vc.extends({
         propTypes: {
-               callBackListener:vc.propTypes.string, //父组件名称
-               callBackFunction:vc.propTypes.string //父组件监听方法
+            callBackListener: vc.propTypes.string, //父组件名称
+            callBackFunction: vc.propTypes.string //父组件监听方法
         },
-        data:{
-            addServiceProvideInfo:{
-                id:'',
-                name:'',
-serviceCode:'',
-params:'',
-queryModel:'',
-sql:'',
-template:'',
-proc:'',
-javaScript:'',
-remark:'',
+        data: {
+            addServiceProvideInfo: {
+                id: '',
+                name: '',
+                serviceCode: '',
+                params: '',
+                queryModel: '',
+                sql: '',
+                template: '',
+                proc: '',
+                javaScript: '',
+                remark: '',
 
             }
         },
-         _initMethod:function(){
+        _initMethod: function () {
 
-         },
-         _initEvent:function(){
-            vc.on('addServiceProvide','openAddServiceProvideModal',function(){
+        },
+        _initEvent: function () {
+            vc.on('addServiceProvide', 'openAddServiceProvideModal', function () {
                 $('#addServiceProvideModel').modal('show');
             });
         },
-        methods:{
-            addServiceProvideValidate(){
+        methods: {
+            addServiceProvideValidate() {
                 return vc.validate.validate({
-                    addServiceProvideInfo:vc.component.addServiceProvideInfo
-                },{
-                    'addServiceProvideInfo.name':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"服务名称不能为空"
+                    addServiceProvideInfo: vc.component.addServiceProvideInfo
+                }, {
+                    'addServiceProvideInfo.name': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "服务名称不能为空"
                         },
- {
-                            limit:"maxin",
-                            param:"2,50",
-                            errInfo:"服务名称必须在2至50字符之间"
-                        },
-                    ],
-'addServiceProvideInfo.serviceCode':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"服务编码不能为空"
-                        },
- {
-                            limit:"maxin",
-                            param:"2,50",
-                            errInfo:"服务编码必须在2至50字符之间"
+                        {
+                            limit: "maxin",
+                            param: "2,50",
+                            errInfo: "服务名称必须在2至50字符之间"
                         },
                     ],
-'addServiceProvideInfo.params':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"参数不能为空"
+                    'addServiceProvideInfo.serviceCode': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "服务编码不能为空"
                         },
- {
-                            limit:"maxLength",
-                            param:"500",
-                            errInfo:"参数内容不能超过200"
-                        },
-                    ],
-'addServiceProvideInfo.queryModel':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"实现方式不能为空"
-                        },
- {
-                            limit:"maxin",
-                            param:"1,12",
-                            errInfo:"实现方式错误"
+                        {
+                            limit: "maxin",
+                            param: "2,50",
+                            errInfo: "服务编码必须在2至50字符之间"
                         },
                     ],
-'addServiceProvideInfo.sql':[
- {
-                            limit:"maxLength",
-                            param:"2000",
-                            errInfo:"sql不能超过2000"
+                    'addServiceProvideInfo.params': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "参数不能为空"
+                        },
+                        {
+                            limit: "maxLength",
+                            param: "500",
+                            errInfo: "参数内容不能超过200"
                         },
                     ],
-'addServiceProvideInfo.template':[
- {
-                            limit:"maxLength",
-                            param:"2000",
-                            errInfo:"输出模板不能超过2000"
+                    'addServiceProvideInfo.queryModel': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "实现方式不能为空"
+                        },
+                        {
+                            limit: "maxin",
+                            param: "1,12",
+                            errInfo: "实现方式错误"
                         },
                     ],
-'addServiceProvideInfo.proc':[
- {
-                            limit:"maxLength",
-                            param:"200",
-                            errInfo:"存储过程不能超过200"
+                    'addServiceProvideInfo.remark': [
+                        {
+                            limit: "maxLength",
+                            param: "200",
+                            errInfo: "备注内容不能超过200"
                         },
                     ],
-'addServiceProvideInfo.javaScript':[
- {
-                            limit:"maxLength",
-                            param:"2000",
-                            errInfo:"java不能超过2000"
-                        },
-                    ],
-'addServiceProvideInfo.remark':[
- {
-                            limit:"maxLength",
-                            param:"200",
-                            errInfo:"备注内容不能超过200"
-                        },
-                    ],
-
-
 
 
                 });
             },
-            saveServiceProvideInfo:function(){
-                if(!vc.component.addServiceProvideValidate()){
+            saveServiceProvideInfo: function () {
+                if (!vc.component.addServiceProvideValidate()) {
                     vc.message(vc.validate.errInfo);
 
-                    return ;
+                    return;
                 }
 
                 vc.component.addServiceProvideInfo.communityId = vc.getCurrentCommunity().communityId;
                 //不提交数据将数据 回调给侦听处理
-                if(vc.notNull($props.callBackListener)){
-                    vc.emit($props.callBackListener,$props.callBackFunction,vc.component.addServiceProvideInfo);
+                if (vc.notNull($props.callBackListener)) {
+                    vc.emit($props.callBackListener, $props.callBackFunction, vc.component.addServiceProvideInfo);
                     $('#addServiceProvideModel').modal('hide');
-                    return ;
+                    return;
                 }
 
                 vc.http.post(
@@ -142,41 +112,41 @@ remark:'',
                     'save',
                     JSON.stringify(vc.component.addServiceProvideInfo),
                     {
-                        emulateJSON:true
-                     },
-                     function(json,res){
+                        emulateJSON: true
+                    },
+                    function (json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
-                        if(res.status == 200){
+                        if (res.status == 200) {
                             //关闭model
                             $('#addServiceProvideModel').modal('hide');
                             vc.component.clearAddServiceProvideInfo();
-                            vc.emit('serviceProvideManage','listServiceProvide',{});
+                            vc.emit('serviceProvideManage', 'listServiceProvide', {});
 
-                            return ;
+                            return;
                         }
                         vc.message(json);
 
-                     },
-                     function(errInfo,error){
+                    },
+                    function (errInfo, error) {
                         console.log('请求失败处理');
 
                         vc.message(errInfo);
 
-                     });
+                    });
             },
-            clearAddServiceProvideInfo:function(){
+            clearAddServiceProvideInfo: function () {
                 vc.component.addServiceProvideInfo = {
-                                            name:'',
-serviceCode:'',
-params:'',
-queryModel:'',
-sql:'',
-template:'',
-proc:'',
-javaScript:'',
-remark:'',
+                    name: '',
+                    serviceCode: '',
+                    params: '',
+                    queryModel: '',
+                    sql: '',
+                    template: '',
+                    proc: '',
+                    javaScript: '',
+                    remark: '',
 
-                                        };
+                };
             }
         }
     });
--- WebService/src/main/resources/components/serviceProvidePackage/edit-serviceProvide/editServiceProvide.js
+++ WebService/src/main/resources/components/serviceProvidePackage/edit-serviceProvide/editServiceProvide.js
@@ -1,133 +1,105 @@
-(function(vc,vm){
+(function (vc, vm) {
 
     vc.extends({
-        data:{
-            editServiceProvideInfo:{
-                id:'',
-name:'',
-serviceCode:'',
-params:'',
-queryModel:'',
-sql:'',
-template:'',
-proc:'',
-javaScript:'',
-remark:'',
+        data: {
+            editServiceProvideInfo: {
+                id: '',
+                name: '',
+                serviceCode: '',
+                params: '',
+                queryModel: '',
+                sql: '',
+                template: '',
+                proc: '',
+                javaScript: '',
+                remark: '',
 
             }
         },
-         _initMethod:function(){
+        _initMethod: function () {
 
-         },
-         _initEvent:function(){
-             vc.on('editServiceProvide','openEditServiceProvideModal',function(_params){
+        },
+        _initEvent: function () {
+            vc.on('editServiceProvide', 'openEditServiceProvideModal', function (_params) {
                 vc.component.refreshEditServiceProvideInfo();
                 $('#editServiceProvideModel').modal('show');
-                vc.copyObject(_params, vc.component.editServiceProvideInfo );
+                vc.copyObject(_params, vc.component.editServiceProvideInfo);
                 vc.component.editServiceProvideInfo.communityId = vc.getCurrentCommunity().communityId;
             });
         },
-        methods:{
-            editServiceProvideValidate:function(){
-                        return vc.validate.validate({
-                            editServiceProvideInfo:vc.component.editServiceProvideInfo
-                        },{
-                            'editServiceProvideInfo.name':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"服务名称不能为空"
+        methods: {
+            editServiceProvideValidate: function () {
+                return vc.validate.validate({
+                    editServiceProvideInfo: vc.component.editServiceProvideInfo
+                }, {
+                    'editServiceProvideInfo.name': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "服务名称不能为空"
                         },
- {
-                            limit:"maxin",
-                            param:"2,50",
-                            errInfo:"服务名称必须在2至50字符之间"
-                        },
-                    ],
-'editServiceProvideInfo.serviceCode':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"服务编码不能为空"
-                        },
- {
-                            limit:"maxin",
-                            param:"2,50",
-                            errInfo:"服务编码必须在2至50字符之间"
+                        {
+                            limit: "maxin",
+                            param: "2,50",
+                            errInfo: "服务名称必须在2至50字符之间"
                         },
                     ],
-'editServiceProvideInfo.params':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"参数不能为空"
+                    'editServiceProvideInfo.serviceCode': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "服务编码不能为空"
                         },
- {
-                            limit:"maxLength",
-                            param:"500",
-                            errInfo:"参数内容不能超过200"
-                        },
-                    ],
-'editServiceProvideInfo.queryModel':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"实现方式不能为空"
-                        },
- {
-                            limit:"maxin",
-                            param:"1,12",
-                            errInfo:"实现方式错误"
+                        {
+                            limit: "maxin",
+                            param: "2,50",
+                            errInfo: "服务编码必须在2至50字符之间"
                         },
                     ],
-'editServiceProvideInfo.sql':[
- {
-                            limit:"maxLength",
-                            param:"2000",
-                            errInfo:"sql不能超过2000"
+                    'editServiceProvideInfo.params': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "参数不能为空"
+                        },
+                        {
+                            limit: "maxLength",
+                            param: "500",
+                            errInfo: "参数内容不能超过200"
                         },
                     ],
-'editServiceProvideInfo.template':[
- {
-                            limit:"maxLength",
-                            param:"2000",
-                            errInfo:"输出模板不能超过2000"
+                    'editServiceProvideInfo.queryModel': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "实现方式不能为空"
+                        },
+                        {
+                            limit: "maxin",
+                            param: "1,12",
+                            errInfo: "实现方式错误"
                         },
                     ],
-'editServiceProvideInfo.proc':[
- {
-                            limit:"maxLength",
-                            param:"200",
-                            errInfo:"存储过程不能超过200"
+                    'editServiceProvideInfo.remark': [
+                        {
+                            limit: "maxLength",
+                            param: "200",
+                            errInfo: "备注内容不能超过200"
                         },
                     ],
-'editServiceProvideInfo.javaScript':[
- {
-                            limit:"maxLength",
-                            param:"2000",
-                            errInfo:"java不能超过2000"
-                        },
-                    ],
-'editServiceProvideInfo.remark':[
- {
-                            limit:"maxLength",
-                            param:"200",
-                            errInfo:"备注内容不能超过200"
-                        },
-                    ],
-'editServiceProvideInfo.id':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"提供ID不能为空"
+                    'editServiceProvideInfo.id': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "提供ID不能为空"
                         }]
 
-                        });
-             },
-            editServiceProvide:function(){
-                if(!vc.component.editServiceProvideValidate()){
+                });
+            },
+            editServiceProvide: function () {
+                if (!vc.component.editServiceProvideValidate()) {
                     vc.message(vc.validate.errInfo);
-                    return ;
+                    return;
                 }
 
                 vc.http.post(
@@ -135,40 +107,40 @@ remark:'',
                     'update',
                     JSON.stringify(vc.component.editServiceProvideInfo),
                     {
-                        emulateJSON:true
-                     },
-                     function(json,res){
+                        emulateJSON: true
+                    },
+                    function (json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
-                        if(res.status == 200){
+                        if (res.status == 200) {
                             //关闭model
                             $('#editServiceProvideModel').modal('hide');
-                             vc.emit('serviceProvideManage','listServiceProvide',{});
-                            return ;
+                            vc.emit('serviceProvideManage', 'listServiceProvide', {});
+                            return;
                         }
                         vc.message(json);
-                     },
-                     function(errInfo,error){
+                    },
+                    function (errInfo, error) {
                         console.log('请求失败处理');
 
                         vc.message(errInfo);
-                     });
+                    });
             },
-            refreshEditServiceProvideInfo:function(){
-                vc.component.editServiceProvideInfo= {
-                  id:'',
-name:'',
-serviceCode:'',
-params:'',
-queryModel:'',
-sql:'',
-template:'',
-proc:'',
-javaScript:'',
-remark:'',
+            refreshEditServiceProvideInfo: function () {
+                vc.component.editServiceProvideInfo = {
+                    id: '',
+                    name: '',
+                    serviceCode: '',
+                    params: '',
+                    queryModel: '',
+                    sql: '',
+                    template: '',
+                    proc: '',
+                    javaScript: '',
+                    remark: '',
 
                 }
             }
         }
     });
 
-})(window.vc,window.vc.component);
+})(window.vc, window.vc.component);
