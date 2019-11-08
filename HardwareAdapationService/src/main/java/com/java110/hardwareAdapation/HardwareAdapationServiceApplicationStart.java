0b3366a4eb737b249f8472393f1d7ebfc84c8a52
wuxw <928255095@qq.com>
2019-11-08 21:06:47 +0800

    加入硬件设配层服务，对接硬件设备，目前对接锐目人脸识别

---
+++ HardwareAdapationService/src/main/java/com/java110/hardwareAdapation/HardwareAdapationServiceApplicationStart.java
@@ -0,0 +1,63 @@
+package com.java110.fee;
+
+import com.java110.core.annotation.Java110ListenerDiscovery;
+import com.java110.event.service.BusinessServiceDataFlowEventPublishing;
+import com.java110.service.init.ServiceStartInit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+
+
+/**
+ * spring boot 初始化启动类
+ *
+ * @version v0.1
+ * @auther com.java110.wuxw
+ * @mail 928255095@qq.com
+ * @date 2016年8月6日
+ * @tag
+ */
+@SpringBootApplication(scanBasePackages = {"com.java110.service", "com.java110.hardwareAdapation",
+        "com.java110.core", "com.java110.cache", "com.java110.config.properties.code","com.java110.db"})
+@EnableDiscoveryClient
+@Java110ListenerDiscovery(listenerPublishClass = BusinessServiceDataFlowEventPublishing.class,
+        basePackages = {"com.java110.hardwareAdapation.listener"})
+@EnableFeignClients(basePackages = {"com.java110.core.smo.user"})
+public class HardwareAdapationServiceApplicationStart {
+
+    private static Logger logger = LoggerFactory.getLogger(HardwareAdapationServiceApplicationStart.class);
+
+
+    /**
+     * 实例化RestTemplate，通过@LoadBalanced注解开启均衡负载能力.
+     *
+     * @return restTemplate
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        StringHttpMessageConverter m = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        RestTemplate restTemplate = new RestTemplateBuilder().additionalMessageConverters(m).build();
+        return restTemplate;
+    }
+
+    public static void main(String[] args) throws Exception {
+        try{
+            ApplicationContext context = SpringApplication.run(HardwareAdapationServiceApplicationStart.class, args);
+            ServiceStartInit.initSystemConfig(context);
+        }catch (Throwable e){
+            logger.error("系统启动失败",e);
+        }
+    }
+}
