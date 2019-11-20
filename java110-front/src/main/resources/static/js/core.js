99d4a255704f69f30dfe99d9746c9929bb4bdf4e
吴学文 <wuxuewen@wuxuewendeMacBook-Pro.local>
2019-11-20 23:21:03 +0800

    加入toast 功能

--- java110-front/src/main/resources/static/js/core.js
+++ java110-front/src/main/resources/static/js/core.js
@@ -332,3 +332,22 @@
     }
 
 })(window.vc)
+
+/**
+toast
+**/
+(function(vc){
+    vc.toast = function Toast(msg,duration){
+                     duration=isNaN(duration)?3000:duration;
+                     var m = document.createElement('div');
+                     m.innerHTML = msg;
+                     m.style.cssText="max-width:60%;min-width: 150px;padding:0 14px;height: 40px;color: rgb(255, 255, 255);line-height: 40px;text-align: center;border-radius: 4px;position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);z-index: 999999;background: rgba(0, 0, 0,.7);font-size: 16px;";
+                     document.body.appendChild(m);
+                     setTimeout(function() {
+                       var d = 0.5;
+                       m.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';
+                       m.style.opacity = '0';
+                       setTimeout(function() { document.body.removeChild(m) }, d * 1000);
+                     }, duration);
+                   }
+})(window.vc);
