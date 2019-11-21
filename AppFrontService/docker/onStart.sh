bb3fb0dc69ef52dbedf11dede60e9ba089afaa4e
wuxw <928255095@qq.com>
2019-11-21 14:24:54 +0800

    优化appFront 名称

--- AppFrontService/docker/onStart.sh
+++ AppFrontService/docker/onStart.sh
@@ -6,6 +6,6 @@ cp  -r ../target .
 
 docker build -t java110/appFront .
 
-docker run -ti --name appFront_test -p8443:8443 -idt java110/appFront:latest
+docker run -ti --name appFront_test -p8012:8012 -idt java110/appFront:latest
 
 docker logs -f appFront_test
