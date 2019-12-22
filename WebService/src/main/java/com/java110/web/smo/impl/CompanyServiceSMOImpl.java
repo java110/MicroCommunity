8694090b2e1a9d40110a81878052c9576ebfb108
wuxw <928255095@qq.com>
2019-12-22 12:31:43 +0800

    优化地区查询功能

--- WebService/src/main/java/com/java110/web/smo/impl/CompanyServiceSMOImpl.java
+++ WebService/src/main/java/com/java110/web/smo/impl/CompanyServiceSMOImpl.java
@@ -143,7 +143,7 @@ public class CompanyServiceSMOImpl extends BaseComponentSMO implements ICompanyS
         Assert.hasLength(pd.getUserId(), "用户还未登录请先登录");
         JSONObject param = JSONObject.parseObject(pd.getReqData());
 
-        responseEntity = this.callCenterService(restTemplate, pd, mapToUrlParam(param), ServiceConstant.SERVICE_API_URL + "/api/area.listAreas", HttpMethod.GET);
+        responseEntity = this.callCenterService(restTemplate, pd, "", ServiceConstant.SERVICE_API_URL + "/api/area.listAreas" + mapToUrlParam(param), HttpMethod.GET);
 
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
             Assert.jsonObjectHaveKey(responseEntity.getBody(), "areas", "查询中心服务异常，不是有效json或未包含areas节点");
