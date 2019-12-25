f663617dbb1afab0f68801ceedef3a8eb11c81f3
wuxw <928255095@qq.com>
2019-12-25 21:22:18 +0800

    解决communityId 错误问题

--- WebService/src/main/resources/components/servicePackage/edit-service/editService.js
+++ WebService/src/main/resources/components/servicePackage/edit-service/editService.js
@@ -1,174 +1,173 @@
-(function(vc,vm){
+(function (vc, vm) {
 
     vc.extends({
-        data:{
-            editServiceInfo:{
-                serviceId:'',
-name:'',
-serviceCode:'',
-businessTypeCd:'API',
-seq:'1',
-messageQueueName:'',
-isInstance:'Y',
-url:'http://order-service/orderApi/service',
-method:'',
-timeout:'60',
-retryCount:'3',
-provideAppId:'8000418002',
+        data: {
+            editServiceInfo: {
+                serviceId: '',
+                name: '',
+                serviceCode: '',
+                businessTypeCd: 'API',
+                seq: '1',
+                messageQueueName: '',
+                isInstance: 'Y',
+                url: 'http://order-service/orderApi/service',
+                method: '',
+                timeout: '60',
+                retryCount: '3',
+                provideAppId: '8000418002',
 
             }
         },
-         _initMethod:function(){
+        _initMethod: function () {
 
-         },
-         _initEvent:function(){
-             vc.on('editService','openEditServiceModal',function(_params){
+        },
+        _initEvent: function () {
+            vc.on('editService', 'openEditServiceModal', function (_params) {
                 vc.component.refreshEditServiceInfo();
                 $('#editServiceModel').modal('show');
                 vc.copyObject(_params, vc.component.editServiceInfo);
-                vc.component.editServiceInfo.communityId = vc.getCurrentCommunity().communityId;
             });
         },
-        methods:{
-            editServiceValidate:function(){
-                        return vc.validate.validate({
-                            editServiceInfo:vc.component.editServiceInfo
-                        },{
-                            'editServiceInfo.name':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"服务名称不能为空"
+        methods: {
+            editServiceValidate: function () {
+                return vc.validate.validate({
+                    editServiceInfo: vc.component.editServiceInfo
+                }, {
+                    'editServiceInfo.name': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "服务名称不能为空"
                         },
- {
-                            limit:"maxLength",
-                            param:"50",
-                            errInfo:"服务名称不能超过50"
-                        },
-                    ],
-'editServiceInfo.serviceCode':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"服务编码不能为空"
-                        },
- {
-                            limit:"maxin",
-                            param:"2,50",
-                            errInfo:"服务编码必须在2至50字符之间"
+                        {
+                            limit: "maxLength",
+                            param: "50",
+                            errInfo: "服务名称不能超过50"
                         },
                     ],
-'editServiceInfo.businessTypeCd':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"秘钥不能为空"
+                    'editServiceInfo.serviceCode': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "服务编码不能为空"
                         },
- {
-                            limit:"maxin",
-                            param:"2,4",
-                            errInfo:"业务类型必须为API"
-                        },
-                    ],
-'editServiceInfo.seq':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"序列不能为空"
-                        },
- {
-                            limit:"maxLength",
-                            param:"1",
-                            errInfo:"序列格式错误"
+                        {
+                            limit: "maxin",
+                            param: "2,50",
+                            errInfo: "服务编码必须在2至50字符之间"
                         },
                     ],
-'editServiceInfo.messageQueueName':[
- {
-                            limit:"maxLength",
-                            param:"50",
-                            errInfo:"消息队列不能超过50"
+                    'editServiceInfo.businessTypeCd': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "秘钥不能为空"
+                        },
+                        {
+                            limit: "maxin",
+                            param: "2,4",
+                            errInfo: "业务类型必须为API"
                         },
                     ],
-'editServiceInfo.isInstance':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"是否实例不能为空"
+                    'editServiceInfo.seq': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "序列不能为空"
                         },
- {
-                            limit:"maxLength",
-                            param:"50",
-                            errInfo:"实例不能超过50"
-                        },
-                    ],
-'editServiceInfo.url':[
- {
-                            limit:"maxLength",
-                            param:"200",
-                            errInfo:"调用地址不能超过200"
+                        {
+                            limit: "maxLength",
+                            param: "1",
+                            errInfo: "序列格式错误"
                         },
                     ],
-'editServiceInfo.method':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"调用方式不能为空"
-                        },
- {
-                            limit:"maxLength",
-                            param:"50",
-                            errInfo:"调用方式不能超过50"
+                    'editServiceInfo.messageQueueName': [
+                        {
+                            limit: "maxLength",
+                            param: "50",
+                            errInfo: "消息队列不能超过50"
                         },
                     ],
-'editServiceInfo.timeout':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"超时时间不能为空"
+                    'editServiceInfo.isInstance': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "是否实例不能为空"
                         },
- {
-                            limit:"num",
-                            param:"",
-                            errInfo:"超时时间必须为数字"
-                        },
-                    ],
-'editServiceInfo.retryCount':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"重试次数不能为空"
-                        },
- {
-                            limit:"num",
-                            param:"",
-                            errInfo:"重试次数必须为数字"
+                        {
+                            limit: "maxLength",
+                            param: "50",
+                            errInfo: "实例不能超过50"
                         },
                     ],
-'editServiceInfo.provideAppId':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"提供服务不能为空"
-                        },
- {
-                            limit:"num",
-                            param:"",
-                            errInfo:"重试次数必须为数字"
+                    'editServiceInfo.url': [
+                        {
+                            limit: "maxLength",
+                            param: "200",
+                            errInfo: "调用地址不能超过200"
                         },
                     ],
-'editServiceInfo.serviceId':[
-{
-                            limit:"required",
-                            param:"",
-                            errInfo:"服务ID不能为空"
+                    'editServiceInfo.method': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "调用方式不能为空"
+                        },
+                        {
+                            limit: "maxLength",
+                            param: "50",
+                            errInfo: "调用方式不能超过50"
+                        },
+                    ],
+                    'editServiceInfo.timeout': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "超时时间不能为空"
+                        },
+                        {
+                            limit: "num",
+                            param: "",
+                            errInfo: "超时时间必须为数字"
+                        },
+                    ],
+                    'editServiceInfo.retryCount': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "重试次数不能为空"
+                        },
+                        {
+                            limit: "num",
+                            param: "",
+                            errInfo: "重试次数必须为数字"
+                        },
+                    ],
+                    'editServiceInfo.provideAppId': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "提供服务不能为空"
+                        },
+                        {
+                            limit: "num",
+                            param: "",
+                            errInfo: "重试次数必须为数字"
+                        },
+                    ],
+                    'editServiceInfo.serviceId': [
+                        {
+                            limit: "required",
+                            param: "",
+                            errInfo: "服务ID不能为空"
                         }]
 
-                        });
-             },
-            editService:function(){
-                if(!vc.component.editServiceValidate()){
+                });
+            },
+            editService: function () {
+                if (!vc.component.editServiceValidate()) {
                     vc.message(vc.validate.errInfo);
-                    return ;
+                    return;
                 }
 
                 vc.http.post(
@@ -176,42 +175,42 @@ provideAppId:'8000418002',
                     'update',
                     JSON.stringify(vc.component.editServiceInfo),
                     {
-                        emulateJSON:true
-                     },
-                     function(json,res){
+                        emulateJSON: true
+                    },
+                    function (json, res) {
                         //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
-                        if(res.status == 200){
+                        if (res.status == 200) {
                             //关闭model
                             $('#editServiceModel').modal('hide');
-                             vc.emit('serviceManage','listService',{});
-                            return ;
+                            vc.emit('serviceManage', 'listService', {});
+                            return;
                         }
                         vc.message(json);
-                     },
-                     function(errInfo,error){
+                    },
+                    function (errInfo, error) {
                         console.log('请求失败处理');
 
                         vc.message(errInfo);
-                     });
+                    });
             },
-            refreshEditServiceInfo:function(){
-                vc.component.editServiceInfo= {
-                  serviceId:'',
-name:'',
-serviceCode:'',
-businessTypeCd:'API',
-seq:'1',
-messageQueueName:'',
-isInstance:'Y',
-url:'http://order-service/orderApi/service',
-method:'',
-timeout:'60',
-retryCount:'3',
-provideAppId:'8000418002',
+            refreshEditServiceInfo: function () {
+                vc.component.editServiceInfo = {
+                    serviceId: '',
+                    name: '',
+                    serviceCode: '',
+                    businessTypeCd: 'API',
+                    seq: '1',
+                    messageQueueName: '',
+                    isInstance: 'Y',
+                    url: 'http://order-service/orderApi/service',
+                    method: '',
+                    timeout: '60',
+                    retryCount: '3',
+                    provideAppId: '8000418002',
 
                 }
             }
         }
     });
 
-})(window.vc,window.vc.component);
+})(window.vc, window.vc.component);
