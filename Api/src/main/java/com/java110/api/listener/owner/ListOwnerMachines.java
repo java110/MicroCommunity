16763794e4122ac136056c086377d8fddbc5bc56
wuxw <928255095@qq.com>
2020-01-01 00:31:53 +0800

    加入位置翻译功能

--- Api/src/main/java/com/java110/api/listener/owner/ListOwnerMachines.java
+++ Api/src/main/java/com/java110/api/listener/owner/ListOwnerMachines.java
@@ -4,12 +4,16 @@ import com.alibaba.fastjson.JSONObject;
 import com.java110.api.listener.AbstractServiceApiListener;
 import com.java110.core.annotation.Java110Listener;
 import com.java110.core.context.DataFlowContext;
+import com.java110.core.smo.community.ICommunityInnerServiceSMO;
 import com.java110.core.smo.hardwareAdapation.IMachineInnerServiceSMO;
 import com.java110.core.smo.owner.IOwnerInnerServiceSMO;
 import com.java110.core.smo.room.IRoomInnerServiceSMO;
+import com.java110.core.smo.unit.IUnitInnerServiceSMO;
 import com.java110.dto.RoomDto;
+import com.java110.dto.community.CommunityDto;
 import com.java110.dto.hardwareAdapation.MachineDto;
 import com.java110.dto.owner.OwnerDto;
+import com.java110.dto.unit.FloorAndUnitDto;
 import com.java110.event.service.api.ServiceDataFlowEvent;
 import com.java110.utils.constant.ServiceCodeConstant;
 import com.java110.utils.util.Assert;
@@ -38,6 +42,12 @@ public class ListOwnerMachines extends AbstractServiceApiListener {
 
     @Autowired
     private IOwnerInnerServiceSMO ownerInnerServiceSMOImpl;
+
+    @Autowired
+    private ICommunityInnerServiceSMO communityInnerServiceSMOImpl;
+
+    @Autowired
+    private IUnitInnerServiceSMO unitInnerServiceSMOImpl;
 
     @Override
     protected void validate(ServiceDataFlowEvent event, JSONObject reqJson) {
@@ -76,6 +86,8 @@ public class ListOwnerMachines extends AbstractServiceApiListener {
         if (machineDtos == null) {
             machineDtos = new ArrayList<>();
         }
+        // 刷新 位置信息
+        refreshMachines(machineDtos);
 
         ApiMachineVo apiMachineVo = new ApiMachineVo();
         apiMachineVo.setPage(1);
@@ -84,6 +96,130 @@ public class ListOwnerMachines extends AbstractServiceApiListener {
         apiMachineVo.setMachines(BeanConvertUtil.covertBeanList(machineDtos, ApiMachineDataVo.class));
         ResponseEntity responseEntity = new ResponseEntity(JSONObject.toJSONString(apiMachineVo), HttpStatus.OK);
         context.setResponseEntity(responseEntity);
+    }
+    private void refreshMachines(List<MachineDto> machines) {
+
+        //批量处理 小区
+        refreshCommunitys(machines);
+
+        //批量处理单元信息
+        refreshUnits(machines);
+
+        //批量处理 房屋信息
+        refreshRooms(machines);
+
+    }
+    /**
+     * 获取批量小区
+     *
+     * @param machines 设备信息
+     * @return 批量userIds 信息
+     */
+    private void refreshCommunitys(List<MachineDto> machines) {
+        List<String> communityIds = new ArrayList<String>();
+        List<MachineDto> tmpMachineDtos = new ArrayList<>();
+        for (MachineDto machineDto : machines) {
+
+            if (!"2000".equals(machineDto.getLocationTypeCd())
+                    && !"3000".equals(machineDto.getLocationTypeCd())
+            ) {
+                communityIds.add(machineDto.getLocationObjId());
+                tmpMachineDtos.add(machineDto);
+            }
+        }
+
+        if (communityIds.size() < 1) {
+            return;
+        }
+        String[] tmpCommunityIds = communityIds.toArray(new String[communityIds.size()]);
+
+        CommunityDto communityDto = new CommunityDto();
+        communityDto.setCommunityIds(tmpCommunityIds);
+        //根据 userId 查询用户信息
+        List<CommunityDto> communityDtos = communityInnerServiceSMOImpl.queryCommunitys(communityDto);
+
+        for (MachineDto machineDto : tmpMachineDtos) {
+            for (CommunityDto tmpCommunityDto : communityDtos) {
+                if (machineDto.getLocationObjId().equals(tmpCommunityDto.getCommunityId())) {
+                    machineDto.setLocationObjName(tmpCommunityDto.getName() + " " + machineDto.getLocationTypeName());
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 获取批量单元
+     *
+     * @param machines 设备信息
+     * @return 批量userIds 信息
+     */
+    private void refreshUnits(List<MachineDto> machines) {
+        List<String> unitIds = new ArrayList<String>();
+        List<MachineDto> tmpMachineDtos = new ArrayList<>();
+        for (MachineDto machineDto : machines) {
+
+            if ("2000".equals(machineDto.getLocationTypeCd())) {
+                unitIds.add(machineDto.getLocationObjId());
+                tmpMachineDtos.add(machineDto);
+            }
+        }
+
+        if (unitIds.size() < 1) {
+            return;
+        }
+        String[] tmpUnitIds = unitIds.toArray(new String[unitIds.size()]);
+
+        FloorAndUnitDto floorAndUnitDto = new FloorAndUnitDto();
+        floorAndUnitDto.setUnitIds(tmpUnitIds);
+        //根据 userId 查询用户信息
+        List<FloorAndUnitDto> unitDtos = unitInnerServiceSMOImpl.getFloorAndUnitInfo(floorAndUnitDto);
+
+        for (MachineDto machineDto : tmpMachineDtos) {
+            for (FloorAndUnitDto tmpUnitDto : unitDtos) {
+                if (machineDto.getLocationObjId().equals(tmpUnitDto.getUnitId())) {
+                    machineDto.setLocationObjName(tmpUnitDto.getFloorNum() + "栋" + tmpUnitDto.getUnitNum() + "单元");
+                    BeanConvertUtil.covertBean(tmpUnitDto, machineDto);
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取批量单元
+     *
+     * @param machines 设备信息
+     * @return 批量userIds 信息
+     */
+    private void refreshRooms(List<MachineDto> machines) {
+        List<String> roomIds = new ArrayList<String>();
+        List<MachineDto> tmpMachineDtos = new ArrayList<>();
+        for (MachineDto machineDto : machines) {
+
+            if ("3000".equals(machineDto.getLocationTypeCd())) {
+                roomIds.add(machineDto.getLocationObjId());
+                tmpMachineDtos.add(machineDto);
+            }
+        }
+        if (roomIds.size() < 1) {
+            return;
+        }
+        String[] tmpRoomIds = roomIds.toArray(new String[roomIds.size()]);
+
+        RoomDto roomDto = new RoomDto();
+        roomDto.setRoomIds(tmpRoomIds);
+        roomDto.setCommunityId(machines.get(0).getCommunityId());
+        //根据 userId 查询用户信息
+        List<RoomDto> roomDtos = roomInnerServiceSMOImpl.queryRooms(roomDto);
+
+        for (MachineDto machineDto : tmpMachineDtos) {
+            for (RoomDto tmpRoomDto : roomDtos) {
+                if (machineDto.getLocationObjId().equals(tmpRoomDto.getRoomId())) {
+                    machineDto.setLocationObjName(tmpRoomDto.getFloorNum() + "栋" + tmpRoomDto.getUnitNum() + "单元" + tmpRoomDto.getRoomNum() + "室");
+                    BeanConvertUtil.covertBean(tmpRoomDto, machineDto);
+                }
+            }
+        }
     }
 
     @Override
@@ -115,5 +251,29 @@ public class ListOwnerMachines extends AbstractServiceApiListener {
 
     public void setMachineInnerServiceSMOImpl(IMachineInnerServiceSMO machineInnerServiceSMOImpl) {
         this.machineInnerServiceSMOImpl = machineInnerServiceSMOImpl;
+    }
+
+    public IOwnerInnerServiceSMO getOwnerInnerServiceSMOImpl() {
+        return ownerInnerServiceSMOImpl;
+    }
+
+    public void setOwnerInnerServiceSMOImpl(IOwnerInnerServiceSMO ownerInnerServiceSMOImpl) {
+        this.ownerInnerServiceSMOImpl = ownerInnerServiceSMOImpl;
+    }
+
+    public ICommunityInnerServiceSMO getCommunityInnerServiceSMOImpl() {
+        return communityInnerServiceSMOImpl;
+    }
+
+    public void setCommunityInnerServiceSMOImpl(ICommunityInnerServiceSMO communityInnerServiceSMOImpl) {
+        this.communityInnerServiceSMOImpl = communityInnerServiceSMOImpl;
+    }
+
+    public IUnitInnerServiceSMO getUnitInnerServiceSMOImpl() {
+        return unitInnerServiceSMOImpl;
+    }
+
+    public void setUnitInnerServiceSMOImpl(IUnitInnerServiceSMO unitInnerServiceSMOImpl) {
+        this.unitInnerServiceSMOImpl = unitInnerServiceSMOImpl;
     }
 }
