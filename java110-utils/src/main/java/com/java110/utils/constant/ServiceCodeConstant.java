d66b6f0aa5e40b9c96de2b306ad84aea64e79a36
wuxw <928255095@qq.com>
2020-01-27 13:38:05 +0800

    加入

--- java110-utils/src/main/java/com/java110/utils/constant/ServiceCodeConstant.java
+++ java110-utils/src/main/java/com/java110/utils/constant/ServiceCodeConstant.java
@@ -485,6 +485,9 @@ public class ServiceCodeConstant {
     //查询费用配置 根据停车位
     public static final String SERVICE_CODE_QUERY_FEE_BY_PARKING_SPACE = "fee.queryFeeByParkingSpace";
 
+    //查询费用配置 根据车辆进出场
+    public static final String SERVICE_CODE_QUERY_FEE_BY_CAR_INOUT = "fee.queryFeeByCarInout";
+
     //保存费用配置
     public static final String SERVICE_CODE_SAVE_FEE_CONFIG = "fee.saveFeeConfig";
 
