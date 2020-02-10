dd97ff2f0c17d4f9bdfefad0b4cc8abb3996f65c
mrzcc <121184950@qq.com>
2020-02-10 14:46:05 +0800

    巡检点Vo

---
+++ java110-bean/src/main/java/com/java110/vo/api/inspectionPoint/ApiInspectionPointDataVo.java
@@ -0,0 +1,107 @@
+package com.java110.vo.api.inspectionPoint;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ApiInspectionPointDataVo implements Serializable {
+
+    private String inspectionId;
+    private String inspectionName;
+    private String remark;
+    private String machineCode;
+    private String machineName;
+    private String locationTypeCd;
+    private String locationTypeName;
+    private String locationObjId;
+    private String locationObjName;
+    private String machineId;
+    private String communityId;
+
+    public String getInspectionId() {
+        return inspectionId;
+    }
+
+    public void setInspectionId(String inspectionId) {
+        this.inspectionId = inspectionId;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getInspectionName() {
+        return inspectionName;
+    }
+
+    public void setInspectionName(String inspectionName) {
+        this.inspectionName = inspectionName;
+    }
+
+    public String getMachineCode() {
+        return machineCode;
+    }
+
+    public void setMachineCode(String machineCode) {
+        this.machineCode = machineCode;
+    }
+
+    public String getMachineName() {
+        return machineName;
+    }
+
+    public void setMachineName(String machineName) {
+        this.machineName = machineName;
+    }
+
+    public String getLocationTypeCd() {
+        return locationTypeCd;
+    }
+
+    public void setLocationTypeCd(String locationTypeCd) {
+        this.locationTypeCd = locationTypeCd;
+    }
+
+    public String getLocationObjId() {
+        return locationObjId;
+    }
+
+    public void setLocationObjId(String locationObjId) {
+        this.locationObjId = locationObjId;
+    }
+
+    public String getLocationObjName() {
+        return locationObjName;
+    }
+
+    public void setLocationObjName(String locationObjName) {
+        this.locationObjName = locationObjName;
+    }
+
+    public String getMachineId() {
+        return machineId;
+    }
+
+    public void setMachineId(String machineId) {
+        this.machineId = machineId;
+    }
+
+    public String getCommunityId() {
+        return communityId;
+    }
+
+    public void setCommunityId(String communityId) {
+        this.communityId = communityId;
+    }
+
+    public String getLocationTypeName() {
+        return locationTypeName;
+    }
+
+    public void setLocationTypeName(String locationTypeName) {
+        this.locationTypeName = locationTypeName;
+    }
+}
