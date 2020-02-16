b9e8b45a5fae6e2569c8ea8842d1361129009414
wuxw <928255095@qq.com>
2020-02-16 19:44:00 +0800

    优化权限组变价

--- WebService/src/main/resources/components/servicePackage/add-service/addService.js
+++ WebService/src/main/resources/components/servicePackage/add-service/addService.js
@@ -170,7 +170,7 @@ provideAppId:'8000418002',
                     return ;
                 }
 
-                vc.component.addServiceInfo.communityId = vc.getCurrentCommunity().communityId;
+                //vc.component.addServiceInfo.communityId = vc.getCurrentCommunity().communityId;
                 //不提交数据将数据 回调给侦听处理
                 if(vc.notNull($props.callBackListener)){
                     vc.emit($props.callBackListener,$props.callBackFunction,vc.component.addServiceInfo);
