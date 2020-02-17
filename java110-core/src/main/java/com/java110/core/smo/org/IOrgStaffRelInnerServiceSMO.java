9e0575bc897d712e2e3f8d70c9abe0f7c866bf8e
mrzcc <121184950@qq.com>
2020-02-17 18:46:04 +0800

    新增查询组织信息接口(以员工id集合为参数)

--- java110-core/src/main/java/com/java110/core/smo/org/IOrgStaffRelInnerServiceSMO.java
+++ java110-core/src/main/java/com/java110/core/smo/org/IOrgStaffRelInnerServiceSMO.java
@@ -38,4 +38,13 @@ public interface IOrgStaffRelInnerServiceSMO {
      */
     @RequestMapping(value = "/queryOrgStaffRelsCount", method = RequestMethod.POST)
     int queryOrgStaffRelsCount(@RequestBody OrgStaffRelDto orgStaffRelDto);
+
+    /**
+     * <p>查询部门公司信息</p>
+     *
+     * @param orgStaffRelDto 员工ids
+     * @return OrgStaffRelDto 对象数据
+     */
+    @RequestMapping(value = "/queryOrgInfoByStaffIds", method = RequestMethod.POST)
+    List<OrgStaffRelDto> queryOrgInfoByStaffIds(@RequestBody OrgStaffRelDto orgStaffRelDto);
 }
