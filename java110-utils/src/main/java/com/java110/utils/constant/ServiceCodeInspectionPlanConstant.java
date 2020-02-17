df0f7c8e37042a077c92cc653bde7f68cdab958c
mrzcc <121184950@qq.com>
2020-02-17 18:38:47 +0800

    巡检计划Constant

---
+++ java110-utils/src/main/java/com/java110/utils/constant/ServiceCodeInspectionPlanConstant.java
@@ -0,0 +1,31 @@
+package com.java110.utils.constant;
+
+/**
+ * 巡检计划常量类
+ * Created by wuxw on 2017/5/20.
+ */
+public class ServiceCodeInspectionPlanConstant {
+
+    /**
+     * 添加 巡检计划
+     */
+    public static final String ADD_INSPECTIONPLAN = "inspectionPlan.saveInspectionPlan";
+
+
+    /**
+     * 修改 巡检计划
+     */
+    public static final String UPDATE_INSPECTIONPLAN = "inspectionPlan.updateInspectionPlan";
+    /**
+     * 删除 巡检计划
+     */
+    public static final String DELETE_INSPECTIONPLAN = "inspectionPlan.deleteInspectionPlan";
+
+
+    /**
+     * 查询 巡检计划
+     */
+    public static final String LIST_INSPECTIONPLANS = "inspectionPlan.listInspectionPlans";
+
+
+}
