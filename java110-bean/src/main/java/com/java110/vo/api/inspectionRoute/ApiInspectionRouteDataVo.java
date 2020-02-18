4741765c3e0e1ad1b207093b74bb54124e4884a0
mrzcc <121184950@qq.com>
2020-02-18 19:56:57 +0800

    优化巡检路线Vo

--- java110-bean/src/main/java/com/java110/vo/api/inspectionRoute/ApiInspectionRouteDataVo.java
+++ java110-bean/src/main/java/com/java110/vo/api/inspectionRoute/ApiInspectionRouteDataVo.java
@@ -6,48 +6,66 @@ import java.util.Date;
 public class ApiInspectionRouteDataVo implements Serializable {
 
     private String configId;
-private String routeName;
-private String inspectionName;
-private String machineQuantity;
-private String checkQuantity;
-private String remark;
-public String getConfigId() {
+    private String routeName;
+    private String inspectionName;
+    private String machineQuantity;
+    private String checkQuantity;
+    private String remark;
+    private String inspectionRouteId;
+
+    public String getConfigId() {
         return configId;
     }
-public void setConfigId(String configId) {
+
+    public void setConfigId(String configId) {
         this.configId = configId;
     }
-public String getRouteName() {
+
+    public String getRouteName() {
         return routeName;
     }
-public void setRouteName(String routeName) {
+
+    public void setRouteName(String routeName) {
         this.routeName = routeName;
     }
-public String getInspectionName() {
+
+    public String getInspectionName() {
         return inspectionName;
     }
-public void setInspectionName(String inspectionName) {
+
+    public void setInspectionName(String inspectionName) {
         this.inspectionName = inspectionName;
     }
-public String getMachineQuantity() {
+
+    public String getMachineQuantity() {
         return machineQuantity;
     }
-public void setMachineQuantity(String machineQuantity) {
+
+    public void setMachineQuantity(String machineQuantity) {
         this.machineQuantity = machineQuantity;
     }
-public String getCheckQuantity() {
+
+    public String getCheckQuantity() {
         return checkQuantity;
     }
-public void setCheckQuantity(String checkQuantity) {
+
+    public void setCheckQuantity(String checkQuantity) {
         this.checkQuantity = checkQuantity;
     }
-public String getRemark() {
+
+    public String getRemark() {
         return remark;
     }
-public void setRemark(String remark) {
+
+    public void setRemark(String remark) {
         this.remark = remark;
     }
 
+    public String getInspectionRouteId() {
+        return inspectionRouteId;
+    }
 
-
+    public void setInspectionRouteId(String inspectionRouteId) {
+        this.inspectionRouteId = inspectionRouteId;
+    }
 }
