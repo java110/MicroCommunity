74180f1be3615d771a4cb6e9da24c8fcc92a0ff2
mrzcc <121184950@qq.com>
2020-02-26 19:02:33 +0800

    采购申请

---
+++ WebService/src/main/resources/components/purchaseApplyPackage/add-purchaseApply/addPurchaseApply.js
@@ -0,0 +1,130 @@
+(function(vc){
+
+    vc.extends({
+        propTypes: {
+               callBackListener:vc.propTypes.string, //父组件名称
+               callBackFunction:vc.propTypes.string //父组件监听方法
+        },
+        data:{
+            addPurchaseApplyInfo:{
+                applyOrderId:'',
+                state:'',
+                description:''
+
+            }
+        },
+         _initMethod:function(){
+            //初始化物品表格
+             vc.component.initResourceTable();
+         },
+         _initEvent:function(){
+            vc.on('addPurchaseApply','openAddPurchaseApplyModal',function(){
+                $('#addPurchaseApplyModel').modal('show');
+            });
+        },
+        methods:{
+            addPurchaseApplyValidate(){
+                return vc.validate.validate({
+                    addPurchaseApplyInfo:vc.component.addPurchaseApplyInfo
+                },{
+                    'addPurchaseApplyInfo.state':[
+{
+                            limit:"required",
+                            param:"",
+                            errInfo:"订单状态不能为空"
+                        },
+ {
+                            limit:"num",
+                            param:"",
+                            errInfo:"计算公式格式错误"
+                        },
+                    ],
+
+
+
+
+                });
+            },
+            savePurchaseApplyInfo:function(){
+                if(!vc.component.addPurchaseApplyValidate()){
+                    vc.toast(vc.validate.errInfo);
+
+                    return ;
+                }
+
+                vc.component.addPurchaseApplyInfo.communityId = vc.getCurrentCommunity().communityId;
+                //不提交数据将数据 回调给侦听处理
+                if(vc.notNull($props.callBackListener)){
+                    vc.emit($props.callBackListener,$props.callBackFunction,vc.component.addPurchaseApplyInfo);
+                    $('#addPurchaseApplyModel').modal('hide');
+                    return ;
+                }
+
+                vc.http.post(
+                    'addPurchaseApply',
+                    'save',
+                    JSON.stringify(vc.component.addPurchaseApplyInfo),
+                    {
+                        emulateJSON:true
+                     },
+                     function(json,res){
+                        //vm.menus = vm.refreshMenuActive(JSON.parse(json),0);
+                        if(res.status == 200){
+                            //关闭model
+                            $('#addPurchaseApplyModel').modal('hide');
+                            vc.component.clearAddPurchaseApplyInfo();
+                            vc.emit('purchaseApplyManage','listPurchaseApply',{});
+
+                            return ;
+                        }
+                        vc.message(json);
+
+                     },
+                     function(errInfo,error){
+                        console.log('请求失败处理');
+
+                        vc.message(errInfo);
+
+                     });
+            },
+            clearAddPurchaseApplyInfo:function(){
+                vc.component.addPurchaseApplyInfo = {
+                    state:'',
+                };
+            },
+            //初始化表格
+            initResourceTable:function(){
+                $('#resource_table').bootstrapTable({
+                    height: 500,
+                    columns: [
+                        {
+                            field:'resId',
+                            title:'资源ID',
+                        },
+                        {
+                            field:'resName',
+                            title:'资源名称',
+                        },{
+                            field:'resCode',
+                            title:'物品编码',
+                        },{
+                            field:'price',
+                            title:'单价',
+                        },{
+                            field:'stock',
+                            title:'库存'
+                        },{
+                            field:'quantity',
+                            title:'采购数量'
+                        },
+                        {
+                            field:'remark',
+                            title:'备注'
+                        }
+                    ],
+                });
+            }
+        }
+    });
+
+})(window.vc);
---
+++ WebService/src/main/resources/components/purchaseApplyPackage/purchaseApply-manage/purchaseApplyManage.js
@@ -0,0 +1,85 @@
+/**
+    入驻小区
+**/
+(function(vc){
+    var DEFAULT_PAGE = 1;
+    var DEFAULT_ROWS = 10;
+    vc.extends({
+        data:{
+            purchaseApplyManageInfo:{
+                purchaseApplys:[],
+                total:0,
+                records:1,
+                moreCondition:false,
+                applyOrderId:'',
+                conditions:{
+                    state:'',
+applyOrderId:'',
+
+                }
+            }
+        },
+        _initMethod:function(){
+            vc.component._listPurchaseApplys(DEFAULT_PAGE, DEFAULT_ROWS);
+        },
+        _initEvent:function(){
+            
+            vc.on('purchaseApplyManage','listPurchaseApply',function(_param){
+                  vc.component._listPurchaseApplys(DEFAULT_PAGE, DEFAULT_ROWS);
+            });
+             vc.on('pagination','page_event',function(_currentPage){
+                vc.component._listPurchaseApplys(_currentPage,DEFAULT_ROWS);
+            });
+        },
+        methods:{
+            _listPurchaseApplys:function(_page, _rows){
+
+                vc.component.purchaseApplyManageInfo.conditions.page = _page;
+                vc.component.purchaseApplyManageInfo.conditions.row = _rows;
+                var param = {
+                    params:vc.component.purchaseApplyManageInfo.conditions
+               };
+
+               //发送get请求
+               vc.http.get('purchaseApplyManage',
+                            'list',
+                             param,
+                             function(json,res){
+                                var _purchaseApplyManageInfo=JSON.parse(json);
+                                vc.component.purchaseApplyManageInfo.total = _purchaseApplyManageInfo.total;
+                                vc.component.purchaseApplyManageInfo.records = _purchaseApplyManageInfo.records;
+                                vc.component.purchaseApplyManageInfo.purchaseApplys = _purchaseApplyManageInfo.purchaseApplys;
+                                vc.emit('pagination','init',{
+                                     total:vc.component.purchaseApplyManageInfo.records,
+                                     currentPage:_page
+                                 });
+                             },function(errInfo,error){
+                                console.log('请求失败处理');
+                             }
+                           );
+            },
+            _openAddPurchaseApplyModal:function(){
+                vc.emit('addPurchaseApply','openAddPurchaseApplyModal',{});
+            },
+            _openEditPurchaseApplyModel:function(_purchaseApply){
+                vc.emit('editPurchaseApply','openEditPurchaseApplyModal',_purchaseApply);
+            },
+            _openDeletePurchaseApplyModel:function(_purchaseApply){
+                vc.emit('deletePurchaseApply','openDeletePurchaseApplyModal',_purchaseApply);
+            },
+            _queryPurchaseApplyMethod:function(){
+                vc.component._listPurchaseApplys(DEFAULT_PAGE, DEFAULT_ROWS);
+
+            },
+            _moreCondition:function(){
+                if(vc.component.purchaseApplyManageInfo.moreCondition){
+                    vc.component.purchaseApplyManageInfo.moreCondition = false;
+                }else{
+                    vc.component.purchaseApplyManageInfo.moreCondition = true;
+                }
+            }
+
+             
+        }
+    });
+})(window.vc);
