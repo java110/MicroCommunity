00b82e73f39c4b781c94f9008e8f1ab5e1e85a3c
mrzcc <121184950@qq.com>
2020-02-26 19:05:44 +0800

    采购申请

---
+++ java110-utils/src/main/java/com/java110/utils/constant/ServiceCodePurchaseApplyConstant.java
@@ -0,0 +1,31 @@
+package com.java110.utils.constant;
+
+/**
+ * 采购申请常量类
+ * Created by wuxw on 2017/5/20.
+ */
+public class ServiceCodePurchaseApplyConstant {
+
+    /**
+     * 添加 采购申请
+     */
+    public static final String ADD_PURCHASE_APPLY = "purchaseApply.savePurchaseApply";
+
+
+    /**
+     * 修改 采购申请
+     */
+    public static final String UPDATE_PURCHASE_APPLY = "purchaseApply.updatePurchaseApply";
+    /**
+     * 删除 采购申请
+     */
+    public static final String DELETE_PURCHASE_APPLY = "purchaseApply.deletePurchaseApply";
+
+
+    /**
+     * 查询 采购申请
+     */
+    public static final String LIST_PURCHASE_APPLY = "purchaseApply.listPurchaseApplys";
+
+
+}
