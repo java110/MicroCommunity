38cb1ca54475c66cad8c800ec877d451496254a7
mrzcc <121184950@qq.com>
2020-02-26 19:04:15 +0800

    采购申请

---
+++ java110-bean/src/main/java/com/java110/dto/purchaseApply/PurchaseApplyDto.java
@@ -0,0 +1,113 @@
+package com.java110.dto.purchaseApply;
+
+import com.java110.dto.PageDto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @ClassName FloorDto
+ * @Description 采购申请数据层封装
+ * @Author wuxw
+ * @Date 2019/4/24 8:52
+ * @Version 1.0
+ * add by wuxw 2019/4/24
+ **/
+public class PurchaseApplyDto extends PageDto implements Serializable {
+
+    private String applyDetailId;
+    private String resOrderType;
+    private String description;
+    private String applyOrderId;
+    private String state;
+    private String storeId;
+    private String entryPerson;
+    private String userId;
+
+
+    private Date createTime;
+
+    private String statusCd = "0";
+
+
+    public String getApplyDetailId() {
+        return applyDetailId;
+    }
+
+    public void setApplyDetailId(String applyDetailId) {
+        this.applyDetailId = applyDetailId;
+    }
+
+    public String getResOrderType() {
+        return resOrderType;
+    }
+
+    public void setResOrderType(String resOrderType) {
+        this.resOrderType = resOrderType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getApplyOrderId() {
+        return applyOrderId;
+    }
+
+    public void setApplyOrderId(String applyOrderId) {
+        this.applyOrderId = applyOrderId;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(String storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getEntryPerson() {
+        return entryPerson;
+    }
+
+    public void setEntryPerson(String entryPerson) {
+        this.entryPerson = entryPerson;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStatusCd() {
+        return statusCd;
+    }
+
+    public void setStatusCd(String statusCd) {
+        this.statusCd = statusCd;
+    }
+}
