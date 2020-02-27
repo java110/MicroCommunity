6df6f010e73d90a6ce25c7ecede5f9c71d779dd8
mrzcc <121184950@qq.com>
2020-02-27 13:03:50 +0800

    更新物品js

--- WebService/src/main/resources/components/resourceStorePackage/view-resourceStore-info/viewResourceStoreInfo.js
+++ WebService/src/main/resources/components/resourceStorePackage/view-resourceStore-info/viewResourceStoreInfo.js
@@ -1,49 +1,49 @@
 /**
-    物品管理 组件
-**/
-(function(vc){
+ 物品管理 组件
+ **/
+(function (vc) {
 
     vc.extends({
         propTypes: {
-           callBackListener:vc.propTypes.string, //父组件名称
-           callBackFunction:vc.propTypes.string //父组件监听方法
+            callBackListener: vc.propTypes.string, //父组件名称
+            callBackFunction: vc.propTypes.string //父组件监听方法
         },
-        data:{
-            viewResourceStoreInfo:{
-                index:0,
-                flowComponent:'viewResourceStoreInfo',
-                resName:'',
-resCode:'',
-price:'',
-stock:'',
-description:'',
+        data: {
+            viewResourceStoreInfo: {
+                index: 0,
+                flowComponent: 'viewResourceStoreInfo',
+                resName: '',
+                resCode: '',
+                price: '',
+                stock: '',
+                description: '',
 
             }
         },
-        _initMethod:function(){
+        _initMethod: function () {
             //根据请求参数查询 查询 业主信息
             vc.component._loadResourceStoreInfoData();
         },
-        _initEvent:function(){
-            vc.on('viewResourceStoreInfo','chooseResourceStore',function(_app){
+        _initEvent: function () {
+            vc.on('viewResourceStoreInfo', 'chooseResourceStore', function (_app) {
                 vc.copyObject(_app, vc.component.viewResourceStoreInfo);
-                vc.emit($props.callBackListener,$props.callBackFunction,vc.component.viewResourceStoreInfo);
+                vc.emit($props.callBackListener, $props.callBackFunction, vc.component.viewResourceStoreInfo);
             });
 
-            vc.on('viewResourceStoreInfo', 'onIndex', function(_index){
+            vc.on('viewResourceStoreInfo', 'onIndex', function (_index) {
                 vc.component.viewResourceStoreInfo.index = _index;
             });
 
         },
-        methods:{
+        methods: {
 
-            _openSelectResourceStoreInfoModel(){
-                vc.emit('chooseResourceStore','openChooseResourceStoreModel',{});
+            _openSelectResourceStoreInfoModel() {
+                vc.emit('chooseResourceStore', 'openChooseResourceStoreModel', {});
             },
-            _openAddResourceStoreInfoModel(){
-                vc.emit('addResourceStore','openAddResourceStoreModal',{});
+            _openAddResourceStoreInfoModel() {
+                vc.emit('addResourceStore', 'openAddResourceStoreModal', {});
             },
-            _loadResourceStoreInfoData:function(){
+            _loadResourceStoreInfoData: function () {
 
             }
         }
