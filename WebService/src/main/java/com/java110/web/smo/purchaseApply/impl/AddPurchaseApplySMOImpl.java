16a1d0b8851529e7420852108d83dca176e127f2
mrzcc <121184950@qq.com>
2020-03-05 17:07:54 +0800

    优化采购申请

--- WebService/src/main/java/com/java110/web/smo/purchaseApply/impl/AddPurchaseApplySMOImpl.java
+++ WebService/src/main/java/com/java110/web/smo/purchaseApply/impl/AddPurchaseApplySMOImpl.java
@@ -3,6 +3,7 @@ package com.java110.web.smo.purchaseApply.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.java110.core.component.AbstractComponentSMO;
 import com.java110.core.context.IPageData;
+import com.java110.entity.component.ComponentValidateResult;
 import com.java110.utils.constant.ServiceConstant;
 import com.java110.utils.util.Assert;
 import com.java110.web.smo.purchaseApply.IAddPurchaseApplySMO;
@@ -25,22 +26,19 @@ public class AddPurchaseApplySMOImpl extends AbstractComponentSMO implements IAd
     @Override
     protected void validate(IPageData pd, JSONObject paramIn) {
 
-        //super.validatePageInfo(pd);
+        Assert.hasKeyAndValue(paramIn, "description", "必填，请填申请说明");
 
-        //Assert.hasKeyAndValue(paramIn, "xxx", "xxx");
-        Assert.hasKeyAndValue(paramIn, "state", "必填，请填订单状态");
-
-
-
-        //super.checkUserHasPrivilege(pd, restTemplate, PrivilegeCodeConstant.AGENT_HAS_LIST_PURCHASEAPPLY);
+        //super.checkUserHasPrivilege(pd, restTemplate, PrivilegeCodeConstant.AGENT_HAS_LIST_PURCHASE_APPLY);
 
     }
 
     @Override
     protected ResponseEntity<String> doBusinessProcess(IPageData pd, JSONObject paramIn) {
         ResponseEntity<String> responseEntity = null;
-        super.validateStoreStaffCommunityRelationship(pd, restTemplate);
-
+        ComponentValidateResult result = super.validateStoreStaffCommunityRelationship(pd, restTemplate);
+        paramIn.put("storeId", result.getStoreId());
+        paramIn.put("userId", pd.getUserId());
+        paramIn.put("userName", pd.getUserName());
         responseEntity = this.callCenterService(restTemplate, pd, paramIn.toJSONString(),
                 ServiceConstant.SERVICE_API_URL + "/api/purchaseApply.savePurchaseApply",
                 HttpMethod.POST);
