4970addc3b5a912b2847f55de7660b5abb24f5b7
java110 <928255095@qq.com>
2020-05-18 18:04:37 +0800

    优化 小区文化图片上传功能

--- FrontService/src/main/java/com/java110/front/components/activities/AddActivitiesViewComponent.java
+++ FrontService/src/main/java/com/java110/front/components/activities/AddActivitiesViewComponent.java
@@ -1,10 +1,14 @@
 package com.java110.front.components.activities;
 
+import com.alibaba.fastjson.JSONObject;
 import com.java110.core.context.IPageData;
+import com.java110.core.context.PageData;
 import com.java110.front.smo.activities.IAddActivitiesSMO;
+import com.java110.front.smo.file.IAddFileSMO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 添加活动组件
@@ -15,6 +19,9 @@ public class AddActivitiesViewComponent {
     @Autowired
     private IAddActivitiesSMO addActivitiesSMOImpl;
 
+    @Autowired
+    private IAddFileSMO addFileSMOImpl;
+
     /**
      * 添加活动数据
      *
@@ -23,6 +30,20 @@ public class AddActivitiesViewComponent {
      */
     public ResponseEntity<String> save(IPageData pd) {
         return addActivitiesSMOImpl.saveActivities(pd);
+    }
+
+
+    /**
+     * 上传图片
+     *
+     * @param pd 页面数据封装
+     * @return ResponseEntity 对象
+     */
+    public ResponseEntity<String> uploadImage(IPageData pd, MultipartFile uploadFile) throws Exception {
+        JSONObject paramIn = JSONObject.parseObject(pd.getReqData());
+        paramIn.put("suffix", "jpeg");
+        IPageData newPd = PageData.newInstance().builder(pd.getUserId(), pd.getUserName(),pd.getToken(), paramIn.toJSONString(), pd.getComponentCode(), pd.getComponentMethod(), "", pd.getSessionId());
+        return addFileSMOImpl.saveFile(newPd, uploadFile);
     }
 
     public IAddActivitiesSMO getAddActivitiesSMOImpl() {
