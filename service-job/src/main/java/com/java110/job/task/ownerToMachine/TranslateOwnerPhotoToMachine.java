47b6d96c48b3e7c9ed73e22b206915698712cd36
java110 <928255095@qq.com>
2020-06-03 22:43:35 +0800

    加入 添加 任务和 结束任务功能

--- service-job/src/main/java/com/java110/job/task/ownerToMachine/TranslateOwnerPhotoToMachine.java
+++ service-job/src/main/java/com/java110/job/task/ownerToMachine/TranslateOwnerPhotoToMachine.java
@@ -1,12 +1,14 @@
-package com.java110.job.task;
+package com.java110.job.task.ownerToMachine;
 
 import com.java110.dto.task.TaskDto;
 import com.java110.job.quartz.TaskSystemQuartz;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
  * @ClassName TransalateOwnerPhotoToMachine
- * @Description TODO
+ * @Description TODO 传输业主图片到门禁 任务
  * @Author wuxw
  * @Date 2020/6/3 20:59
  * @Version 1.0
@@ -14,8 +16,13 @@ import org.springframework.stereotype.Component;
  **/
 @Component
 public class TranslateOwnerPhotoToMachine extends TaskSystemQuartz {
+
+    private static Logger logger = LoggerFactory.getLogger(TranslateOwnerPhotoToMachine.class);
+
     @Override
     protected void process(TaskDto taskDto) throws Exception {
+
+        logger.debug("任务在执行" + taskDto.toString());
 
     }
 }
