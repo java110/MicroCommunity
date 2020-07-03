ca4662481a96825dffab34118fbfb6b18a92923e
java110 <928255095@qq.com>
2020-07-03 19:23:50 +0800

    优化 事务

---
+++ java110-core/src/main/java/com/java110/core/annotation/Java110Transactional.java
@@ -0,0 +1,13 @@
+package com.java110.core.annotation;
+
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface Java110Transactional {
+}
