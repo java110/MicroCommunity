60ce738edcea9f34a6998822594776e0a8bce10d
java110 <928255095@qq.com>
2020-07-08 16:06:07 +0800

    优化 评价功能完成

--- service-community/src/main/java/com/java110/community/service/repair/AppraiseRepairServiceImpl.java
+++ service-community/src/main/java/com/java110/community/service/repair/AppraiseRepairServiceImpl.java
@@ -1,11 +1,14 @@
 package com.java110.community.service.repair;
 
 import com.java110.community.dao.IRepairServiceDao;
+import com.java110.community.dao.IRepairUserServiceDao;
 import com.java110.core.annotation.Java110Transactional;
 import com.java110.dto.appraise.AppraiseDto;
 import com.java110.dto.repair.RepairDto;
+import com.java110.dto.repair.RepairUserDto;
 import com.java110.intf.common.appraise.ISaveAppraiseService;
 import com.java110.intf.community.repair.IAppraiseRepairService;
+import com.java110.utils.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,16 +28,28 @@ public class AppraiseRepairServiceImpl implements IAppraiseRepairService {
     @Autowired
     private IRepairServiceDao repairServiceDaoImpl;
 
+    @Autowired
+    private IRepairUserServiceDao repairUserServiceDaoImpl;
+
     @Override
     @Java110Transactional
     public AppraiseDto appraiseRepair(@RequestBody AppraiseDto appraiseDto) {
 
-        //将工单表的状态修改为完成
+
         Map info = new HashMap();
+        info.put("repairId", appraiseDto.getObjId());
+        info.put("staffId", appraiseDto.getAppraiseUserId());
+        info.put("state", RepairUserDto.STATE_DOING);
+        info.put("endTime", DateUtil.getNow(DateUtil.DATE_FORMATE_STRING_A));
+        info.put("context", "评价完成");
+        repairUserServiceDaoImpl.updateRepairUserInfoInstance(info);
+        //将工单表的状态修改为完成
+        info = new HashMap();
         info.put("repairId", appraiseDto.getObjId());
         info.put("statusCd", "0");
         info.put("state", RepairDto.STATE_RETURN_VISIT);
         repairServiceDaoImpl.updateRepairInfoInstance(info);
+
 
         //评价表中加入评价内容
         appraiseDto.setParentAppraiseId("-1");
