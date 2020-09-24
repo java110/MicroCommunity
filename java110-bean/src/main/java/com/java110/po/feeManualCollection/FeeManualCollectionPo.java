35b62877bb0c2a60244ae8770574cf385ed611e7
java110 <928255095@qq.com>
2020-09-24 18:35:30 +0800

    优化

--- java110-bean/src/main/java/com/java110/po/feeManualCollection/FeeManualCollectionPo.java
+++ java110-bean/src/main/java/com/java110/po/feeManualCollection/FeeManualCollectionPo.java
@@ -1,95 +1,117 @@
 package com.java110.po.feeManualCollection;
 
 import java.io.Serializable;
-import java.util.Date;
 
 public class FeeManualCollectionPo implements Serializable {
 
     private String squarePrice;
-private String ownerName;
-private String link;
-private String roomArea;
-private String remark;
-private String statusCd = "0";
-private String state;
-private String ownerId;
-private String communityId;
-private String collectionId;
-private String roomId;
-private String roomName;
-public String getSquarePrice() {
+    private String ownerName;
+    private String link;
+    private String roomArea;
+    private String remark;
+    private String statusCd = "0";
+    private String state;
+    private String ownerId;
+    private String communityId;
+    private String collectionId;
+    private String roomId;
+    private String roomName;
+
+    public String getSquarePrice() {
         return squarePrice;
     }
-public void setSquarePrice(String squarePrice) {
+
+    public void setSquarePrice(String squarePrice) {
         this.squarePrice = squarePrice;
     }
-public String getOwnerName() {
+
+    public String getOwnerName() {
         return ownerName;
     }
-public void setOwnerName(String ownerName) {
+
+    public void setOwnerName(String ownerName) {
         this.ownerName = ownerName;
     }
-public String getLink() {
+
+    public String getLink() {
         return link;
     }
-public void setLink(String link) {
+
+    public void setLink(String link) {
         this.link = link;
     }
-public String getRoomArea() {
+
+    public String getRoomArea() {
         return roomArea;
     }
-public void setRoomArea(String roomArea) {
+
+    public void setRoomArea(String roomArea) {
         this.roomArea = roomArea;
     }
-public String getRemark() {
+
+    public String getRemark() {
         return remark;
     }
-public void setRemark(String remark) {
+
+    public void setRemark(String remark) {
         this.remark = remark;
     }
-public String getStatusCd() {
+
+    public String getStatusCd() {
         return statusCd;
     }
-public void setStatusCd(String statusCd) {
+
+    public void setStatusCd(String statusCd) {
         this.statusCd = statusCd;
     }
-public String getState() {
+
+    public String getState() {
         return state;
     }
-public void setState(String state) {
+
+    public void setState(String state) {
         this.state = state;
     }
-public String getOwnerId() {
+
+    public String getOwnerId() {
         return ownerId;
     }
-public void setOwnerId(String ownerId) {
+
+    public void setOwnerId(String ownerId) {
         this.ownerId = ownerId;
     }
-public String getCommunityId() {
+
+    public String getCommunityId() {
         return communityId;
     }
-public void setCommunityId(String communityId) {
+
+    public void setCommunityId(String communityId) {
         this.communityId = communityId;
     }
-public String getCollectionId() {
+
+    public String getCollectionId() {
         return collectionId;
     }
-public void setCollectionId(String collectionId) {
+
+    public void setCollectionId(String collectionId) {
         this.collectionId = collectionId;
     }
-public String getRoomId() {
+
+    public String getRoomId() {
         return roomId;
     }
-public void setRoomId(String roomId) {
+
+    public void setRoomId(String roomId) {
         this.roomId = roomId;
     }
-public String getRoomName() {
+
+    public String getRoomName() {
         return roomName;
     }
-public void setRoomName(String roomName) {
+
+    public void setRoomName(String roomName) {
         this.roomName = roomName;
     }
-
 
 
 }
