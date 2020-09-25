2117a61045086f4ac85b55ba842481f05ad6a3b7
java110 <928255095@qq.com>
2020-09-25 11:34:43 +0800

    优化代码

--- service-front/src/main/java/com/java110/front/configuration/ServiceConfiguration.java
+++ service-front/src/main/java/com/java110/front/configuration/ServiceConfiguration.java
@@ -25,6 +25,7 @@ public class ServiceConfiguration {
         exclusions.append("/callComponent/download/getFile/fileByObjId,");//放开 下载图片也不需要登录
         exclusions.append("/app/payment/notify,");//微信支付通知
         exclusions.append("/app/payment/rentingNotify,");//微信支付通知
+        exclusions.append("/app/payment/oweFeeToPay,");//欠费微信支付通知
         exclusions.append("/app/loginWx,");// 登录跳过
         exclusions.append("/app/getWxPhoto,");// 登录跳过
         exclusions.append("/app/loginProperty,");// 物业APP登录跳过
