4c88609b0955dbeef56ee1a257e9c42fd681fe04
java110 <928255095@qq.com>
2020-10-21 12:33:14 +0800

    优化代码

--- service-goods/src/main/java/com/java110/goods/bmo/groupBuySetting/impl/SaveGroupBuySettingBMOImpl.java
+++ service-goods/src/main/java/com/java110/goods/bmo/groupBuySetting/impl/SaveGroupBuySettingBMOImpl.java
@@ -3,18 +3,26 @@ package com.java110.goods.bmo.groupBuySetting.impl;
 import com.java110.core.annotation.Java110Transactional;
 import com.java110.core.factory.GenerateCodeFactory;
 import com.java110.goods.bmo.groupBuySetting.ISaveGroupBuySettingBMO;
+import com.java110.intf.IGroupBuyBatchInnerServiceSMO;
 import com.java110.intf.IGroupBuySettingInnerServiceSMO;
+import com.java110.po.groupBuyBatch.GroupBuyBatchPo;
 import com.java110.po.groupBuySetting.GroupBuySettingPo;
+import com.java110.utils.util.DateUtil;
 import com.java110.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
 
 @Service("saveGroupBuySettingBMOImpl")
 public class SaveGroupBuySettingBMOImpl implements ISaveGroupBuySettingBMO {
 
     @Autowired
     private IGroupBuySettingInnerServiceSMO groupBuySettingInnerServiceSMOImpl;
+
+    @Autowired
+    private IGroupBuyBatchInnerServiceSMO groupBuyBatchInnerServiceSMOImpl;
 
     /**
      * 添加小区信息
@@ -28,11 +36,25 @@ public class SaveGroupBuySettingBMOImpl implements ISaveGroupBuySettingBMO {
         groupBuySettingPo.setSettingId(GenerateCodeFactory.getGeneratorId(GenerateCodeFactory.CODE_PREFIX_settingId));
         int flag = groupBuySettingInnerServiceSMOImpl.saveGroupBuySetting(groupBuySettingPo);
 
-        if (flag > 0) {
-            return ResultVo.createResponseEntity(ResultVo.CODE_OK, "保存成功");
+        if (flag < 1) {
+            return ResultVo.createResponseEntity(ResultVo.CODE_ERROR, "保存失败");
         }
 
-        return ResultVo.createResponseEntity(ResultVo.CODE_ERROR, "保存失败");
+        GroupBuyBatchPo groupBuyBatchPo = new GroupBuyBatchPo();
+        groupBuyBatchPo.setStoreId(groupBuySettingPo.getStoreId());
+        groupBuyBatchPo.setSettingId(groupBuySettingPo.getSettingId());
+        groupBuyBatchPo.setBatchId(GenerateCodeFactory.getGeneratorId(GenerateCodeFactory.CODE_PREFIX_batchId));
+        groupBuyBatchPo.setBatchStartTime(DateUtil.getFormatTimeString(DateUtil.getCurrentDate(), DateUtil.DATE_FORMATE_STRING_A));
+        groupBuyBatchPo.setCurBatch("Y");
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR_OF_DAY, Integer.parseInt(groupBuySettingPo.getValidHours()));
+        groupBuyBatchPo.setBatchEndTime(DateUtil.getFormatTimeString(calendar.getTime(), DateUtil.DATE_FORMATE_STRING_A));
+        flag = groupBuyBatchInnerServiceSMOImpl.saveGroupBuyBatch(groupBuyBatchPo);
+
+        if (flag < 0) {
+            return ResultVo.createResponseEntity(ResultVo.CODE_ERROR, "保存失败");
+        }
+        return ResultVo.createResponseEntity(ResultVo.CODE_OK, "保存成功");
     }
 
 }
--- service-goods/src/main/java/com/java110/goods/bmo/groupBuySetting/impl/UpdateGroupBuySettingBMOImpl.java
+++ service-goods/src/main/java/com/java110/goods/bmo/groupBuySetting/impl/UpdateGroupBuySettingBMOImpl.java
@@ -1,19 +1,28 @@
 package com.java110.goods.bmo.groupBuySetting.impl;
 
 import com.java110.core.annotation.Java110Transactional;
+import com.java110.core.factory.GenerateCodeFactory;
+import com.java110.dto.groupBuyBatch.GroupBuyBatchDto;
 import com.java110.goods.bmo.groupBuySetting.IUpdateGroupBuySettingBMO;
+import com.java110.intf.IGroupBuyBatchInnerServiceSMO;
 import com.java110.intf.IGroupBuySettingInnerServiceSMO;
+import com.java110.po.groupBuyBatch.GroupBuyBatchPo;
 import com.java110.po.groupBuySetting.GroupBuySettingPo;
+import com.java110.utils.util.DateUtil;
 import com.java110.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
 
 @Service("updateGroupBuySettingBMOImpl")
 public class UpdateGroupBuySettingBMOImpl implements IUpdateGroupBuySettingBMO {
 
     @Autowired
     private IGroupBuySettingInnerServiceSMO groupBuySettingInnerServiceSMOImpl;
+    @Autowired
+    private IGroupBuyBatchInnerServiceSMO groupBuyBatchInnerServiceSMOImpl;
 
     /**
      * @param groupBuySettingPo
@@ -24,11 +33,33 @@ public class UpdateGroupBuySettingBMOImpl implements IUpdateGroupBuySettingBMO {
 
         int flag = groupBuySettingInnerServiceSMOImpl.updateGroupBuySetting(groupBuySettingPo);
 
-        if (flag > 0) {
-            return ResultVo.createResponseEntity(ResultVo.CODE_OK, "保存成功");
+        if (flag < 1) {
+            return ResultVo.createResponseEntity(ResultVo.CODE_ERROR, "保存失败");
         }
 
-        return ResultVo.createResponseEntity(ResultVo.CODE_ERROR, "保存失败");
+        GroupBuyBatchDto groupBuyBatchDto = new GroupBuyBatchDto();
+        groupBuyBatchDto.setStoreId(groupBuySettingPo.getStoreId());
+        groupBuyBatchDto.setSettingId(groupBuySettingPo.getSettingId());
+        int count = groupBuyBatchInnerServiceSMOImpl.queryGroupBuyBatchsCount(groupBuyBatchDto);
+
+        if (count < 1) {
+            GroupBuyBatchPo groupBuyBatchPo = new GroupBuyBatchPo();
+            groupBuyBatchPo.setStoreId(groupBuySettingPo.getStoreId());
+            groupBuyBatchPo.setSettingId(groupBuySettingPo.getSettingId());
+            groupBuyBatchPo.setBatchId(GenerateCodeFactory.getGeneratorId(GenerateCodeFactory.CODE_PREFIX_batchId));
+            groupBuyBatchPo.setBatchStartTime(DateUtil.getFormatTimeString(DateUtil.getCurrentDate(), DateUtil.DATE_FORMATE_STRING_A));
+            groupBuyBatchPo.setCurBatch("Y");
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.HOUR_OF_DAY, Integer.parseInt(groupBuySettingPo.getValidHours()));
+            groupBuyBatchPo.setBatchEndTime(DateUtil.getFormatTimeString(calendar.getTime(), DateUtil.DATE_FORMATE_STRING_A));
+            flag = groupBuyBatchInnerServiceSMOImpl.saveGroupBuyBatch(groupBuyBatchPo);
+
+            if (flag < 0) {
+                return ResultVo.createResponseEntity(ResultVo.CODE_ERROR, "保存失败");
+            }
+        }
+        return ResultVo.createResponseEntity(ResultVo.CODE_OK, "保存成功");
+
     }
 
 }
