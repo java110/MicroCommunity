4dcdb85164dfb6d94408dd5a72a17e3b7c80fece
java110 <928255095@qq.com>
2020-11-21 16:19:00 +0800

    优化代码

--- service-user/src/main/java/com/java110/user/bmo/owner/impl/ComprehensiveQueryImpl.java
+++ service-user/src/main/java/com/java110/user/bmo/owner/impl/ComprehensiveQueryImpl.java
@@ -95,7 +95,7 @@ public class ComprehensiveQueryImpl implements IComprehensiveQuery {
         ownerDto.setCommunityId(communityId);
         ownerDto.setIdCard(searchValue);
         ownerDto.setOwnerTypeCds(new String[]{OwnerDto.OWNER_TYPE_CD_MEMBER, OwnerDto.OWNER_TYPE_CD_RENTING});
-        List<OwnerDto> ownerDtos = ownerInnerServiceSMOImpl.queryOwners(ownerDto);
+        List<OwnerDto> ownerDtos = ownerInnerServiceSMOImpl.queryOwnerMembers(ownerDto);
         Assert.listOnlyOne(ownerDtos, "未找到成员信息或者查询到多条，请换其他条件查询");
 
         return queryByOwnerId(communityId, ownerDtos.get(0).getOwnerId());
@@ -114,7 +114,7 @@ public class ComprehensiveQueryImpl implements IComprehensiveQuery {
         ownerDto.setCommunityId(communityId);
         ownerDto.setLink(searchValue);
         ownerDto.setOwnerTypeCds(new String[]{OwnerDto.OWNER_TYPE_CD_MEMBER, OwnerDto.OWNER_TYPE_CD_RENTING});
-        List<OwnerDto> ownerDtos = ownerInnerServiceSMOImpl.queryOwners(ownerDto);
+        List<OwnerDto> ownerDtos = ownerInnerServiceSMOImpl.queryOwnerMembers(ownerDto);
         Assert.listOnlyOne(ownerDtos, "未找到成员信息或者查询到多条，请换其他条件查询");
 
         return queryByOwnerId(communityId, ownerDtos.get(0).getOwnerId());
@@ -133,7 +133,7 @@ public class ComprehensiveQueryImpl implements IComprehensiveQuery {
         ownerDto.setCommunityId(communityId);
         ownerDto.setName(searchValue);
         ownerDto.setOwnerTypeCds(new String[]{OwnerDto.OWNER_TYPE_CD_MEMBER, OwnerDto.OWNER_TYPE_CD_RENTING});
-        List<OwnerDto> ownerDtos = ownerInnerServiceSMOImpl.queryOwners(ownerDto);
+        List<OwnerDto> ownerDtos = ownerInnerServiceSMOImpl.queryOwnerMembers(ownerDto);
         Assert.listOnlyOne(ownerDtos, "未找到成员信息或者查询到多条，请换其他条件查询");
 
         return queryByOwnerId(communityId, ownerDtos.get(0).getOwnerId());
