d559f7d36dbd6784b12ef68b717f36f68ca6a550
java110 <928255095@qq.com>
2020-12-11 14:48:01 +0800

    优化月份错误

--- service-job/src/main/java/com/java110/job/task/wechat/PublicWeChatPushMessageTemplate.java
+++ service-job/src/main/java/com/java110/job/task/wechat/PublicWeChatPushMessageTemplate.java
@@ -170,7 +170,7 @@ public class PublicWeChatPushMessageTemplate extends TaskSystemQuartz {
                     /*data.setFirst(new Content("物业费缴费提醒"));*/
                     data.setFirst(new Content(fee.getFeeTypeName() + "提醒"));
                     data.setKeyword1(new Content(fee.getPayerObjName()));
-                    data.setKeyword2(new Content(year + "年-" + month + "月"));
+                    data.setKeyword2(new Content(year + "年-" + (month + 1) + "月"));
                     data.setKeyword3(new Content(fee.getAmountOwed()));
                     data.setRemark(new Content("请您及时缴费,如有疑问请联系相关物业人员"));
                     if (!StringUtil.isEmpty(oweUrl)) {
