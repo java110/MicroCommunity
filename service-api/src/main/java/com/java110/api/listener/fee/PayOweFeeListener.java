f72460742150ad8507fe142a9562261db8e8e1bf
java110 <928255095@qq.com>
2020-12-23 11:30:35 +0800

    加入 支付类型

--- service-api/src/main/java/com/java110/api/listener/fee/PayOweFeeListener.java
+++ service-api/src/main/java/com/java110/api/listener/fee/PayOweFeeListener.java
@@ -132,6 +132,8 @@ public class PayOweFeeListener extends AbstractServiceApiDataFlowListener {
     }
 
     private void getFeeReceiptDetailPo(DataFlowContext dataFlowContext, JSONObject paramObj, JSONArray businesses, List<FeeReceiptDetailPo> feeReceiptDetailPos, List<FeeReceiptPo> feeReceiptPos) {
+        paramObj.put("primeRate", "6");
+        paramObj.put("remark", "微信支付");
         businesses.add(feeBMOImpl.addOweFeeDetail(paramObj, dataFlowContext, feeReceiptDetailPos, feeReceiptPos));
         businesses.add(feeBMOImpl.modifyOweFee(paramObj, dataFlowContext));
 
