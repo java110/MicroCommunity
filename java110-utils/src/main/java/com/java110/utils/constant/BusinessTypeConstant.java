df085766a573609df8412781f6c71c9cfee37e3b
java110 <928255095@qq.com>
2021-01-09 11:55:44 +0800

    优化代码

--- java110-utils/src/main/java/com/java110/utils/constant/BusinessTypeConstant.java
+++ java110-utils/src/main/java/com/java110/utils/constant/BusinessTypeConstant.java
@@ -1254,5 +1254,23 @@ public class BusinessTypeConstant {
      */
     public static final String BUSINESS_TYPE_DELETE_TEMP_CAR_FEE_CONFIG_INFO="651100050001";
 
+    /**
+     *  保存工作流节点 处理员工 ORDER_FEE_SERVICE_URL
+     *  3保存 feeServiceTopic
+     */
+    public static final String BUSINESS_TYPE_SAVE_TEMP_CAR_FEE_CONFIG_ATTR_INFO="651100030002";
+
+
+    /**
+     *  修改工作流节点 处理员工
+     *  3保存
+     */
+    public static final String BUSINESS_TYPE_UPDATE_TEMP_CAR_FEE_CONFIG_ATTR_INFO="651100040002";
+
+    /**
+     * 删除工作流节点 处理员工
+     */
+    public static final String BUSINESS_TYPE_DELETE_TEMP_CAR_FEE_CONFIG_ATTR_INFO="651100050002";
+
 
 }
