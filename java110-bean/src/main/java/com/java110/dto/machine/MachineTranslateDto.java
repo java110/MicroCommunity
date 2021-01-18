0680863b1134ba515436f67c4e88fa6e29e067a5
java110 <928255095@qq.com>
2021-01-18 10:25:44 +0800

    加入 临时车费用同步物联网

--- java110-bean/src/main/java/com/java110/dto/machine/MachineTranslateDto.java
+++ java110-bean/src/main/java/com/java110/dto/machine/MachineTranslateDto.java
@@ -44,6 +44,9 @@ public class MachineTranslateDto extends PageDto implements Serializable {
     public static final String CMD_DELETE_OWNER_CAR = "703";
     public static final String CMD_ADD_CAR_BLACK_WHITE = "801";
     public static final String CMD_DELETE_CAR_BLACK_WHITE = "803";
+    public static final String CMD_ADD_TEAM_CAR_FEE_CONFIG = "901";
+    public static final String CMD_UPDATE_TEAM_CAR_FEE_CONFIG = "902";
+    public static final String CMD_DELETE_TEAM_CAR_FEE_CONFIG = "903";
     public static final String CMD_OPEN_DOOR = "5";
 
     //小区信息
@@ -52,6 +55,7 @@ public class MachineTranslateDto extends PageDto implements Serializable {
     public static final String TYPE_OWNER = "8899";
     public static final String TYPE_PARKING_AREA = "2233";
     public static final String TYPE_OWNER_CAR = "4455";
+    public static final String TYPE_TEAM_CAR_FEE_CONFIG = "1122";
 
     //同步状态
     public static final String STATE_SUCCESS = "20000";
---
+++ service-job/src/main/java/com/java110/job/adapt/hcIot/tempCarFee/DeleteTempCarFeeConfigToIotAdapt.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2017-2020 吴学文 and java110 team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.java110.job.adapt.hcIot.tempCarFee;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.java110.dto.tempCarFeeConfig.TempCarFeeConfigDto;
+import com.java110.entity.order.Business;
+import com.java110.intf.community.IParkingSpaceInnerServiceSMO;
+import com.java110.intf.fee.ITempCarFeeConfigInnerServiceSMO;
+import com.java110.job.adapt.DatabusAdaptImpl;
+import com.java110.job.adapt.hcIot.asyn.IIotSendAsyn;
+import com.java110.po.tempCarFeeConfig.TempCarFeeConfigPo;
+import com.java110.utils.constant.StatusConstant;
+import com.java110.utils.util.Assert;
+import com.java110.utils.util.BeanConvertUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * HC iot 删除名单同步iot
+ *
+ * <p>
+ * 接口协议地址： https://gitee.com/java110/MicroCommunityThings/blob/master/back/docs/api.md
+ *
+ * @desc add by 吴学文 18:58
+ */
+@Component(value = "deleteTempCarFeeConfigToIotAdapt")
+public class DeleteTempCarFeeConfigToIotAdapt extends DatabusAdaptImpl {
+
+    @Autowired
+    private IIotSendAsyn hcTempCarFeeConfigAsynImpl;
+
+
+    @Autowired
+    private IParkingSpaceInnerServiceSMO parkingSpaceInnerServiceSMOImpl;
+
+    @Autowired
+    private ITempCarFeeConfigInnerServiceSMO tempCarFeeConfigInnerServiceSMOImpl;
+
+    /**
+     * {
+     * "extTempCarFeeConfigId": "702020042194860037"
+     * }
+     *
+     * @param business   当前处理业务
+     * @param businesses 所有业务信息
+     */
+    @Override
+    public void execute(Business business, List<Business> businesses) {
+        JSONObject data = business.getData();
+        if (data.containsKey(TempCarFeeConfigPo.class.getSimpleName())) {
+            Object bObj = data.get(TempCarFeeConfigPo.class.getSimpleName());
+            JSONArray businessTempCarFeeConfigs = null;
+            if (bObj instanceof JSONObject) {
+                businessTempCarFeeConfigs = new JSONArray();
+                businessTempCarFeeConfigs.add(bObj);
+            } else if (bObj instanceof List) {
+                businessTempCarFeeConfigs = JSONArray.parseArray(JSONObject.toJSONString(bObj));
+            } else {
+                businessTempCarFeeConfigs = (JSONArray) bObj;
+            }
+            //JSONObject businessTempCarFeeConfig = data.getJSONObject("businessTempCarFeeConfig");
+            for (int bTempCarFeeConfigIndex = 0; bTempCarFeeConfigIndex < businessTempCarFeeConfigs.size(); bTempCarFeeConfigIndex++) {
+                JSONObject businessTempCarFeeConfig = businessTempCarFeeConfigs.getJSONObject(bTempCarFeeConfigIndex);
+                doSendTempCarFeeConfig(business, businessTempCarFeeConfig);
+            }
+        }
+    }
+
+    private void doSendTempCarFeeConfig(Business business, JSONObject businessTempCarFeeConfig) {
+        TempCarFeeConfigPo tempCarFeeConfigPo = BeanConvertUtil.covertBean(businessTempCarFeeConfig, TempCarFeeConfigPo.class);
+        TempCarFeeConfigDto tempCarFeeConfigDto = new TempCarFeeConfigDto();
+        tempCarFeeConfigDto.setConfigId(tempCarFeeConfigPo.getConfigId());
+        tempCarFeeConfigDto.setStatusCd(StatusConstant.STATUS_CD_INVALID);
+        List<TempCarFeeConfigDto> tempCarFeeConfigDtos = tempCarFeeConfigInnerServiceSMOImpl.queryTempCarFeeConfigs(tempCarFeeConfigDto);
+        Assert.listOnlyOne(tempCarFeeConfigDtos, "未找到临时车收费标准");
+
+        JSONObject postParameters = new JSONObject();
+        postParameters.put("extConfigId", tempCarFeeConfigDtos.get(0).getConfigId());
+        postParameters.put("extPaId", tempCarFeeConfigDtos.get(0).getPaId());
+        postParameters.put("extCommunityId", tempCarFeeConfigDtos.get(0).getCommunityId());
+        hcTempCarFeeConfigAsynImpl.deleteTempCarFeeConfig(postParameters);
+    }
+}
