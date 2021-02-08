ac0558fd1b4662318d3ffef35b19db8526ea3db7
java110 <928255095@qq.com>
2021-02-08 10:27:49 +0800

    添加最美员工功能

--- service-user/src/main/java/com/java110/user/api/ActivitiesRuleApi.java
+++ service-user/src/main/java/com/java110/user/api/ActivitiesRuleApi.java
@@ -1,9 +1,15 @@
 package com.java110.user.api;
 
 import com.alibaba.fastjson.JSONObject;
+import com.java110.dto.activitiesBeautifulStaff.ActivitiesBeautifulStaffDto;
 import com.java110.dto.activitiesRule.ActivitiesRuleDto;
 import com.java110.dto.questionAnswer.QuestionAnswerDto;
+import com.java110.po.activitiesBeautifulStaff.ActivitiesBeautifulStaffPo;
 import com.java110.po.activitiesRule.ActivitiesRulePo;
+import com.java110.user.bmo.activitiesBeautifulStaff.IDeleteActivitiesBeautifulStaffBMO;
+import com.java110.user.bmo.activitiesBeautifulStaff.IGetActivitiesBeautifulStaffBMO;
+import com.java110.user.bmo.activitiesBeautifulStaff.ISaveActivitiesBeautifulStaffBMO;
+import com.java110.user.bmo.activitiesBeautifulStaff.IUpdateActivitiesBeautifulStaffBMO;
 import com.java110.user.bmo.activitiesRule.IDeleteActivitiesRuleBMO;
 import com.java110.user.bmo.activitiesRule.IGetActivitiesRuleBMO;
 import com.java110.user.bmo.activitiesRule.ISaveActivitiesRuleBMO;
@@ -32,6 +38,17 @@ public class ActivitiesRuleApi {
 
     @Autowired
     private IGetActivitiesRuleBMO getActivitiesRuleBMOImpl;
+
+    @Autowired
+    private ISaveActivitiesBeautifulStaffBMO saveActivitiesBeautifulStaffBMOImpl;
+    @Autowired
+    private IUpdateActivitiesBeautifulStaffBMO updateActivitiesBeautifulStaffBMOImpl;
+    @Autowired
+    private IDeleteActivitiesBeautifulStaffBMO deleteActivitiesBeautifulStaffBMOImpl;
+
+    @Autowired
+    private IGetActivitiesBeautifulStaffBMO getActivitiesBeautifulStaffBMOImpl;
+
 
     /**
      * 微信保存消息模板
@@ -130,5 +147,88 @@ public class ActivitiesRuleApi {
         activitiesRuleDto.setRow(row);
         activitiesRuleDto.setObjIds(new String[]{storeId, communityId});
         return getActivitiesRuleBMOImpl.get(activitiesRuleDto);
+    }
+
+
+
+    /**
+     * 微信保存消息模板
+     *
+     * @param reqJson
+     * @return
+     * @serviceCode /activitiesRule/saveActivitiesBeautifulStaff
+     * @path /app/activitiesRule/saveActivitiesBeautifulStaff
+     */
+    @RequestMapping(value = "/saveActivitiesBeautifulStaff", method = RequestMethod.POST)
+    public ResponseEntity<String> saveActivitiesBeautifulStaff(@RequestBody JSONObject reqJson) {
+
+        Assert.hasKeyAndValue(reqJson, "staffId", "请求报文中未包含staffId");
+        Assert.hasKeyAndValue(reqJson, "activitiesNum", "请求报文中未包含activitiesNum");
+        Assert.hasKeyAndValue(reqJson, "workContent", "请求报文中未包含workContent");
+        Assert.hasKeyAndValue(reqJson, "ruleId", "请求报文中未包含ruleId");
+
+
+        ActivitiesBeautifulStaffPo activitiesBeautifulStaffPo = BeanConvertUtil.covertBean(reqJson, ActivitiesBeautifulStaffPo.class);
+        return saveActivitiesBeautifulStaffBMOImpl.save(activitiesBeautifulStaffPo);
+    }
+
+    /**
+     * 微信修改消息模板
+     *
+     * @param reqJson
+     * @return
+     * @serviceCode /activitiesRule/updateActivitiesBeautifulStaff
+     * @path /app/activitiesRule/updateActivitiesBeautifulStaff
+     */
+    @RequestMapping(value = "/updateActivitiesBeautifulStaff", method = RequestMethod.POST)
+    public ResponseEntity<String> updateActivitiesBeautifulStaff(@RequestBody JSONObject reqJson) {
+
+        Assert.hasKeyAndValue(reqJson, "staffId", "请求报文中未包含staffId");
+        Assert.hasKeyAndValue(reqJson, "activitiesNum", "请求报文中未包含activitiesNum");
+        Assert.hasKeyAndValue(reqJson, "workContent", "请求报文中未包含workContent");
+        Assert.hasKeyAndValue(reqJson, "ruleId", "请求报文中未包含ruleId");
+        Assert.hasKeyAndValue(reqJson, "beId", "beId不能为空");
+
+
+        ActivitiesBeautifulStaffPo activitiesBeautifulStaffPo = BeanConvertUtil.covertBean(reqJson, ActivitiesBeautifulStaffPo.class);
+        return updateActivitiesBeautifulStaffBMOImpl.update(activitiesBeautifulStaffPo);
+    }
+
+    /**
+     * 微信删除消息模板
+     *
+     * @param reqJson
+     * @return
+     * @serviceCode /activitiesRule/deleteActivitiesBeautifulStaff
+     * @path /app/activitiesRule/deleteActivitiesBeautifulStaff
+     */
+    @RequestMapping(value = "/deleteActivitiesBeautifulStaff", method = RequestMethod.POST)
+    public ResponseEntity<String> deleteActivitiesBeautifulStaff(@RequestBody JSONObject reqJson) {
+        Assert.hasKeyAndValue(reqJson, "communityId", "小区ID不能为空");
+
+        Assert.hasKeyAndValue(reqJson, "beId", "beId不能为空");
+
+
+        ActivitiesBeautifulStaffPo activitiesBeautifulStaffPo = BeanConvertUtil.covertBean(reqJson, ActivitiesBeautifulStaffPo.class);
+        return deleteActivitiesBeautifulStaffBMOImpl.delete(activitiesBeautifulStaffPo);
+    }
+
+    /**
+     * 微信删除消息模板
+     *
+     * @param storeId 小区ID
+     * @return
+     * @serviceCode /activitiesRule/queryActivitiesBeautifulStaff
+     * @path /app/activitiesRule/queryActivitiesBeautifulStaff
+     */
+    @RequestMapping(value = "/queryActivitiesBeautifulStaff", method = RequestMethod.GET)
+    public ResponseEntity<String> queryActivitiesBeautifulStaff(@RequestHeader(value = "store-id", required = false) String storeId,
+                                                                @RequestParam(value = "page") int page,
+                                                                @RequestParam(value = "row") int row) {
+        ActivitiesBeautifulStaffDto activitiesBeautifulStaffDto = new ActivitiesBeautifulStaffDto();
+        activitiesBeautifulStaffDto.setPage(page);
+        activitiesBeautifulStaffDto.setRow(row);
+        activitiesBeautifulStaffDto.setStoreId(storeId);
+        return getActivitiesBeautifulStaffBMOImpl.get(activitiesBeautifulStaffDto);
     }
 }
