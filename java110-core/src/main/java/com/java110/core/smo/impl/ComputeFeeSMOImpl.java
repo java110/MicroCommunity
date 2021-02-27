b10c12404249dd43687f1d7ba2ef2fba613bff20
java110 <928255095@qq.com>
2021-02-27 17:08:34 +0800

    优化代码

--- java110-core/src/main/java/com/java110/core/smo/impl/ComputeFeeSMOImpl.java
+++ java110-core/src/main/java/com/java110/core/smo/impl/ComputeFeeSMOImpl.java
@@ -66,7 +66,7 @@ public class ComputeFeeSMOImpl implements IComputeFeeSMO {
     @Autowired(required = false)
     private IParkingSpaceInnerServiceSMO parkingSpaceInnerServiceSMOImpl;
 
-    @Autowired
+    @Autowired(required = false)
     private ICommunityInnerServiceSMO communityInnerServiceSMOImpl;
 
     @Override
