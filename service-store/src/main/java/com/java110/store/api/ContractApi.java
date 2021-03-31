7f4a0efeeb68bc5dfb79101dee4b791500bad9e4
java110 <928255095@qq.com>
2021-03-31 22:05:22 +0800

    优化代码

--- service-store/src/main/java/com/java110/store/api/ContractApi.java
+++ service-store/src/main/java/com/java110/store/api/ContractApi.java
@@ -54,7 +54,12 @@ import com.java110.utils.util.BeanConvertUtil;
 import com.java110.utils.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -209,6 +214,25 @@ public class ContractApi {
         return updateContractBMOImpl.update(contractPo, reqJson);
     }
 
+
+    /**
+     * 结束合同
+     *
+     * @param reqJson
+     * @return
+     * @serviceCode /contract/stopContract
+     * @path /app/contract/stopContract
+     */
+    @RequestMapping(value = "/stopContract", method = RequestMethod.POST)
+    public ResponseEntity<String> stopContract(@RequestBody JSONObject reqJson) {
+
+        Assert.hasKeyAndValue(reqJson, "contractId", "contractId不能为空");
+
+        ContractPo contractPo = BeanConvertUtil.covertBean(reqJson, ContractPo.class);
+        contractPo.setState(ContractDto.STATE_COMPLAINT);
+        return updateContractBMOImpl.update(contractPo, reqJson);
+    }
+
     /**
      * 微信修改消息模板
      *
@@ -228,6 +252,7 @@ public class ContractApi {
         return updateContractBMOImpl.update(contractPo, reqJson);
     }
 
+
     /**
      * 微信删除消息模板
      *
@@ -246,6 +271,7 @@ public class ContractApi {
         ContractPo contractPo = BeanConvertUtil.covertBean(reqJson, ContractPo.class);
         return deleteContractBMOImpl.delete(contractPo);
     }
+
 
     /**
      * 微信删除消息模板
