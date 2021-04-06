77b16a0550e5369bd3a5cb02da86ce8d173b4d06
java110 <928255095@qq.com>
2021-04-06 11:46:33 +0800

    优化代码

--- service-community/src/main/java/com/java110/community/bmo/repairReturnVisit/impl/SaveRepairReturnVisitBMOImpl.java
+++ service-community/src/main/java/com/java110/community/bmo/repairReturnVisit/impl/SaveRepairReturnVisitBMOImpl.java
@@ -3,7 +3,10 @@ package com.java110.community.bmo.repairReturnVisit.impl;
 import com.java110.community.bmo.repairReturnVisit.ISaveRepairReturnVisitBMO;
 import com.java110.core.annotation.Java110Transactional;
 import com.java110.core.factory.GenerateCodeFactory;
+import com.java110.dto.repair.RepairDto;
+import com.java110.intf.community.IRepairInnerServiceSMO;
 import com.java110.intf.community.IRepairReturnVisitInnerServiceSMO;
+import com.java110.po.owner.RepairPoolPo;
 import com.java110.po.repairReturnVisit.RepairReturnVisitPo;
 import com.java110.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,9 @@ public class SaveRepairReturnVisitBMOImpl implements ISaveRepairReturnVisitBMO {
 
     @Autowired
     private IRepairReturnVisitInnerServiceSMO repairReturnVisitInnerServiceSMOImpl;
+
+    @Autowired
+    private IRepairInnerServiceSMO repairInnerServiceSMOImpl;
 
     /**
      * 添加小区信息
@@ -28,11 +34,21 @@ public class SaveRepairReturnVisitBMOImpl implements ISaveRepairReturnVisitBMO {
         repairReturnVisitPo.setVisitId(GenerateCodeFactory.getGeneratorId(GenerateCodeFactory.CODE_PREFIX_visitId));
         int flag = repairReturnVisitInnerServiceSMOImpl.saveRepairReturnVisit(repairReturnVisitPo);
 
-        if (flag > 0) {
-            return ResultVo.createResponseEntity(ResultVo.CODE_OK, "保存成功");
+        if (flag < 0) {
+            return ResultVo.createResponseEntity(ResultVo.CODE_ERROR, "保存失败");
         }
 
-        return ResultVo.createResponseEntity(ResultVo.CODE_ERROR, "保存失败");
+        RepairPoolPo repairPoolPo = new RepairPoolPo();
+        repairPoolPo.setRepairId(repairReturnVisitPo.getRepairId());
+        repairPoolPo.setState(RepairDto.STATE_COMPLATE);
+        flag = repairInnerServiceSMOImpl.updateRepair(repairPoolPo);
+
+        if (flag < 0) {
+            return ResultVo.createResponseEntity(ResultVo.CODE_ERROR, "保存失败");
+        }
+
+        return ResultVo.createResponseEntity(ResultVo.CODE_OK, "保存成功");
+
     }
 
 }
