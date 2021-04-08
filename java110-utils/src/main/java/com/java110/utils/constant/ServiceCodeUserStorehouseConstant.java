6dfb563bbef429681c6597bae17e8ce1bace16da
java110 <928255095@qq.com>
2021-04-08 21:17:24 +0800

    优化查询我的物品代码

--- java110-utils/src/main/java/com/java110/utils/constant/ServiceCodeUserStorehouseConstant.java
+++ java110-utils/src/main/java/com/java110/utils/constant/ServiceCodeUserStorehouseConstant.java
@@ -9,23 +9,23 @@ public class ServiceCodeUserStorehouseConstant {
     /**
      * 添加 个人物品
      */
-    public static final String ADD_USERSTOREHOUSE = "userStorehouse.saveUserStorehouse";
+    public static final String ADD_USERSTOREHOUSE = "resourceStore.saveUserStorehouse";
 
 
     /**
      * 修改 个人物品
      */
-    public static final String UPDATE_USERSTOREHOUSE = "userStorehouse.updateUserStorehouse";
+    public static final String UPDATE_USERSTOREHOUSE = "resourceStore.updateUserStorehouse";
     /**
      * 删除 个人物品
      */
-    public static final String DELETE_USERSTOREHOUSE = "userStorehouse.deleteUserStorehouse";
+    public static final String DELETE_USERSTOREHOUSE = "resourceStore.deleteUserStorehouse";
 
 
     /**
      * 查询 个人物品
      */
-    public static final String LIST_USERSTOREHOUSES = "userStorehouse.listUserStorehouses";
+    public static final String LIST_USERSTOREHOUSES = "resourceStore.listUserStorehouses";
 
 
 }
