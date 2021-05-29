025c02109ba0cbb500c79bc32d3a3d82aeab0384
java110 <928255095@qq.com>
2021-05-29 17:30:01 +0800

    修复没有业主的房屋bug

--- service-front/src/main/java/com/java110/front/smo/assetImport/impl/AssetImportSMOImpl.java
+++ service-front/src/main/java/com/java110/front/smo/assetImport/impl/AssetImportSMOImpl.java
@@ -5,19 +5,11 @@ import com.alibaba.fastjson.JSONObject;
 import com.java110.core.component.BaseComponentSMO;
 import com.java110.core.context.IPageData;
 import com.java110.dto.RoomDto;
-import com.java110.entity.assetImport.ImportFee;
-import com.java110.entity.assetImport.ImportFloor;
-import com.java110.entity.assetImport.ImportOwner;
-import com.java110.entity.assetImport.ImportParkingSpace;
-import com.java110.entity.assetImport.ImportRoom;
+import com.java110.entity.assetImport.*;
 import com.java110.entity.component.ComponentValidateResult;
 import com.java110.front.smo.assetImport.IAssetImportSMO;
 import com.java110.utils.constant.ServiceConstant;
-import com.java110.utils.util.Assert;
-import com.java110.utils.util.CommonUtil;
-import com.java110.utils.util.DateUtil;
-import com.java110.utils.util.ImportExcelUtils;
-import com.java110.utils.util.StringUtil;
+import com.java110.utils.util.*;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.slf4j.Logger;
@@ -794,7 +786,7 @@ public class AssetImportSMOImpl extends BaseComponentSMO implements IAssetImport
                 Assert.hasValue(os[3], "房屋信息选项中" + (osIndex + 1) + "行房屋楼层为空");
                 Assert.hasValue(os[4], "房屋信息选项中" + (osIndex + 1) + "行房屋户型为空");
                 Assert.hasValue(os[5], "房屋信息选项中" + (osIndex + 1) + "行建筑面积为空");
-                if (!StringUtil.isNullOrNone(os[6])) {
+                if (os.length > 6 && !StringUtil.isNullOrNone(os[6])) {
                     Assert.hasValue(os[7], "房屋信息选项中" + (osIndex + 1) + "行房屋费用为空");
                     Assert.hasValue(os[8], "房屋信息选项中" + (osIndex + 1) + "行费用到期时间为空");
                 }
@@ -805,11 +797,11 @@ public class AssetImportSMOImpl extends BaseComponentSMO implements IAssetImport
                 importRoom.setSection(os[4].toString());
                 importRoom.setBuiltUpArea(Double.parseDouble(os[5].toString()));
 
-                if (!StringUtil.isNullOrNone(os[6])) {
+                if (os.length > 6 && !StringUtil.isNullOrNone(os[6])) {
                     importRoom.setRoomFeeId(os[7].toString());
                     importRoom.setFeeEndDate(os[8].toString());
                 }
-                if (StringUtil.isNullOrNone(os[6])) {
+                if (os.length < 7 || StringUtil.isNullOrNone(os[6])) {
                     rooms.add(importRoom);
                     continue;
                 }
