eddea331e671f1dc892239dca8b45414b9f1e49c
java110 <928255095@qq.com>
2021-06-17 01:49:22 +0800

    优化账户预存定时任务

--- java110-bean/src/main/java/com/java110/dto/app/AppDto.java
+++ java110-bean/src/main/java/com/java110/dto/app/AppDto.java
@@ -22,6 +22,7 @@ public class AppDto extends PageDto implements Serializable {
     public static final String WECHAT_MALL_APP_ID = "992021030901240071";
     public static final String OWNER_WECHAT_PAY = "992020011134400001"; // 微信支付
     public static final String OWNER_APP_PAY = "992020061440050003"; // app微信支付
+    public static final String JOB_APP_ID = "992021061746360004"; // 定时任务应用
 
 
     private String appId;
