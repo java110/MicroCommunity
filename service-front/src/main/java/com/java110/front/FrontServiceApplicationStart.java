bd199ec0fa775f8ec6fbbad0c8ad082394d595db
java110 <928255095@qq.com>
2021-08-05 14:09:22 +0800

    优化代码

--- service-front/src/main/java/com/java110/front/FrontServiceApplicationStart.java
+++ service-front/src/main/java/com/java110/front/FrontServiceApplicationStart.java
@@ -63,7 +63,7 @@ import java.nio.charset.Charset;
 //@EnableConfigurationProperties(EventProperties.class)
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableCaching
-@EnableFeignClients(basePackages = {"com.java110.intf.order"})
+@EnableFeignClients(basePackages = {"com.java110.intf.order","com.java110.core.smo"})
 public class FrontServiceApplicationStart {
 
     private static Logger logger = LoggerFactory.getLogger(FrontServiceApplicationStart.class);
