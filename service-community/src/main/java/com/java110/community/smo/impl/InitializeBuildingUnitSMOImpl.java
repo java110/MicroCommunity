0fbb8ae2abb6add852a0e4c9ebe2456b3c24c84c
1098226878 <1098226878@qq.com>
2021-08-06 11:57:07 +0800

    优化代码

--- service-community/src/main/java/com/java110/community/smo/impl/InitializeBuildingUnitSMOImpl.java
+++ service-community/src/main/java/com/java110/community/smo/impl/InitializeBuildingUnitSMOImpl.java
@@ -7,7 +7,7 @@ import com.java110.intf.community.IinitializeBuildingUnitSMO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
