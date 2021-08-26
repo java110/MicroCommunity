002b0812f26a986fb6f2b2adb120e7a2afb101ca
java110 <928255095@qq.com>
2021-08-26 15:36:18 +0800

    优化代码

--- service-common/src/main/java/com/java110/common/activity/resourceEnter/ResourceEnterToDepartmentListener.java
+++ service-common/src/main/java/com/java110/common/activity/resourceEnter/ResourceEnterToDepartmentListener.java
@@ -20,29 +20,7 @@ public class ResourceEnterToDepartmentListener implements TaskListener , Executi
     public void notify(DelegateTask delegateTask) {
         logger.info("查询部门审核人员");
 
-//        auditUserInnerServiceSMOImpl = ApplicationContextFactory.getBean("auditUserInnerServiceSMOImpl", IAuditUserInnerServiceSMO.class);
-//        AuditUserDto auditUserDto = new AuditUserDto();
-//        PurchaseApplyDto purchaseApplyDto = (PurchaseApplyDto)delegateTask.getVariable("purchaseApplyDto");
         String nextAuditStaffId = delegateTask.getVariable("nextAuditStaffId").toString();
-        /*auditUserDto.setStoreId(purchaseApplyDto.getStoreId());
-        auditUserDto.setObjCode("resourceEntry");
-        auditUserDto.setAuditLink("809001");
-        List<AuditUserDto> auditUserDtos = auditUserInnerServiceSMOImpl.queryAuditUsers(auditUserDto);
-
-
-        for (AuditUserDto tmpAuditUser : auditUserDtos) {
-            AuditUser auditUser = BeanConvertUtil.covertBean(tmpAuditUser, AuditUser.class);
-            logger.info("查询到用户："+tmpAuditUser.getUserName());
-
-            delegateTask.setVariable(auditUser.getUserId(), auditUser);
-
-        }
-
-        logger.info("查询审核人员人数："+auditUserDtos.size());
-
-        if (auditUserDtos == null || auditUserDtos.size() < 1) {
-            return;
-        }*/
 
         delegateTask.setAssignee(nextAuditStaffId);
         logger.info("设置部门审核人员："+nextAuditStaffId);
