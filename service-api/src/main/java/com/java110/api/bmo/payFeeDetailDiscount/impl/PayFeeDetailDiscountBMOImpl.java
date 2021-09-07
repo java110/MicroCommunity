b6318c2635c898b388b151854b28d50f43cfc24b
java110 <928255095@qq.com>
2021-09-07 11:05:25 +0800

    优化退费时没有考虑折扣 优惠问题

--- service-api/src/main/java/com/java110/api/bmo/payFeeDetailDiscount/impl/PayFeeDetailDiscountBMOImpl.java
+++ service-api/src/main/java/com/java110/api/bmo/payFeeDetailDiscount/impl/PayFeeDetailDiscountBMOImpl.java
@@ -5,7 +5,6 @@ import com.java110.api.bmo.ApiBaseBMO;
 import com.java110.api.bmo.payFeeDetailDiscount.IPayFeeDetailDiscountBMO;
 import com.java110.core.context.DataFlowContext;
 import com.java110.intf.fee.IPayFeeDetailDiscountInnerServiceSMO;
-import com.java110.po.fee.PayFeePo;
 import com.java110.po.payFeeDetailDiscount.PayFeeDetailDiscountPo;
 import com.java110.utils.constant.BusinessTypeConstant;
 import com.java110.utils.constant.CommonConstant;
@@ -42,6 +41,25 @@ public class PayFeeDetailDiscountBMOImpl extends ApiBaseBMO implements IPayFeeDe
         business.getJSONObject(CommonConstant.HTTP_BUSINESS_DATAS).put(PayFeeDetailDiscountPo.class.getSimpleName(), businessFee);
         return business;
 
+    }
+
+    /**
+     * 添加小区信息
+     *
+     * @param paramInJson     接口调用放传入入参
+     * @param dataFlowContext 数据上下文
+     * @return 订单服务能够接受的报文
+     */
+    public void addPayFeeDetailDiscountTwo(JSONObject paramInJson, JSONObject discountJson, DataFlowContext dataFlowContext) {
+        JSONObject businessFee = new JSONObject();
+        businessFee.put("detailDiscountId", "-1");
+        businessFee.put("discountPrice", discountJson.getString("discountPrice"));
+        businessFee.put("discountId", discountJson.getString("discountId"));
+        businessFee.put("detailId", paramInJson.containsKey("newDetailId") ? paramInJson.getString("newDetailId") : paramInJson.getString("detailId"));
+        businessFee.put("communityId", paramInJson.getString("communityId"));
+        businessFee.put("feeId", paramInJson.getString("feeId"));
+        PayFeeDetailDiscountPo payFeeDetailDiscountPo = BeanConvertUtil.covertBean(businessFee, PayFeeDetailDiscountPo.class);
+        super.insert(dataFlowContext, payFeeDetailDiscountPo, BusinessTypeConstant.BUSINESS_TYPE_SAVE_DETAIL_DISCOUNT_INFO);
     }
 
 
