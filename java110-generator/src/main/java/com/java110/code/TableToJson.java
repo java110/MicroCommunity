c4639b61053f66822a5230d53d06efb4307ad8ef
wuxw <928255095@qq.com>
2021-09-16 22:09:16 +0800

    加入收据模板功能

--- java110-generator/src/main/java/com/java110/code/TableToJson.java
+++ java110-generator/src/main/java/com/java110/code/TableToJson.java
@@ -7,23 +7,14 @@ import com.java110.utils.util.StringUtil;
 public class TableToJson {
 
     //show create table c_orders  用这个语句获取
-    public static final String createTableSql = "CREATE TABLE `gov_car` (\n" +
-            "  `car_id` varchar(30) NOT NULL PRIMARY key COMMENT '汽车ID',\n" +
-            "  `pa_id` varchar(30) NOT NULL COMMENT '停车场ID',\n" +
-            "  `car_num` varchar(12) NOT NULL COMMENT '车牌号',\n" +
-            "  `car_brand` varchar(50) NOT NULL COMMENT '汽车品牌',\n" +
-            "  `car_type` varchar(4) NOT NULL COMMENT '9901 家用小汽车，9902 客车，9903 货车',\n" +
-            "  `car_color` varchar(12) NOT NULL COMMENT '颜色',\n" +
-            "  `ca_id` varchar(30) NOT NULL COMMENT '区域ID',\n" +
-            "  `gov_community_id` varchar(30) NOT NULL COMMENT '小区ID',\n" +
-            "  `start_time` datetime NOT NULL COMMENT '起租时间',\n" +
-            "  `end_time` datetime NOT NULL COMMENT '结租时间',\n" +
-            "   gov_person_id varchar(30) not null comment '车主ID',\n" +
-            "  `car_type_cd` varchar(4) NOT NULL DEFAULT '1001' COMMENT '1001 主车辆 1002 成员车辆',\n" +
-            "  `remark` varchar(200) DEFAULT NULL COMMENT '备注',\n" +
+    public static final String createTableSql = "CREATE TABLE `fee_print_page` (\n" +
+            "  `page_id` varchar(30) NOT NULL COMMENT '页面ID',\n" +
+            "  `page_name` varchar(128) NOT NULL COMMENT '名称',\n" +
+            "  `community_id` varchar(30) NOT NULL COMMENT '小区ID',\n" +
+            "  `page_url` varchar(512) NOT NULL COMMENT '收据页面',\n" +
+            "   state varchar(12) not null default 'F' comment '状态 T 有效 F为无效',\n" +
             "  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',\n" +
-            "  `status_cd` varchar(2) NOT NULL DEFAULT '0' COMMENT '数据状态，详细参考c_status表，0, 在用 1失效',\n" +
-            "  `datasource_type` varchar(30) NOT NULL DEFAULT '999999' COMMENT '数据来源 999999 政务系统添加 777777 物业系统同步'\n" +
+            "  `status_cd` varchar(2) NOT NULL DEFAULT '0' COMMENT '数据状态 1表示 失效 0 有效'\n" +
             ")";
 
     public static void main(String[] args) {
@@ -38,9 +29,9 @@ public class TableToJson {
         param.put("desc", "");
         param.put("id", "");
         param.put("name", "");
-        param.put("shareColumn", "ca_id");
+        param.put("shareColumn", "community_id");
         param.put("shareName", "");
-        param.put("shareParam", "caId");
+        param.put("shareParam", "communityId");
         param.put("tableName", tableName);
         JSONObject paramColumn = new JSONObject();
         JSONArray requireds = new JSONArray();
