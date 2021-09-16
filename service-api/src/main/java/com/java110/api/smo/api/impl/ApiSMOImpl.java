9484f0109e6da227860b3d81efd07776baef005b
java110 <928255095@qq.com>
2021-09-16 10:41:04 +0800

    优化代码

--- service-api/src/main/java/com/java110/api/smo/api/impl/ApiSMOImpl.java
+++ service-api/src/main/java/com/java110/api/smo/api/impl/ApiSMOImpl.java
@@ -12,7 +12,6 @@ import com.java110.utils.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -87,6 +86,13 @@ public class ApiSMOImpl extends DefaultAbstractComponentSMO implements IApiSMO {
             if (!StringUtil.isEmpty(result.getUserName())) {
                 headers.put("user-name", URLEncoder.encode(result.getUserName(), "UTF-8"));
             }
+        }
+
+        if (!headers.containsKey("user_id")) {
+            headers.put("user_id", "-1");
+        }
+        if (!headers.containsKey("user-id")) {
+            headers.put("user-id", "-1");
         }
         headers.put("store-id", result.getStoreId());
         ResponseEntity<String> responseEntity = apiServiceSMOImpl.service(body, headers);
