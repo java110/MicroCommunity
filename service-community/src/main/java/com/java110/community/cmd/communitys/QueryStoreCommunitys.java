d2fe2226f8c9e9ecd838b439de33e4f6bc218087
java110 <928255095@qq.com>
2021-09-18 09:36:18 +0800

    调整 商户小区实现过程

---
+++ service-community/src/main/java/com/java110/community/cmd/communitys/QueryStoreCommunitys.java
@@ -0,0 +1,43 @@
+package com.java110.community.cmd.communitys;
+
+import com.alibaba.fastjson.JSONObject;
+import com.java110.core.annotation.Java110Cmd;
+import com.java110.core.context.ICmdDataFlowContext;
+import com.java110.core.event.cmd.AbstractServiceCmdListener;
+import com.java110.core.event.cmd.CmdEvent;
+import com.java110.dto.CommunityMemberDto;
+import com.java110.dto.community.CommunityDto;
+import com.java110.intf.community.ICommunityInnerServiceSMO;
+import com.java110.utils.exception.CmdException;
+import com.java110.utils.util.BeanConvertUtil;
+import com.java110.vo.ResultVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * 查询商户小区
+ * add by 吴学文 2021-09-18
+ */
+@Java110Cmd(serviceCode = "/communitys/queryStoreCommunitys")
+public class QueryStoreCommunitys extends AbstractServiceCmdListener {
+    private final static Logger logger = LoggerFactory.getLogger(QueryStoreCommunitys.class);
+
+
+    @Autowired
+    private ICommunityInnerServiceSMO communityInnerServiceSMOImpl;
+
+    @Override
+    public void validate(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) {
+
+    }
+
+    @Override
+    public void doCmd(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) throws CmdException {
+        CommunityMemberDto communityMemberDto = BeanConvertUtil.covertBean(reqJson, CommunityMemberDto.class);
+        List<CommunityDto> communityDtos = communityInnerServiceSMOImpl.getStoreCommunitys(communityMemberDto);
+        cmdDataFlowContext.setResponseEntity(ResultVo.createResponseEntity(communityDtos));
+    }
+}
