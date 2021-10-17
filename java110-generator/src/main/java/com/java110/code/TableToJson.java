e201c05eb9ef437f3c2519f0dae7bb6a10e4cda2
java110 <928255095@qq.com>
2021-10-18 00:11:41 +0800

    加入岗亭功能

--- java110-generator/src/main/java/com/java110/code/TableToJson.java
+++ java110-generator/src/main/java/com/java110/code/TableToJson.java
@@ -7,25 +7,26 @@ import com.java110.utils.util.StringUtil;
 public class TableToJson {
 
     //show create table c_orders  用这个语句获取
-    public static final String createTableSql = "CREATE TABLE `owner_car_open_user` (\n" +
-            "  `open_user_id` varchar(30) NOT NULL COMMENT 'app用户ID',\n" +
-            "  `open_id` varchar(128) NOT NULL COMMENT '微信或支付宝开放ID',\n" +
-            "  `car_num` varchar(32) NOT NULL COMMENT '车牌号',\n" +
-            "  `open_type` varchar(12) NOT NULL DEFAULT 'WECHAT' COMMENT '微信WECHAT 支付宝 ALI',\n" +
-            "  `nickname` varchar(255) DEFAULT NULL COMMENT '昵称',\n" +
-            "  `headimgurl` varchar(255) DEFAULT NULL COMMENT '微信头像',\n" +
+    public static final String createTableSql = "CREATE TABLE `parking_box` (\n" +
+            "  `box_id` varchar(30) NOT NULL COMMENT '岗亭ID',\n" +
+            "  `box_name` varchar(64) NOT NULL COMMENT '岗亭名称',\n" +
+            "  `community_id` varchar(30) NOT NULL COMMENT '小区ID',\n" +
+            "  `temp_car_in` varchar(12) NOT NULL COMMENT '临时车是否进场 Y 进场 N 不进场',\n" +
+            "  `fee` varchar(12) NOT NULL DEFAULT 'Y' COMMENT '岗亭是否 收费，主要考虑 岗亭嵌套问题 Y 收费 N 不收费',\n" +
+            "  `blue_car_in` varchar(12) NOT NULL COMMENT '蓝牌车是否可以进场 Y 进场 N 不进场',\n" +
+            "  `yelow_car_in` varchar(12) NOT NULL COMMENT '黄牌车是否可以进场 Y 进场 N 不进场',\n" +
+            "  `remark` varchar(300) DEFAULT NULL COMMENT '备注',\n" +
             "  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',\n" +
-            "  `status_cd` varchar(2) NOT NULL DEFAULT '0' COMMENT '数据状态，详细参考c_status表，0, 在用 1失效',\n" +
-            "  PRIMARY KEY (`open_user_id`)\n" +
-            ")";
+            "  `status_cd` varchar(2) NOT NULL DEFAULT '0' COMMENT '数据状态，详细参考c_status表，S 保存，0, 在用 1失效'\n" +
+            ") ";
 
     public static void main(String[] args) {
-        String desc = "车辆微信关系";
-        String id = "openUserId";
-        String name = "ownerCarOpenUser";
-        String shareName = "user"; //生成到那个服务下
-        String shareColumn = "open_user_id";
-        String shareParam = "openUserId";
+        String desc = "岗亭";
+        String id = "boxId";
+        String name = "parkingBox";
+        String shareName = "community"; //生成到那个服务下
+        String shareColumn = "community_id";
+        String shareParam = "communityId";
 
         //业务名称 desc 业务编码名称生成后类名 name 主键 id  需要放到那个服务 shareName
         String newSql = createTableSql.substring(createTableSql.indexOf("(") + 1, createTableSql.lastIndexOf(")"));
