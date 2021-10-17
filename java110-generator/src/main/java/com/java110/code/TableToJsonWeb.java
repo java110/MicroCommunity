95131f4c0bee1d1bf34ff38a82d96eadb0bdc98c
java110 <928255095@qq.com>
2021-10-18 01:17:41 +0800

    优化代码

--- java110-generator/src/main/java/com/java110/code/TableToJsonWeb.java
+++ java110-generator/src/main/java/com/java110/code/TableToJsonWeb.java
@@ -7,22 +7,26 @@ import com.java110.utils.util.StringUtil;
 public class TableToJsonWeb {
 
     //show create table c_orders  用这个语句获取
-    public static final String createTableSql = "CREATE TABLE `machine_type` (\n" +
-            "  `type_id` varchar(30) NOT NULL COMMENT '设备类型ID',\n" +
-            "  `machine_type_cd` varchar(30) NOT NULL COMMENT '设备大类 详情看t_dict',\n" +
-            "  `machine_type_name` varchar(30) NOT NULL COMMENT '设备类型名称',\n" +
+    public static final String createTableSql = "CREATE TABLE `parking_box` (\n" +
+            "  `box_id` varchar(30) NOT NULL COMMENT '岗亭ID',\n" +
+            "  `box_name` varchar(64) NOT NULL COMMENT '岗亭名称',\n" +
+            "  `community_id` varchar(30) NOT NULL COMMENT '小区ID',\n" +
+            "  `temp_car_in` varchar(12) NOT NULL COMMENT '临时车是否进场 Y 进场 N 不进场',\n" +
+            "  `fee` varchar(12) NOT NULL DEFAULT 'Y' COMMENT '岗亭是否 收费，主要考虑 岗亭嵌套问题 Y 收费 N 不收费',\n" +
+            "  `blue_car_in` varchar(12) NOT NULL COMMENT '蓝牌车是否可以进场 Y 进场 N 不进场',\n" +
+            "  `yelow_car_in` varchar(12) NOT NULL COMMENT '黄牌车是否可以进场 Y 进场 N 不进场',\n" +
+            "  `remark` varchar(300) DEFAULT NULL COMMENT '备注',\n" +
             "  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',\n" +
-            "  `status_cd` varchar(2) NOT NULL DEFAULT '0' COMMENT '数据状态，详细参考c_status表，S 保存，0, 在用 1失效',\n" +
-            "  KEY `idx_bm_machine_id` (`type_id`) USING BTREE\n" +
+            "  `status_cd` varchar(2) NOT NULL DEFAULT '0' COMMENT '数据状态，详细参考c_status表，S 保存，0, 在用 1失效'\n" +
             ")";
 
     public static void main(String[] args) {
-        String templateName = "设备类型"; //业务名称
-        String templateCode = "machineType"; //表名大写
-        String templateKey = "typeId"; //表主键
-        String templateKeyName = "设备类型ID";//主键说明
-        String searchCode = "typeId"; //分片字段
-        String searchName = "设备类型ID"; //分片字段说明
+        String templateName = "岗亭"; //业务名称
+        String templateCode = "parkingBox"; //表名大写
+        String templateKey = "boxId"; //表主键
+        String templateKeyName = "岗亭ID";//主键说明
+        String searchCode = "boxId"; //分片字段
+        String searchName = "岗亭ID"; //分片字段说明
         String directories = "property"; //前端生成到那个目录下
 
         // templateName 业务名称 业务编码名称生成后文件名 templateCode 主键 templateKey
