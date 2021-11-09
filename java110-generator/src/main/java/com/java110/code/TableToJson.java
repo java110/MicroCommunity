42940f5da4f29d35ef7455f71082f3b517f927cb
java110 <928255095@qq.com>
2021-11-09 13:33:49 +0800

    优化代码

--- java110-generator/src/main/java/com/java110/code/TableToJson.java
+++ java110-generator/src/main/java/com/java110/code/TableToJson.java
@@ -7,23 +7,25 @@ import com.java110.utils.util.StringUtil;
 public class TableToJson {
 
     //show create table c_orders  用这个语句获取
-    public static final String createTableSql = "CREATE TABLE `report_custom` (\n" +
-            "  `custom_id` varchar(30) NOT NULL COMMENT '报表编号',\n" +
-            "  `group_id` varchar(30) NOT NULL COMMENT '组编号',\n" +
-            "  `title` varchar(64) NOT NULL COMMENT '选项标题',\n" +
-            "  `seq` int(11) NOT NULL COMMENT '排序',\n" +
+    public static final String createTableSql = "CREATE TABLE `report_custom_component_condition` (\n" +
+            "  `condition_id` varchar(30) NOT NULL COMMENT '条件ID',\n" +
+            "  `component_id` varchar(30) NOT NULL COMMENT '组件ID',\n" +
+            "  `name` varchar(64) NOT NULL COMMENT '名称',\n" +
+            "  `holdpace` varchar(64) NOT NULL DEFAULT 'C' COMMENT '提示',\n" +
+            "  `param` varchar(64) NOT NULL COMMENT '参数',\n" +
+            "  `type` varchar(12) NOT NULL COMMENT '类型 input 输入框',\n" +
             "  `remark` varchar(512) DEFAULT NULL COMMENT '描述',\n" +
             "  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',\n" +
             "  `status_cd` varchar(2) NOT NULL DEFAULT '0' COMMENT '数据状态，详细参考c_status表，S 保存，0, 在用 1失效'\n" +
-            ") ";
+            ")";
 
     public static void main(String[] args) {
-        String desc = "报表组件";
-        String id = "customId";
-        String name = "reportCustom";
+        String desc = "报表组件条件";
+        String id = "conditionId";
+        String name = "reportCustomComponentCondition";
         String shareName = "report"; //生成到那个服务下
-        String shareColumn = "custom_id";
-        String shareParam = "customId";
+        String shareColumn = "condition_id";
+        String shareParam = "conditionId";
         //业务名称 desc 业务编码名称生成后类名 name 主键 id  需要放到那个服务 shareName
         String newSql = createTableSql.substring(createTableSql.indexOf("(") + 1, createTableSql.lastIndexOf(")"));
         String tableName = createTableSql.substring(createTableSql.indexOf("TABLE") + 5, createTableSql.indexOf("("));
