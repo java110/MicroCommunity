ab4cf523f77043dafe8d3ffbbf99080fe4392a5e
java110 <928255095@qq.com>
2021-12-21 13:06:38 +0800

    优化代码

--- java110-generator/src/main/java/com/java110/code/TableToJson.java
+++ java110-generator/src/main/java/com/java110/code/TableToJson.java
@@ -18,32 +18,29 @@ import java.nio.charset.Charset;
 public class TableToJson {
 
     //show create table c_orders  用这个语句获取
-    public static final String createTableSql = "CREATE TABLE `r_repair_user` (\n" +
-            "  `ru_id` varchar(30) NOT NULL COMMENT '报修派单ID',\n" +
-            "  `repair_id` varchar(30) NOT NULL COMMENT '报修ID',\n" +
-            "  `b_id` varchar(30) NOT NULL COMMENT '业务Id',\n" +
-            "  `community_id` varchar(30) NOT NULL COMMENT '小区ID',\n" +
+    public static final String createTableSql = "CREATE TABLE `online_pay` (\n" +
+            "  `pay_id` varchar(30) NOT NULL COMMENT '支付ID',\n" +
+            "  `order_id` varchar(30) NOT NULL COMMENT '订单ID',\n" +
+            "  `transaction_id` varchar(64) DEFAULT NULL COMMENT '交易ID',\n" +
+            "  `app_id` varchar(64) DEFAULT NULL COMMENT '微信APPID',\n" +
+            "  `mch_id` varchar(64) NOT NULL COMMENT '商户ID',\n" +
+            "  `total_fee` decimal(10,2) NOT NULL COMMENT '付款总金额',\n" +
+            "  `refund_fee` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '退款金额',\n" +
+            "  `pay_name` varchar(256) DEFAULT NULL COMMENT '名称',\n" +
+            "  `open_id` varchar(30) DEFAULT NULL COMMENT '开放ID',\n" +
+            "  `state` varchar(12) NOT NULL DEFAULT 'W' COMMENT '状态 W待支付 C 支付完成 F 通知失败 WT 待退费 CT退费完成',\n" +
+            "  `message` varchar(1024) DEFAULT NULL COMMENT '状态原因',\n" +
             "  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',\n" +
-            "  `state` varchar(8) NOT NULL COMMENT '员工处理状态，请查看t_dict 表',\n" +
-            "  `context` longtext COMMENT '报修内容',\n" +
-            "  `status_cd` varchar(2) NOT NULL DEFAULT '0' COMMENT '数据状态，详细参考c_status表，S 保存，0, 在用 1失效',\n" +
-            "  `staff_id` varchar(30) NOT NULL COMMENT '当前处理员工',\n" +
-            "  `staff_name` varchar(64) NOT NULL COMMENT '当前处理员工名称',\n" +
-            "  `pre_staff_id` varchar(30) NOT NULL COMMENT '上一节点处理员工',\n" +
-            "  `pre_staff_name` varchar(64) NOT NULL COMMENT '上一节点处理员工名称',\n" +
-            "  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '开始时间',\n" +
-            "  `end_time` timestamp NULL DEFAULT NULL COMMENT '结束时间',\n" +
-            "  `repair_event` varchar(12) NOT NULL COMMENT '事件，startUser 开始用户 auditUser 审核处理员工',\n" +
-            "  `pre_ru_id` varchar(30) NOT NULL COMMENT '上一节点处理ID'\n" +
+            "  `status_cd` varchar(2) NOT NULL DEFAULT '0' COMMENT '数据状态，详细参考c_status表，0在用，1失效'\n" +
             ")";
 
     public static void main(String[] args) {
-        String desc = "费用明细";
-        String id = "ruId";
-        String name = "repairUserNew";
-        String shareName = "fee"; //生成到那个服务下
-        String shareColumn = "community_id";
-        String shareParam = "communityId";
+        String desc = "线上支付";
+        String id = "payId";
+        String name = "onlinePay";
+        String shareName = "acct"; //生成到那个服务下
+        String shareColumn = "order_id";
+        String shareParam = "orderId";
         //业务名称 desc 业务编码名称生成后类名 name 主键 id  需要放到那个服务 shareName
         String newSql = createTableSql.substring(createTableSql.indexOf("(") + 1, createTableSql.lastIndexOf(")"));
         String tableName = createTableSql.substring(createTableSql.indexOf("TABLE") + 5, createTableSql.indexOf("("));
---
+++ service-acct/src/main/java/com/java110/acct/dao/impl/OnlinePayV1ServiceDaoImpl.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2017-2020 吴学文 and java110 team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.java110.acct.dao.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.java110.utils.constant.ResponseConstant;
+import com.java110.utils.exception.DAOException;
+import com.java110.utils.util.DateUtil;
+import com.java110.core.base.dao.BaseServiceDao;
+import com.java110.acct.dao.IOnlinePayV1ServiceDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 类表述：
+ * add by 吴学文 at 2021-12-21 13:05:25 mail: 928255095@qq.com
+ * open source address: https://gitee.com/wuxw7/MicroCommunity
+ * 官网：http://www.homecommunity.cn
+ * 温馨提示：如果您对此文件进行修改 请不要删除原有作者及注释信息，请补充您的 修改的原因以及联系邮箱如下
+ * // modify by 张三 at 2021-09-12 第10行在某种场景下存在某种bug 需要修复，注释10至20行 加入 20行至30行
+ */
+@Service("onlinePayV1ServiceDaoImpl")
+public class OnlinePayV1ServiceDaoImpl extends BaseServiceDao implements IOnlinePayV1ServiceDao {
+
+    private static Logger logger = LoggerFactory.getLogger(OnlinePayV1ServiceDaoImpl.class);
+
+
+
+
+
+    /**
+     * 保存线上支付信息 到 instance
+     * @param info   bId 信息
+     * @throws DAOException DAO异常
+     */
+    @Override
+    public int saveOnlinePayInfo(Map info) throws DAOException {
+        logger.debug("保存 saveOnlinePayInfo 入参 info : {}",info);
+
+        int saveFlag = sqlSessionTemplate.insert("onlinePayV1ServiceDaoImpl.saveOnlinePayInfo",info);
+
+        return saveFlag;
+    }
+
+
+    /**
+     * 查询线上支付信息（instance）
+     * @param info bId 信息
+     * @return List<Map>
+     * @throws DAOException DAO异常
+     */
+    @Override
+    public List<Map> getOnlinePayInfo(Map info) throws DAOException {
+        logger.debug("查询 getOnlinePayInfo 入参 info : {}",info);
+
+        List<Map> businessOnlinePayInfos = sqlSessionTemplate.selectList("onlinePayV1ServiceDaoImpl.getOnlinePayInfo",info);
+
+        return businessOnlinePayInfos;
+    }
+
+
+    /**
+     * 修改线上支付信息
+     * @param info 修改信息
+     * @throws DAOException DAO异常
+     */
+    @Override
+    public int updateOnlinePayInfo(Map info) throws DAOException {
+        logger.debug("修改 updateOnlinePayInfo 入参 info : {}",info);
+
+        int saveFlag = sqlSessionTemplate.update("onlinePayV1ServiceDaoImpl.updateOnlinePayInfo",info);
+
+        return saveFlag;
+    }
+
+     /**
+     * 查询线上支付数量
+     * @param info 线上支付信息
+     * @return 线上支付数量
+     */
+    @Override
+    public int queryOnlinePaysCount(Map info) {
+        logger.debug("查询 queryOnlinePaysCount 入参 info : {}",info);
+
+        List<Map> businessOnlinePayInfos = sqlSessionTemplate.selectList("onlinePayV1ServiceDaoImpl.queryOnlinePaysCount", info);
+        if (businessOnlinePayInfos.size() < 1) {
+            return 0;
+        }
+
+        return Integer.parseInt(businessOnlinePayInfos.get(0).get("count").toString());
+    }
+
+
+}
