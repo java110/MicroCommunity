8cf520878ba4e31a5a57cac21b0943b6a79480d9
java110 <928255095@qq.com>
2022-01-14 19:26:52 +0800

    优化代码

--- java110-core/src/main/java/com/java110/core/client/RestTemplate.java
+++ java110-core/src/main/java/com/java110/core/client/RestTemplate.java
@@ -1,12 +1,10 @@
 package com.java110.core.client;
 
+import com.java110.core.log.LoggerFactory;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RequestCallback;
-import org.springframework.web.client.ResponseExtractor;
 import org.springframework.web.client.RestClientException;
 
 /**
@@ -22,6 +20,7 @@ public class RestTemplate extends org.springframework.web.client.RestTemplate {
 
     /**
      * 重写spring RestTemplate类 加入日志等信息
+     *
      * @param url
      * @param method
      * @param requestEntity
@@ -35,9 +34,9 @@ public class RestTemplate extends org.springframework.web.client.RestTemplate {
     public <T> ResponseEntity<T> exchange(String url, HttpMethod method,
                                           HttpEntity<?> requestEntity, Class<T> responseType, Object... uriVariables) throws RestClientException {
 
-        logger.debug("请求信息：url:{},method:{},request:{},uriVariables:{}",url,method,requestEntity,uriVariables);
+        logger.debug("请求信息：url:{},method:{},request:{},uriVariables:{}", url, method, requestEntity, uriVariables);
         ResponseEntity<T> responseEntity = super.exchange(url, method, requestEntity, responseType, uriVariables);
-        logger.debug("返回信息：responseEntity:{}",responseEntity);
+        logger.debug("返回信息：responseEntity:{}", responseEntity);
 
         return responseEntity;
     }
