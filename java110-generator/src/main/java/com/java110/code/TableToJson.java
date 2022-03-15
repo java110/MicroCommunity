e77749e5c2589f162df8af84bfac54428630738d
java110 <928255095@qq.com>
2022-03-15 11:10:46 +0800

    加入巡检项目功能

--- java110-generator/src/main/java/com/java110/code/TableToJson.java
+++ java110-generator/src/main/java/com/java110/code/TableToJson.java
@@ -18,21 +18,21 @@ import java.nio.charset.Charset;
 public class TableToJson {
 
     //show create table c_orders  用这个语句获取
-    public static final String createTableSql = "CREATE TABLE `m_menu_group_community` (\n" +
-            "  `gc_id` varchar(30) NOT NULL COMMENT '主键ID',\n" +
-            "  `g_id` varchar(30) NOT NULL COMMENT '菜单组ID',\n" +
-            "  `name` varchar(10) NOT NULL COMMENT '菜单组名称',\n" +
-            "  `community_id` varchar(30) NOT NULL COMMENT '小区ID',\n" +
-            "  `community_name` varchar(64) NOT NULL COMMENT '小区名称',\n" +
+    public static final String createTableSql = "CREATE TABLE `inspection_item_title_value` (\n" +
+            "  `value_id` varchar(30) NOT NULL COMMENT '主键ID',\n" +
+            "  `title_id` varchar(30) NOT NULL COMMENT '题目ID',\n" +
+            "  `item_value` varchar(256) NOT NULL COMMENT '选项说明',\n" +
+            "  `community_id` varchar(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '小区ID',\n" +
+            "  `seq` int(11) NOT NULL DEFAULT '1' COMMENT '显示顺序',\n" +
             "  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',\n" +
-            "  `status_cd` varchar(2) NOT NULL DEFAULT '0' COMMENT '数据状态，详细参考c_status表，0在用，1失效'\n" +
-            ")";
+            "  `status_cd` varchar(2) NOT NULL DEFAULT '0' COMMENT '数据状态，详细参考c_status表，S 保存，0, 在用 1失效'\n" +
+            ") ";
 
     public static void main(String[] args) {
-        String desc = "小区功能";
-        String id = "gcId";
-        String name = "menuGroupCommunity";
-        String shareName = "user"; //生成到那个服务下
+        String desc = "题目选项";
+        String id = "valueId";
+        String name = "inspectionItemTitleValue";
+        String shareName = "community"; //生成到那个服务下
         String shareColumn = "community_id";
         String shareParam = "communityId";
         //业务名称 desc 业务编码名称生成后类名 name 主键 id  需要放到那个服务 shareName
