7365d3992ac88bc10ff0a3b2e38ced8636f13b26
java110 <928255095@qq.com>
2022-03-22 18:45:27 +0800

    优化 工作办理

--- java110-interface/src/main/java/com/java110/config/feign/FeignConfiguration.java
+++ java110-interface/src/main/java/com/java110/config/feign/FeignConfiguration.java
@@ -26,7 +26,7 @@ public class FeignConfiguration {
         if (SystemLogDto.getLogSwatch()) {
             return Logger.Level.FULL;
         } else {
-            return Logger.Level.BASIC;
+            return Logger.Level.NONE;
         }
     }
 
