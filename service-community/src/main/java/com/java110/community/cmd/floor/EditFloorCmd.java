35bcb5b033002d9dc0c711f0490c3599e65b4582
java110 <928255095@qq.com>
2022-03-24 08:59:06 +0800

    优化 编辑楼栋

--- service-community/src/main/java/com/java110/community/cmd/floor/EditFloorCmd.java
+++ service-community/src/main/java/com/java110/community/cmd/floor/EditFloorCmd.java
@@ -19,8 +19,8 @@ import com.java110.utils.util.BeanConvertUtil;
 import com.java110.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Java110Cmd(serviceCode = "floor.saveFloor")
-public class SaveFloorCmd extends AbstractServiceCmdListener {
+@Java110Cmd(serviceCode = "floor.editFloor")
+public class EditFloorCmd extends AbstractServiceCmdListener {
     @Autowired
     private IFloorInnerServiceSMO floorInnerServiceSMOImpl;
 
@@ -29,39 +29,30 @@ public class SaveFloorCmd extends AbstractServiceCmdListener {
 
     @Override
     public void validate(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) {
+        Assert.jsonObjectHaveKey(reqJson, "floorId", "请求报文中未包含floorId");
         Assert.jsonObjectHaveKey(reqJson, "name", "请求报文中未包含name");
         Assert.jsonObjectHaveKey(reqJson, "userId", "请求报文中未包含userId");
         Assert.jsonObjectHaveKey(reqJson, "floorNum", "请求报文中未包含floorNum");
         Assert.jsonObjectHaveKey(reqJson, "communityId", "请求报文中未包含communityId");
-        FloorDto floorDto = new FloorDto();
-        floorDto.setFloorNum(reqJson.getString("floorNum"));
-        floorDto.setCommunityId(reqJson.getString("communityId"));
-
-
-        int floorCount = floorInnerServiceSMOImpl.queryFloorsCount(floorDto);
-
-        if (floorCount > 0) {
-            throw new ListenerExecuteException(ResponseConstant.RESULT_CODE_ERROR, "楼栋编号已经存在");
-        }
     }
 
     @Override
     @Java110Transactional
     public void doCmd(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) throws CmdException {
-        String floorId = GenerateCodeFactory.getGeneratorId(GenerateCodeFactory.CODE_PREFIX_floorId);
         JSONObject businessFloor = new JSONObject();
-        businessFloor.put("floorId", floorId);
-        businessFloor.put("name", reqJson.getString("name"));
+        businessFloor.put("floorId", reqJson.getString("floorId"));
+        businessFloor.put("name", reqJson.getString("floorName"));
         businessFloor.put("remark", reqJson.getString("remark"));
         businessFloor.put("userId", reqJson.getString("userId"));
         businessFloor.put("floorNum", reqJson.getString("floorNum"));
         businessFloor.put("communityId", reqJson.getString("communityId"));
         businessFloor.put("floorArea", reqJson.getString("floorArea"));
+
         FloorPo floorPo = BeanConvertUtil.covertBean(businessFloor, FloorPo.class);
-        int flag = floorV1InnerServiceSMOImpl.saveFloor(floorPo);
+        int flag = floorV1InnerServiceSMOImpl.updateFloor(floorPo);
 
         if (flag < 1) {
-            throw new CmdException("保存楼栋失败");
+            throw new CmdException("修改楼栋失败");
         }
 
         cmdDataFlowContext.setResponseEntity(ResultVo.success());
