93b4f9782c2b880cc9d42670611fcb11f05e8446
java110 <928255095@qq.com>
2022-03-24 09:05:54 +0800

    删除楼栋

---
+++ service-community/src/main/java/com/java110/community/cmd/floor/DeleteFloorCmd.java
@@ -0,0 +1,44 @@
+package com.java110.community.cmd.floor;
+
+import com.alibaba.fastjson.JSONObject;
+import com.java110.core.annotation.Java110Cmd;
+import com.java110.core.annotation.Java110Transactional;
+import com.java110.core.context.ICmdDataFlowContext;
+import com.java110.core.event.cmd.AbstractServiceCmdListener;
+import com.java110.core.event.cmd.CmdEvent;
+import com.java110.intf.community.IFloorInnerServiceSMO;
+import com.java110.intf.community.IFloorV1InnerServiceSMO;
+import com.java110.po.floor.FloorPo;
+import com.java110.utils.exception.CmdException;
+import com.java110.utils.util.Assert;
+import com.java110.utils.util.BeanConvertUtil;
+import com.java110.vo.ResultVo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Java110Cmd(serviceCode = "floor.deleteFloor")
+public class DeleteFloorCmd extends AbstractServiceCmdListener {
+    @Autowired
+    private IFloorInnerServiceSMO floorInnerServiceSMOImpl;
+
+    @Autowired
+    private IFloorV1InnerServiceSMO floorV1InnerServiceSMOImpl;
+
+    @Override
+    public void validate(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) {
+        Assert.jsonObjectHaveKey(reqJson, "floorId", "请求报文中未包含floorId");
+        Assert.jsonObjectHaveKey(reqJson, "communityId", "请求报文中未包含communityId");
+    }
+
+    @Override
+    @Java110Transactional
+    public void doCmd(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) throws CmdException {
+        FloorPo floorPo = BeanConvertUtil.covertBean(reqJson, FloorPo.class);
+        int flag = floorV1InnerServiceSMOImpl.deleteFloor(floorPo);
+
+        if (flag < 1) {
+            throw new CmdException("删除楼栋失败");
+        }
+
+        cmdDataFlowContext.setResponseEntity(ResultVo.success());
+    }
+}
