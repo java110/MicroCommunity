c2dd4d3d2992898cbb31d42dec6c4aebead7e555
java110 <928255095@qq.com>
2022-04-01 08:38:23 +0800

    查询费用 修改为cmd 模式

--- service-fee/src/main/java/com/java110/fee/cmd/feeConfig/ListFeeConfigsCmd.java
+++ service-fee/src/main/java/com/java110/fee/cmd/feeConfig/ListFeeConfigsCmd.java
@@ -1,72 +1,41 @@
-package com.java110.api.listener.fee;
+package com.java110.fee.cmd.feeConfig;
 
 import com.alibaba.fastjson.JSONObject;
-import com.java110.api.listener.AbstractServiceApiListener;
-import com.java110.core.annotation.Java110Listener;
-import com.java110.core.context.DataFlowContext;
+import com.java110.core.annotation.Java110Cmd;
+import com.java110.core.context.ICmdDataFlowContext;
+import com.java110.core.event.cmd.AbstractServiceCmdListener;
+import com.java110.core.event.cmd.CmdEvent;
 import com.java110.dto.PageDto;
-import com.java110.intf.fee.IFeeConfigInnerServiceSMO;
 import com.java110.dto.fee.FeeConfigDto;
-import com.java110.core.event.service.api.ServiceDataFlowEvent;
-import com.java110.utils.constant.ServiceCodeFeeConfigConstant;
+import com.java110.intf.fee.IFeeConfigInnerServiceSMO;
+import com.java110.utils.exception.CmdException;
 import com.java110.utils.util.Assert;
 import com.java110.utils.util.BeanConvertUtil;
 import com.java110.utils.util.StringUtil;
 import com.java110.vo.api.feeConfig.ApiFeeConfigDataVo;
 import com.java110.vo.api.feeConfig.ApiFeeConfigVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- * 查询小区侦听类
- */
-@Java110Listener("listFeeConfigsListener")
-public class ListFeeConfigsListener extends AbstractServiceApiListener {
+@Java110Cmd(serviceCode = "feeConfig.listFeeConfigs")
+public class ListFeeConfigsCmd extends AbstractServiceCmdListener {
 
     @Autowired
     private IFeeConfigInnerServiceSMO feeConfigInnerServiceSMOImpl;
 
     @Override
-    public String getServiceCode() {
-        return ServiceCodeFeeConfigConstant.LIST_FEECONFIGS;
-    }
-
-    @Override
-    public HttpMethod getHttpMethod() {
-        return HttpMethod.GET;
-    }
-
-
-    @Override
-    public int getOrder() {
-        return DEFAULT_ORDER;
-    }
-
-
-    public IFeeConfigInnerServiceSMO getFeeConfigInnerServiceSMOImpl() {
-        return feeConfigInnerServiceSMOImpl;
-    }
-
-    public void setFeeConfigInnerServiceSMOImpl(IFeeConfigInnerServiceSMO feeConfigInnerServiceSMOImpl) {
-        this.feeConfigInnerServiceSMOImpl = feeConfigInnerServiceSMOImpl;
-    }
-
-    @Override
-    protected void validate(ServiceDataFlowEvent event, JSONObject reqJson) {
+    public void validate(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) throws CmdException {
         super.validatePageInfo(reqJson);
         Assert.hasKeyAndValue(reqJson, "communityId", "未包含小区ID");
 
     }
 
     @Override
-    protected void doSoService(ServiceDataFlowEvent event, DataFlowContext context, JSONObject reqJson) {
-
+    public void doCmd(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) throws CmdException {
         FeeConfigDto feeConfigDto = BeanConvertUtil.covertBean(reqJson, FeeConfigDto.class);
 
         if(!StringUtil.isEmpty(reqJson.getString("isFlag")) && reqJson.getString("isFlag").equals("0")){
@@ -91,7 +60,6 @@ public class ListFeeConfigsListener extends AbstractServiceApiListener {
 
         ResponseEntity<String> responseEntity = new ResponseEntity<String>(JSONObject.toJSONString(apiFeeConfigVo), HttpStatus.OK);
 
-        context.setResponseEntity(responseEntity);
-
+        cmdDataFlowContext.setResponseEntity(responseEntity);
     }
 }
