5cf5034e6252d800c9adace4944c2bb32741b52d
java110 <928255095@qq.com>
2022-04-08 09:49:48 +0800

    优化添加停车场失败问题

--- service-community/src/main/java/com/java110/community/cmd/parkingArea/UpdateParkingAreaCmd.java
+++ service-community/src/main/java/com/java110/community/cmd/parkingArea/UpdateParkingAreaCmd.java
@@ -15,10 +15,11 @@ import com.java110.po.parkingAreaAttr.ParkingAreaAttrPo;
 import com.java110.utils.exception.CmdException;
 import com.java110.utils.util.Assert;
 import com.java110.utils.util.BeanConvertUtil;
+import com.java110.utils.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Java110Cmd(serviceCode = "parkingArea.saveParkingArea")
-public class SaveParkingAreaCmd extends AbstractServiceCmdListener {
+@Java110Cmd(serviceCode = "parkingArea.updateParkingArea")
+public class UpdateParkingAreaCmd extends AbstractServiceCmdListener {
 
     @Autowired
     private IParkingAreaV1InnerServiceSMO parkingAreaV1InnerServiceSMOImpl;
@@ -28,65 +29,53 @@ public class SaveParkingAreaCmd extends AbstractServiceCmdListener {
 
     @Override
     public void validate(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) throws CmdException {
-        //Assert.hasKeyAndValue(reqJson, "xxx", "xxx");
-
+        Assert.hasKeyAndValue(reqJson, "paId", "停车场ID不能为空");
         Assert.hasKeyAndValue(reqJson, "num", "必填，请填写停车场编号");
         Assert.hasKeyAndValue(reqJson, "communityId", "必填，请填写小区信息");
         Assert.hasKeyAndValue(reqJson, "typeCd", "必填，请选择停车场类型");
 
-        //属性校验
         Assert.judgeAttrValue(reqJson);
     }
 
     @Override
     @Java110Transactional
     public void doCmd(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) throws CmdException {
-        addParkingArea(reqJson);
-
-        dealAttr(reqJson);
-    }
-
-    public void addParkingArea(JSONObject paramInJson) {
 
         JSONObject businessParkingArea = new JSONObject();
-        businessParkingArea.putAll(paramInJson);
-        businessParkingArea.put("paId", GenerateCodeFactory.getGeneratorId(GenerateCodeFactory.CODE_PREFIX_paId));
+        businessParkingArea.putAll(reqJson);
         ParkingAreaPo parkingAreaPo = BeanConvertUtil.covertBean(businessParkingArea, ParkingAreaPo.class);
-        //super.insert(dataFlowContext, parkingAreaPo, BusinessTypeConstant.BUSINESS_TYPE_SAVE_PARKING_AREA);
-        int flag = parkingAreaV1InnerServiceSMOImpl.saveParkingArea(parkingAreaPo);
-
+        int flag = parkingAreaV1InnerServiceSMOImpl.updateParkingArea(parkingAreaPo);
         if (flag < 1) {
             throw new CmdException("保存停车场失败");
         }
-        paramInJson.put("paId", businessParkingArea.getString("paId"));
-    }
-
-    private void dealAttr(JSONObject paramObj) {
-
-        if (!paramObj.containsKey("attrs")) {
+        if (!reqJson.containsKey("attrs")) {
             return;
         }
 
-        JSONArray attrs = paramObj.getJSONArray("attrs");
+        JSONArray attrs = reqJson.getJSONArray("attrs");
         if (attrs.size() < 1) {
             return;
         }
 
-
         JSONObject attr = null;
-        int flag = 0;
+        ParkingAreaAttrPo parkingAreaAttrPo = null;
         for (int attrIndex = 0; attrIndex < attrs.size(); attrIndex++) {
             attr = attrs.getJSONObject(attrIndex);
-            attr.put("communityId", paramObj.getString("communityId"));
-            attr.put("paId", paramObj.getString("paId"));
-            attr.put("attrId", GenerateCodeFactory.getGeneratorId(GenerateCodeFactory.CODE_PREFIX_attrId));
-            // parkingAreaAttrBMOImpl.addParkingAreaAttr(attr, context);
-            ParkingAreaAttrPo parkingAreaAttrPo = BeanConvertUtil.covertBean(attr, ParkingAreaAttrPo.class);
-            flag = parkingAreaAttrV1InnerServiceSMOImpl.saveParkingAreaAttr(parkingAreaAttrPo);
+            attr.put("communityId", reqJson.getString("communityId"));
+            if (!attr.containsKey("attrId") || attr.getString("attrId").startsWith("-") || StringUtil.isEmpty(attr.getString("attrId"))) {
+                attr.put("attrId", GenerateCodeFactory.getGeneratorId(GenerateCodeFactory.CODE_PREFIX_attrId));
+                parkingAreaAttrPo = BeanConvertUtil.covertBean(attr, ParkingAreaAttrPo.class);
+                flag = parkingAreaAttrV1InnerServiceSMOImpl.saveParkingAreaAttr(parkingAreaAttrPo);
+                if (flag < 1) {
+                    throw new CmdException("保存停车场失败");
+                }
+                continue;
+            }
+            parkingAreaAttrPo = BeanConvertUtil.covertBean(attr, ParkingAreaAttrPo.class);
+            flag = parkingAreaAttrV1InnerServiceSMOImpl.updateParkingAreaAttr(parkingAreaAttrPo);
             if (flag < 1) {
                 throw new CmdException("保存停车场失败");
             }
         }
-
     }
 }
