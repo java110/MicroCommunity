67cb374d6e5d27444f34dd007a20b85e3acf8739
java110 <928255095@qq.com>
2022-04-22 13:06:13 +0800

    优化 权限 提示 多语言版本

---
+++ java110-generator/src/main/java/com/java110/code/DealHtml.java
@@ -0,0 +1,12 @@
+package com.java110.code;
+
+/**
+ * @ClassName DealHtml
+ * @Description TODO
+ * @Author wuxw
+ * @Date 2022/4/20 22:18
+ * @Version 1.0
+ * add by wuxw 2022/4/20
+ **/
+public class DealHtml {
+}
---
+++ java110-generator/src/main/java/com/java110/code/translate.java
@@ -0,0 +1,12 @@
+package com.java110.code;
+
+/**
+ * @ClassName translate
+ * @Description TODO
+ * @Author wuxw
+ * @Date 2022/4/21 10:37
+ * @Version 1.0
+ * add by wuxw 2022/4/21
+ **/
+public class translate {
+}
