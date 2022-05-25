863457d3588cdb826087a1ff5c72f2ae9075009c
java110 <928255095@qq.com>
2022-05-25 14:03:26 +0800

    优化 公众号 表结构

--- java110-bean/src/main/java/com/java110/po/smallWechat/SmallWechatPo.java
+++ java110-bean/src/main/java/com/java110/po/smallWechat/SmallWechatPo.java
@@ -16,7 +16,7 @@
 package com.java110.po.smallWechat;
 
 import java.io.Serializable;
-import java.util.Date;
+
 /**
  * 类表述： Po 数据模型实体对象 基本保持与数据库模型一直 用于 增加修改删除 等时的数据载体
  * add by 吴学文 at 2022-05-25 10:46:07 mail: 928255095@qq.com
@@ -28,97 +28,122 @@ import java.util.Date;
 public class SmallWechatPo implements Serializable {
 
     private String mchId;
-private String statusCd = "0";
-private String storeId;
-private String wechatType;
-private String certPath;
-private String appid;
-private String appsecret;
-private String name;
-private String objId;
-private String wechatId;
-private String payPassword;
-private String objType;
-private String remarks;
-public String getMchId() {
+    private String statusCd = "0";
+    private String storeId;
+    private String wechatType;
+    private String certPath;
+    private String appId;
+    private String appSecret;
+    private String name;
+    private String objId;
+    private String wechatId;
+    private String payPassword;
+    private String objType;
+    private String remarks;
+
+    public String getMchId() {
         return mchId;
     }
-public void setMchId(String mchId) {
+
+    public void setMchId(String mchId) {
         this.mchId = mchId;
     }
-public String getStatusCd() {
+
+    public String getStatusCd() {
         return statusCd;
     }
-public void setStatusCd(String statusCd) {
+
+    public void setStatusCd(String statusCd) {
         this.statusCd = statusCd;
     }
-public String getStoreId() {
+
+    public String getStoreId() {
         return storeId;
     }
-public void setStoreId(String storeId) {
+
+    public void setStoreId(String storeId) {
         this.storeId = storeId;
     }
-public String getWechatType() {
+
+    public String getWechatType() {
         return wechatType;
     }
-public void setWechatType(String wechatType) {
+
+    public void setWechatType(String wechatType) {
         this.wechatType = wechatType;
     }
-public String getCertPath() {
+
+    public String getCertPath() {
         return certPath;
     }
-public void setCertPath(String certPath) {
+
+    public void setCertPath(String certPath) {
         this.certPath = certPath;
     }
-public String getAppid() {
-        return appid;
-    }
-public void setAppid(String appid) {
-        this.appid = appid;
-    }
-public String getAppsecret() {
-        return appsecret;
-    }
-public void setAppsecret(String appsecret) {
-        this.appsecret = appsecret;
-    }
-public String getName() {
-        return name;
-    }
-public void setName(String name) {
-        this.name = name;
-    }
-public String getObjId() {
-        return objId;
-    }
-public void setObjId(String objId) {
-        this.objId = objId;
-    }
-public String getWechatId() {
-        return wechatId;
-    }
-public void setWechatId(String wechatId) {
-        this.wechatId = wechatId;
-    }
-public String getPayPassword() {
-        return payPassword;
-    }
-public void setPayPassword(String payPassword) {
-        this.payPassword = payPassword;
-    }
-public String getObjType() {
-        return objType;
-    }
-public void setObjType(String objType) {
-        this.objType = objType;
-    }
-public String getRemarks() {
-        return remarks;
-    }
-public void setRemarks(String remarks) {
-        this.remarks = remarks;
+
+    public String getAppId() {
+        return appId;
     }
 
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getAppSecret() {
+        return appSecret;
+    }
+
+    public void setAppSecret(String appSecret) {
+        this.appSecret = appSecret;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getObjId() {
+        return objId;
+    }
+
+    public void setObjId(String objId) {
+        this.objId = objId;
+    }
+
+    public String getWechatId() {
+        return wechatId;
+    }
+
+    public void setWechatId(String wechatId) {
+        this.wechatId = wechatId;
+    }
+
+    public String getPayPassword() {
+        return payPassword;
+    }
+
+    public void setPayPassword(String payPassword) {
+        this.payPassword = payPassword;
+    }
+
+    public String getObjType() {
+        return objType;
+    }
+
+    public void setObjType(String objType) {
+        this.objType = objType;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
 
 
 }
