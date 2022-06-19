e59a56ff250f2478b37ea964c1a2edbf46353524
java110 <928255095@qq.com>
2022-06-19 11:03:33 +0800

    活动改为 cmd模式

--- service-community/src/main/java/com/java110/community/cmd/activities/SaveActivitiesCmd.java
+++ service-community/src/main/java/com/java110/community/cmd/activities/SaveActivitiesCmd.java
@@ -1,10 +1,27 @@
-package com.java110.api.listener.activities;
+/*
+ * Copyright 2017-2020 吴学文 and java110 team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.java110.community.cmd.activities;
 
 import com.alibaba.fastjson.JSONObject;
-import com.java110.api.listener.AbstractServiceApiPlusListener;
-import com.java110.core.annotation.Java110Listener;
+import com.java110.core.annotation.Java110Cmd;
+import com.java110.core.annotation.Java110Transactional;
 import com.java110.core.context.DataFlowContext;
-import com.java110.core.event.service.api.ServiceDataFlowEvent;
+import com.java110.core.context.ICmdDataFlowContext;
+import com.java110.core.event.cmd.Cmd;
+import com.java110.core.event.cmd.CmdEvent;
 import com.java110.core.factory.GenerateCodeFactory;
 import com.java110.dto.activitiesType.ActivitiesTypeDto;
 import com.java110.dto.community.CommunityDto;
@@ -13,7 +30,9 @@ import com.java110.dto.org.OrgCommunityDto;
 import com.java110.dto.org.OrgDto;
 import com.java110.dto.org.OrgStaffRelDto;
 import com.java110.intf.common.IFileInnerServiceSMO;
+import com.java110.intf.common.IFileRelInnerServiceSMO;
 import com.java110.intf.community.IActivitiesTypeInnerServiceSMO;
+import com.java110.intf.community.IActivitiesV1InnerServiceSMO;
 import com.java110.intf.community.ICommunityInnerServiceSMO;
 import com.java110.intf.user.IOrgCommunityInnerServiceSMO;
 import com.java110.intf.user.IOrgInnerServiceSMO;
@@ -21,24 +40,38 @@ import com.java110.intf.user.IOrgStaffRelInnerServiceSMO;
 import com.java110.po.activities.ActivitiesPo;
 import com.java110.po.file.FileRelPo;
 import com.java110.utils.constant.BusinessTypeConstant;
-import com.java110.utils.constant.ServiceCodeActivitiesConstant;
 import com.java110.utils.constant.StateConstant;
+import com.java110.utils.exception.CmdException;
 import com.java110.utils.util.Assert;
 import com.java110.utils.util.BeanConvertUtil;
+import com.java110.vo.ResultVo;
 import com.java110.vo.api.community.ApiCommunityDataVo;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 /**
- * 保存小区侦听
- * add by wuxw 2019-06-30
+ * 类表述：保存
+ * 服务编码：activities.saveActivities
+ * 请求路劲：/app/activities.SaveActivities
+ * add by 吴学文 at 2022-06-19 10:49:17 mail: 928255095@qq.com
+ * open source address: https://gitee.com/wuxw7/MicroCommunity
+ * 官网：http://www.homecommunity.cn
+ * 温馨提示：如果您对此文件进行修改 请不要删除原有作者及注释信息，请补充您的 修改的原因以及联系邮箱如下
+ * // modify by 张三 at 2021-09-12 第10行在某种场景下存在某种bug 需要修复，注释10至20行 加入 20行至30行
  */
-@Java110Listener("saveActivitiesListener")
-public class SaveActivitiesListener extends AbstractServiceApiPlusListener {
+@Java110Cmd(serviceCode = "activities.saveActivities")
+public class SaveActivitiesCmd extends Cmd {
 
+    private static Logger logger = LoggerFactory.getLogger(SaveActivitiesCmd.class);
+
+    public static final String CODE_PREFIX_ID = "10";
+
+    @Autowired
+    private IActivitiesV1InnerServiceSMO activitiesV1InnerServiceSMOImpl;
     @Autowired
     private IFileInnerServiceSMO fileInnerServiceSMOImpl;
 
@@ -57,8 +90,13 @@ public class SaveActivitiesListener extends AbstractServiceApiPlusListener {
     @Autowired
     private IOrgInnerServiceSMO orgInnerServiceSMOImpl;
 
+
+    @Autowired
+    private IFileRelInnerServiceSMO fileRelInnerServiceSMOImpl;
+
+
     @Override
-    protected void validate(ServiceDataFlowEvent event, JSONObject reqJson) {
+    public void validate(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) {
         //Assert.hasKeyAndValue(reqJson, "xxx", "xxx");
         Assert.hasKeyAndValue(reqJson, "title", "必填，请填写业活动标题");
         Assert.hasKeyAndValue(reqJson, "typeCd", "必填，请选择活动类型");
@@ -71,9 +109,10 @@ public class SaveActivitiesListener extends AbstractServiceApiPlusListener {
     }
 
     @Override
-    protected void doSoService(ServiceDataFlowEvent event, DataFlowContext context, JSONObject reqJson) {
+    @Java110Transactional
+    public void doCmd(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) throws CmdException {
         if (!reqJson.containsKey("isMoreCommunity") || "N".equals(reqJson.getString("isMoreCommunity"))) {
-            addActivities(context, reqJson);
+            addActivities(cmdDataFlowContext, reqJson);
             return;
         }
 
@@ -100,7 +139,7 @@ public class SaveActivitiesListener extends AbstractServiceApiPlusListener {
                 continue;
             }
             reqJson.put("typeCd", activitiesTypeDtos.get(0).getTypeCd());
-            addActivities(context, reqJson);
+            addActivities(cmdDataFlowContext, reqJson);
         }
     }
 
@@ -140,7 +179,7 @@ public class SaveActivitiesListener extends AbstractServiceApiPlusListener {
         return communitys;
     }
 
-    public void addActivities(DataFlowContext context, JSONObject reqJson) {
+    public void addActivities(ICmdDataFlowContext context, JSONObject reqJson) {
         reqJson.put("activitiesId", GenerateCodeFactory.getGeneratorId(GenerateCodeFactory.CODE_PREFIX_activitiesId));
         if (reqJson.containsKey("headerImg") && !StringUtils.isEmpty(reqJson.getString("headerImg"))) {
             FileDto fileDto = new FileDto();
@@ -161,30 +200,20 @@ public class SaveActivitiesListener extends AbstractServiceApiPlusListener {
             fileRelPo.setObjId(reqJson.getString("activitiesId"));
             fileRelPo.setSaveWay("table");
             fileRelPo.setRelTypeCd("70000");
-            super.insert(context, fileRelPo, BusinessTypeConstant.BUSINESS_TYPE_SAVE_FILE_REL);
+            int flag = fileRelInnerServiceSMOImpl.saveFileRel(fileRelPo);
+            if(flag < 1){
+                throw new CmdException("保存广告失败");
+            }
         }
         ActivitiesPo activitiesPo = BeanConvertUtil.covertBean(reqJson, ActivitiesPo.class);
         activitiesPo.setReadCount("0");
         activitiesPo.setLikeCount("0");
         activitiesPo.setCollectCount("0");
         activitiesPo.setState("11000");
-        //添加单元信息
-        super.insert(context, activitiesPo, BusinessTypeConstant.BUSINESS_TYPE_SAVE_ACTIVITIES);
-    }
 
-
-    @Override
-    public String getServiceCode() {
-        return ServiceCodeActivitiesConstant.ADD_ACTIVITIES;
-    }
-
-    @Override
-    public HttpMethod getHttpMethod() {
-        return HttpMethod.POST;
-    }
-
-    @Override
-    public int getOrder() {
-        return DEFAULT_ORDER;
+        int flag = activitiesV1InnerServiceSMOImpl.saveActivities(activitiesPo);
+        if(flag < 1){
+            throw new CmdException("保存广告失败");
+        }
     }
 }
--- service-community/src/main/java/com/java110/community/cmd/activities/UpdateActivitiesCmd.java
+++ service-community/src/main/java/com/java110/community/cmd/activities/UpdateActivitiesCmd.java
@@ -1,51 +1,75 @@
-package com.java110.api.listener.activities;
+/*
+ * Copyright 2017-2020 吴学文 and java110 team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.java110.community.cmd.activities;
 
 import com.alibaba.fastjson.JSONObject;
-import com.java110.api.bmo.activities.IActivitiesBMO;
-import com.java110.api.listener.AbstractServiceApiPlusListener;
-import com.java110.core.annotation.Java110Listener;
-import com.java110.core.context.DataFlowContext;
-import com.java110.core.event.service.api.ServiceDataFlowEvent;
+import com.java110.core.annotation.Java110Cmd;
+import com.java110.core.annotation.Java110Transactional;
+import com.java110.core.context.ICmdDataFlowContext;
+import com.java110.core.event.cmd.Cmd;
+import com.java110.core.event.cmd.CmdEvent;
 import com.java110.core.factory.GenerateCodeFactory;
-import com.java110.intf.community.IActivitiesInnerServiceSMO;
-import com.java110.intf.common.IFileInnerServiceSMO;
-import com.java110.intf.common.IFileRelInnerServiceSMO;
 import com.java110.dto.file.FileDto;
 import com.java110.dto.file.FileRelDto;
+import com.java110.intf.common.IFileInnerServiceSMO;
+import com.java110.intf.common.IFileRelInnerServiceSMO;
+import com.java110.intf.community.IActivitiesInnerServiceSMO;
+import com.java110.intf.community.IActivitiesV1InnerServiceSMO;
 import com.java110.po.activities.ActivitiesPo;
 import com.java110.po.file.FileRelPo;
 import com.java110.utils.constant.BusinessTypeConstant;
-import com.java110.utils.constant.ServiceCodeActivitiesConstant;
+import com.java110.utils.exception.CmdException;
 import com.java110.utils.util.Assert;
 import com.java110.utils.util.BeanConvertUtil;
+import com.java110.vo.ResultVo;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
+
 /**
- * 保存活动侦听
- * add by wuxw 2019-06-30
+ * 类表述：更新
+ * 服务编码：activities.updateActivities
+ * 请求路劲：/app/activities.UpdateActivities
+ * add by 吴学文 at 2022-06-19 10:49:17 mail: 928255095@qq.com
+ * open source address: https://gitee.com/wuxw7/MicroCommunity
+ * 官网：http://www.homecommunity.cn
+ * 温馨提示：如果您对此文件进行修改 请不要删除原有作者及注释信息，请补充您的 修改的原因以及联系邮箱如下
+ * // modify by 张三 at 2021-09-12 第10行在某种场景下存在某种bug 需要修复，注释10至20行 加入 20行至30行
  */
-@Java110Listener("updateActivitiesListener")
-public class UpdateActivitiesListener extends AbstractServiceApiPlusListener {
+@Java110Cmd(serviceCode = "activities.updateActivities")
+public class UpdateActivitiesCmd extends Cmd {
 
-    @Autowired
-    private IActivitiesInnerServiceSMO activitiesInnerServiceSMOImpl;
-
-    @Autowired
-    private IActivitiesBMO activitiesBMOImpl;
+  private static Logger logger = LoggerFactory.getLogger(UpdateActivitiesCmd.class);
 
     @Autowired
     private IFileInnerServiceSMO fileInnerServiceSMOImpl;
 
     @Autowired
     private IFileRelInnerServiceSMO fileRelInnerServiceSMOImpl;
+    @Autowired
+    private IActivitiesV1InnerServiceSMO activitiesV1InnerServiceSMOImpl;
+    @Autowired
+    private IActivitiesInnerServiceSMO activitiesInnerServiceSMOImpl;
 
     @Override
-    protected void validate(ServiceDataFlowEvent event, JSONObject reqJson) {
-
+    public void validate(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) {
         Assert.hasKeyAndValue(reqJson, "activitiesId", "活动ID不能为空");
         Assert.hasKeyAndValue(reqJson, "communityId", "小区ID不能为空");
         Assert.hasKeyAndValue(reqJson, "title", "必填，请填写业活动标题");
@@ -57,8 +81,10 @@ public class UpdateActivitiesListener extends AbstractServiceApiPlusListener {
 
     }
 
+
     @Override
-    protected void doSoService(ServiceDataFlowEvent event, DataFlowContext context, JSONObject reqJson) {
+    @Java110Transactional
+    public void doCmd(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) throws CmdException {
 
 
         if (reqJson.containsKey("headerImg") && !StringUtils.isEmpty(reqJson.getString("headerImg"))) {
@@ -80,45 +106,37 @@ public class UpdateActivitiesListener extends AbstractServiceApiPlusListener {
 
             if (fileRelDtos == null || fileRelDtos.size() == 0) {
                 FileRelPo fileRelPo = new FileRelPo();
-                fileRelPo.setFileRelId("-1");
+                fileRelPo.setFileRelId(GenerateCodeFactory.getGeneratorId(GenerateCodeFactory.CODE_PREFIX_fileRelId));
                 fileRelPo.setFileRealName(reqJson.getString("headerImg"));
                 fileRelPo.setFileSaveName(reqJson.getString("fileSaveName"));
                 fileRelPo.setObjId(reqJson.getString("activitiesId"));
                 fileRelPo.setSaveWay("table");
                 fileRelPo.setRelTypeCd("70000");
-                super.insert(context, fileRelPo, BusinessTypeConstant.BUSINESS_TYPE_SAVE_FILE_REL);
+                int flag = fileRelInnerServiceSMOImpl.saveFileRel(fileRelPo);
+                if(flag < 1){
+                    throw new CmdException("保存广告失败");
+                }
             } else {
                 FileRelPo fileRelPo = new FileRelPo();
                 fileRelPo.setFileRelId(fileRelDtos.get(0).getFileRelId());
                 fileRelPo.setFileRealName(reqJson.getString("headerImg"));
                 fileRelPo.setFileSaveName(reqJson.getString("fileSaveName"));
                 fileRelPo.setObjId(reqJson.getString("activitiesId"));
-                super.update(context, fileRelPo, BusinessTypeConstant.BUSINESS_TYPE_UPDATE_FILE_REL);
+                int flag = fileRelInnerServiceSMOImpl.updateFileRel(fileRelPo);
+                if(flag < 1){
+                    throw new CmdException("保存广告失败");
+                }
             }
 
         }
 
         ActivitiesPo activitiesPo = BeanConvertUtil.covertBean(reqJson, ActivitiesPo.class);
+        int flag = activitiesV1InnerServiceSMOImpl.updateActivities(activitiesPo);
 
-        super.update(context, activitiesPo, BusinessTypeConstant.BUSINESS_TYPE_UPDATE_ACTIVITIES);
+        if (flag < 1) {
+            throw new CmdException("更新数据失败");
+        }
 
+        cmdDataFlowContext.setResponseEntity(ResultVo.success());
     }
-
-
-    @Override
-    public String getServiceCode() {
-        return ServiceCodeActivitiesConstant.UPDATE_ACTIVITIES;
-    }
-
-    @Override
-    public HttpMethod getHttpMethod() {
-        return HttpMethod.POST;
-    }
-
-    @Override
-    public int getOrder() {
-        return DEFAULT_ORDER;
-    }
-
-
 }
