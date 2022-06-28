c52c1612364e3460a903a05bdebc0372a39b333e
java110 <928255095@qq.com>
2022-06-28 11:40:13 +0800

    优化databus bug

--- service-user/src/main/java/com/java110/user/cmd/owner/DeleteOwnerCarsCmd.java
+++ service-user/src/main/java/com/java110/user/cmd/owner/DeleteOwnerCarsCmd.java
@@ -2,6 +2,7 @@ package com.java110.user.cmd.owner;
 
 import com.alibaba.fastjson.JSONObject;
 import com.java110.core.annotation.Java110Cmd;
+import com.java110.core.annotation.Java110Transactional;
 import com.java110.core.context.ICmdDataFlowContext;
 import com.java110.core.event.cmd.Cmd;
 import com.java110.core.event.cmd.CmdEvent;
@@ -71,6 +72,7 @@ public class DeleteOwnerCarsCmd extends Cmd {
     }
 
     @Override
+    @Java110Transactional
     public void doCmd(CmdEvent event, ICmdDataFlowContext context, JSONObject reqJson) throws CmdException {
         if (reqJson.containsKey("psId") && !StringUtil.isEmpty(reqJson.getString("psId"))) {
             ParkingSpaceDto parkingSpaceDto = new ParkingSpaceDto();
