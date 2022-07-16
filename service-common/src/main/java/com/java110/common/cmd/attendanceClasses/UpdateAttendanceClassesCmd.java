a31bca84f6bb35acd219ed18a3e49f21c6511c31
java110 <928255095@qq.com>
2022-07-16 18:07:58 +0800

    优化考勤工鞥呢

---
+++ service-common/src/main/java/com/java110/common/cmd/attendanceClasses/UpdateAttendanceClassesCmd.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2017-2020 吴学文 and java110 team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.java110.common.cmd.attendanceClasses;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.java110.core.annotation.Java110Cmd;
+import com.java110.core.annotation.Java110Transactional;
+import com.java110.core.context.ICmdDataFlowContext;
+import com.java110.core.event.cmd.Cmd;
+import com.java110.core.event.cmd.CmdEvent;
+import com.java110.core.factory.GenerateCodeFactory;
+import com.java110.intf.common.IAttendanceClassesAttrV1InnerServiceSMO;
+import com.java110.intf.common.IAttendanceClassesV1InnerServiceSMO;
+import com.java110.po.attendanceClasses.AttendanceClassesPo;
+import com.java110.po.attendanceClassesAttr.AttendanceClassesAttrPo;
+import com.java110.utils.exception.CmdException;
+import com.java110.utils.util.Assert;
+import com.java110.utils.util.BeanConvertUtil;
+import com.java110.utils.util.StringUtil;
+import com.java110.vo.ResultVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+/**
+ * 类表述：更新
+ * 服务编码：attendanceClasses.updateAttendanceClasses
+ * 请求路劲：/app/attendanceClasses.UpdateAttendanceClasses
+ * add by 吴学文 at 2022-07-16 17:50:14 mail: 928255095@qq.com
+ * open source address: https://gitee.com/wuxw7/MicroCommunity
+ * 官网：http://www.homecommunity.cn
+ * 温馨提示：如果您对此文件进行修改 请不要删除原有作者及注释信息，请补充您的 修改的原因以及联系邮箱如下
+ * // modify by 张三 at 2021-09-12 第10行在某种场景下存在某种bug 需要修复，注释10至20行 加入 20行至30行
+ */
+@Java110Cmd(serviceCode = "attendanceClasses.updateAttendanceClasses")
+public class UpdateAttendanceClassesCmd extends Cmd {
+
+    private static Logger logger = LoggerFactory.getLogger(UpdateAttendanceClassesCmd.class);
+
+
+    @Autowired
+    private IAttendanceClassesV1InnerServiceSMO attendanceClassesV1InnerServiceSMOImpl;
+
+    @Autowired
+    private IAttendanceClassesAttrV1InnerServiceSMO attendanceClassesAttrV1InnerServiceSMOImpl;
+
+    @Override
+    public void validate(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) {
+        Assert.hasKeyAndValue(reqJson, "classesId", "classesId不能为空");
+        Assert.hasKeyAndValue(reqJson, "storeId", "storeId不能为空");
+
+    }
+
+    @Override
+    @Java110Transactional
+    public void doCmd(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) throws CmdException {
+
+        AttendanceClassesPo attendanceClassesPo = BeanConvertUtil.covertBean(reqJson, AttendanceClassesPo.class);
+        int flag = attendanceClassesV1InnerServiceSMOImpl.updateAttendanceClasses(attendanceClassesPo);
+
+        if (flag < 1) {
+            throw new CmdException("更新数据失败");
+        }
+
+        JSONArray attrs = reqJson.getJSONArray("attrs");
+        if (attrs == null || attrs.size() < 1) {
+            return;
+        }
+
+        JSONObject attr = null;
+        for (int attrIndex = 0; attrIndex < attrs.size(); attrIndex++) {
+            attr = attrs.getJSONObject(attrIndex);
+            attr.put("classesId", reqJson.getString("classesId"));
+            attr.put("storeId", reqJson.getString("storeId"));
+            if (!attr.containsKey("attrId") || attr.getString("attrId").startsWith("-") || StringUtil.isEmpty(attr.getString("attrId"))) {
+                attr.put("attrId", GenerateCodeFactory.getGeneratorId("11"));
+                AttendanceClassesAttrPo attendanceClassesAttrPo = BeanConvertUtil.covertBean(attr, AttendanceClassesAttrPo.class);
+                flag = attendanceClassesAttrV1InnerServiceSMOImpl.saveAttendanceClassesAttr(attendanceClassesAttrPo);
+                if (flag < 1) {
+                    throw new CmdException("保存数据失败");
+                }
+                continue;
+            }
+            AttendanceClassesAttrPo attendanceClassesAttrPo = BeanConvertUtil.covertBean(attr, AttendanceClassesAttrPo.class);
+            flag = attendanceClassesAttrV1InnerServiceSMOImpl.updateAttendanceClassesAttr(attendanceClassesAttrPo);
+            if (flag < 1) {
+                throw new CmdException("保存数据失败");
+            }
+        }
+        cmdDataFlowContext.setResponseEntity(ResultVo.success());
+    }
+}
