dce7552e8f21411542e1ab6854ddcac22b724155
wuxw <928255095@qq.com>
2022-07-18 23:21:31 +0800

    优化代码

--- service-user/src/main/java/com/java110/user/cmd/login/UserServiceLoginCmd.java
+++ service-user/src/main/java/com/java110/user/cmd/login/UserServiceLoginCmd.java
@@ -1,47 +1,45 @@
-package com.java110.api.listener.login;
+package com.java110.user.cmd.login;
 
 import com.alibaba.fastjson.JSONObject;
-import com.java110.api.listener.AbstractServiceApiDataFlowListener;
-import com.java110.core.annotation.Java110Listener;
-import com.java110.core.context.DataFlowContext;
-import com.java110.core.event.service.api.ServiceDataFlowEvent;
+import com.java110.core.annotation.Java110Cmd;
+import com.java110.core.context.ICmdDataFlowContext;
+import com.java110.core.event.cmd.Cmd;
+import com.java110.core.event.cmd.CmdEvent;
 import com.java110.core.factory.AuthenticationFactory;
-import com.java110.core.factory.DataFlowFactory;
 import com.java110.core.factory.GenerateCodeFactory;
+import com.java110.core.log.LoggerFactory;
 import com.java110.dto.store.StoreDto;
 import com.java110.dto.store.StoreUserDto;
 import com.java110.dto.userLogin.UserLoginDto;
-import com.java110.entity.center.AppService;
 import com.java110.intf.store.IStoreInnerServiceSMO;
 import com.java110.intf.user.IUserInnerServiceSMO;
 import com.java110.intf.user.IUserLoginInnerServiceSMO;
 import com.java110.po.userLogin.UserLoginPo;
+import com.java110.service.context.DataQuery;
+import com.java110.service.smo.IQueryServiceSMO;
 import com.java110.utils.constant.CommonConstant;
 import com.java110.utils.constant.ResponseConstant;
 import com.java110.utils.constant.ServiceCodeConstant;
+import com.java110.utils.exception.CmdException;
 import com.java110.utils.exception.SMOException;
 import com.java110.utils.util.Assert;
 import com.java110.utils.util.DateUtil;
 import org.slf4j.Logger;
-import com.java110.core.log.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * 用户注册 侦听
- * Created by wuxw on 2018/5/18.
- */
-@Java110Listener("userLoginServiceListener")
-public class UserLoginServiceListener extends AbstractServiceApiDataFlowListener {
+@Java110Cmd(serviceCode = "user.service.login")
+public class UserServiceLoginCmd extends Cmd{
 
-    private final static Logger logger = LoggerFactory.getLogger(UserLoginServiceListener.class);
+    private final static Logger logger = LoggerFactory.getLogger(UserServiceLoginCmd.class);
 
+    @Autowired
+    private IQueryServiceSMO queryServiceSMOImpl;
 
     @Autowired
     private IUserLoginInnerServiceSMO userLoginInnerServiceSMOImpl;
@@ -51,86 +49,43 @@ public class UserLoginServiceListener extends AbstractServiceApiDataFlowListener
 
     @Autowired
     private IUserInnerServiceSMO userInnerServiceSMOImpl;
-
     @Override
-    public String getServiceCode() {
-        return ServiceCodeConstant.SERVICE_CODE_USER_SERVICE_LOGIN;
+    public void validate(CmdEvent event, ICmdDataFlowContext context, JSONObject reqJson) throws CmdException {
+        Assert.jsonObjectHaveKey(reqJson, "username", "用户登录，未包含username节点，请检查" + reqJson);
+        Assert.jsonObjectHaveKey(reqJson, "passwd", "用户登录，未包含passwd节点，请检查" + reqJson);
     }
 
     @Override
-    public HttpMethod getHttpMethod() {
-        return HttpMethod.POST;
-    }
+    public void doCmd(CmdEvent event, ICmdDataFlowContext context, JSONObject reqJson) throws CmdException {
 
-
-    @Override
-    public int getOrder() {
-        return 0;
-    }
-
-
-    /**
-     * 请求参数格式：
-     * {
-     * "username":"admin",
-     * "passwd":"1234565"
-     * }
-     * 返回报文：
-     * {
-     * "userId":"",
-     * "token":"12dddd"
-     * }
-     *
-     * @param event
-     */
-    @Override
-    public void soService(ServiceDataFlowEvent event) {
-        //获取数据上下文对象
-        DataFlowContext dataFlowContext = event.getDataFlowContext();
-        AppService service = event.getAppService();
-        String paramIn = dataFlowContext.getReqData();
-        Assert.isJsonObject(paramIn, "用户注册请求参数有误，不是有效的json格式 " + paramIn);
-        Assert.jsonObjectHaveKey(paramIn, "username", "用户登录，未包含username节点，请检查" + paramIn);
-        Assert.jsonObjectHaveKey(paramIn, "passwd", "用户登录，未包含passwd节点，请检查" + paramIn);
-        RestTemplate restTemplate = super.getRestTemplate();
-        ResponseEntity responseEntity = null;
-        JSONObject paramInJson = JSONObject.parseObject(paramIn);
-        //根据AppId 查询 是否有登录的服务，查询登录地址调用
-        AppService appService = DataFlowFactory.getService(dataFlowContext.getAppId(), ServiceCodeConstant.SERVICE_CODE_QUERY_USER_LOGIN);
-        if (appService == null) {
-            responseEntity = new ResponseEntity<String>("当前没有权限访问" + ServiceCodeConstant.SERVICE_CODE_QUERY_USER_LOGIN, HttpStatus.UNAUTHORIZED);
-            dataFlowContext.setResponseEntity(responseEntity);
+        DataQuery dataQuery = new DataQuery();
+        dataQuery.setServiceCode(ServiceCodeConstant.SERVICE_CODE_QUERY_USER_LOGIN);
+        JSONObject param = new JSONObject();
+        param.put("userCode", reqJson.getString("username"));
+        param.put("pwd", reqJson.getString("passwd"));
+        param.put("levelCdTag","1");
+        dataQuery.setRequestParams(param);
+        queryServiceSMOImpl.commonQueryService(dataQuery);
+        ResponseEntity<String> responseEntity = dataQuery.getResponseEntity();
+        if (responseEntity.getStatusCode() != HttpStatus.OK) {
+            context.setResponseEntity(new ResponseEntity<>("初始化商户", HttpStatus.FORBIDDEN));
             return;
         }
-        String requestUrl = appService.getUrl() + "?userCode=" + paramInJson.getString("username") + "&pwd=" + paramInJson.getString("passwd") + "&levelCdTag=1";
-        HttpHeaders header = new HttpHeaders();
-        header.add(CommonConstant.HTTP_SERVICE.toLowerCase(), ServiceCodeConstant.SERVICE_CODE_QUERY_USER_LOGIN);
-        HttpEntity<String> httpEntity = new HttpEntity<String>("", header);
-        try {
-            responseEntity = restTemplate.exchange(requestUrl, HttpMethod.GET, httpEntity, String.class);
-        } catch (HttpStatusCodeException e) { //这里spring 框架 在4XX 或 5XX 时抛出 HttpServerErrorException 异常，需要重新封装一下
-            responseEntity = new ResponseEntity<String>("请求登录查询异常，" + e.getResponseBodyAsString(), e.getStatusCode());
-            dataFlowContext.setResponseEntity(responseEntity);
-            return;
-        }
+        Assert.isJsonObject(responseEntity.getBody(), "调用登录查询异常,返回报文有误，不是有效的json格式 " + responseEntity.getBody());
 
-        String resultBody = responseEntity.getBody().toString();
-
-        Assert.isJsonObject(resultBody, "调用登录查询异常,返回报文有误，不是有效的json格式 " + resultBody);
-
-        JSONObject resultInfo = JSONObject.parseObject(resultBody);
+        JSONObject resultInfo = JSONObject.parseObject(responseEntity.getBody());
         if (!resultInfo.containsKey("user") || !resultInfo.getJSONObject("user").containsKey("userPwd")
                 || !resultInfo.getJSONObject("user").containsKey("userId")) {
             responseEntity = new ResponseEntity<String>("用户或密码错误", HttpStatus.UNAUTHORIZED);
-            dataFlowContext.setResponseEntity(responseEntity);
+            context.setResponseEntity(responseEntity);
             return;
         }
 
         JSONObject userInfo = resultInfo.getJSONObject("user");
         String userPwd = userInfo.getString("userPwd");
-        if (!userPwd.equals(paramInJson.getString("passwd"))) {
+        if (!userPwd.equals(reqJson.getString("passwd"))) {
             responseEntity = new ResponseEntity<String>("密码错误", HttpStatus.UNAUTHORIZED);
-            dataFlowContext.setResponseEntity(responseEntity);
+            context.setResponseEntity(responseEntity);
             return;
         }
 
@@ -144,7 +99,7 @@ public class UserLoginServiceListener extends AbstractServiceApiDataFlowListener
             String state = storeUserDtos.get(0).getState();
             if ("48002".equals(state)) {
                 responseEntity = new ResponseEntity<String>("当前商户限制登录，请联系管理员", HttpStatus.UNAUTHORIZED);
-                dataFlowContext.setResponseEntity(responseEntity);
+                context.setResponseEntity(responseEntity);
                 return;
             }
 
@@ -174,7 +129,7 @@ public class UserLoginServiceListener extends AbstractServiceApiDataFlowListener
             userLoginPo.setUserName(userInfo.getString("userName"));
             userLoginInnerServiceSMOImpl.saveUserLogin(userLoginPo);
             responseEntity = new ResponseEntity<String>(userInfo.toJSONString(), HttpStatus.OK);
-            dataFlowContext.setResponseEntity(responseEntity);
+            context.setResponseEntity(responseEntity);
         } catch (Exception e) {
             logger.error("登录异常：", e);
             throw new SMOException(ResponseConstant.RESULT_CODE_INNER_ERROR, "系统内部错误，请联系管理员");
@@ -194,6 +149,4 @@ public class UserLoginServiceListener extends AbstractServiceApiDataFlowListener
 
         return paramObj;
     }
-
-
 }
