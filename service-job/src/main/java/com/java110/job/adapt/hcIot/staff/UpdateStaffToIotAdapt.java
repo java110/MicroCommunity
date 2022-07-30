b774e9835402a02e1d9bccf38d4df4affe3adeee
java110 <928255095@qq.com>
2022-07-30 18:47:53 +0800

    优化代码

--- service-job/src/main/java/com/java110/job/adapt/hcIot/staff/UpdateStaffToIotAdapt.java
+++ service-job/src/main/java/com/java110/job/adapt/hcIot/staff/UpdateStaffToIotAdapt.java
@@ -152,7 +152,7 @@ public class UpdateStaffToIotAdapt extends DatabusAdaptImpl {
                 storeUserObj.put("staffName", tmpOrgStaffRelDto.getStaffName());
                 storeUserObj.put("departmentId", tmpOrgStaffRelDto.getOrgId());
                 storeUserObj.put("departmentName", tmpOrgStaffRelDto.getOrgName());
-                if (machineDtos != null && machineDtos.size() < 1) {
+                if (machineDtos != null && machineDtos.size() > 0) {
                     storeUserObj.put("machineCode", machineDtos.get(0).getMachineCode());
                     storeUserObj.put("extMachineId", machineDtos.get(0).getMachineId());
                     storeUserObj.put("extCommunityId", machineDtos.get(0).getCommunityId());
