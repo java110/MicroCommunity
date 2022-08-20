fc0286488ad3dbb187ae67dfbe4777f2cc8bff0a
java110 <928255095@qq.com>
2022-08-20 17:15:18 +0800

    优化common

--- java110-interface/src/main/java/com/java110/intf/api/IApiCallBackInnerServiceSMO.java
+++ java110-interface/src/main/java/com/java110/intf/api/IApiCallBackInnerServiceSMO.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "${java110.api-service}", configuration = {FeignConfiguration.class})
+@FeignClient(name = "api-service", configuration = {FeignConfiguration.class})
 @RequestMapping("/apiCallBack")
 public interface IApiCallBackInnerServiceSMO {
 
