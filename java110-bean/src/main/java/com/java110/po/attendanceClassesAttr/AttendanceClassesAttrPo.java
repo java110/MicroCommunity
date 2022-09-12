9837f9e18174e3a66e225b4efe56791cee9db1b2
java110 <928255095@qq.com>
2022-09-12 15:53:53 +0800

    优化代码

--- java110-bean/src/main/java/com/java110/po/attendanceClassesAttr/AttendanceClassesAttrPo.java
+++ java110-bean/src/main/java/com/java110/po/attendanceClassesAttr/AttendanceClassesAttrPo.java
@@ -1,44 +1,53 @@
 package com.java110.po.attendanceClassesAttr;
 
 import java.io.Serializable;
-import java.util.Date;
 
 public class AttendanceClassesAttrPo implements Serializable {
 
     private String classesId;
-private String attrId;
-private String specCd;
-private String storeId;
-private String value;
-private String statusCd;
-public String getClassesId() {
+    private String attrId;
+    private String specCd;
+    private String storeId;
+    private String value;
+    private String statusCd = "0";
+
+    public String getClassesId() {
         return classesId;
     }
-public void setClassesId(String classesId) {
+
+    public void setClassesId(String classesId) {
         this.classesId = classesId;
     }
-public String getAttrId() {
+
+    public String getAttrId() {
         return attrId;
     }
-public void setAttrId(String attrId) {
+
+    public void setAttrId(String attrId) {
         this.attrId = attrId;
     }
-public String getSpecCd() {
+
+    public String getSpecCd() {
         return specCd;
     }
-public void setSpecCd(String specCd) {
+
+    public void setSpecCd(String specCd) {
         this.specCd = specCd;
     }
-public String getStoreId() {
+
+    public String getStoreId() {
         return storeId;
     }
-public void setStoreId(String storeId) {
+
+    public void setStoreId(String storeId) {
         this.storeId = storeId;
     }
-public String getValue() {
+
+    public String getValue() {
         return value;
     }
-public void setValue(String value) {
+
+    public void setValue(String value) {
         this.value = value;
     }
 
