27f0a5e5e7ce27613d484c5758376d14ea961c6c
wuxw <928255095@qq.com>
2022-09-28 16:58:07 +0800

    优化代码

---
+++ java110-bean/src/main/java/com/java110/dto/dataPrivilegeStaff/DataPrivilegeStaffDto.java
@@ -0,0 +1,83 @@
+package com.java110.dto.dataPrivilegeStaff;
+
+import com.java110.dto.PageDto;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @ClassName FloorDto
+ * @Description 数据权限员工关系数据层封装
+ * @Author wuxw
+ * @Date 2019/4/24 8:52
+ * @Version 1.0
+ * add by wuxw 2019/4/24
+ **/
+public class DataPrivilegeStaffDto extends PageDto implements Serializable {
+
+    private String dpsId;
+private String dpId;
+private String staffName;
+private String remark;
+private String communityId;
+private String staffId;
+
+
+    private Date createTime;
+
+    private String statusCd = "0";
+
+
+    public String getDpsId() {
+        return dpsId;
+    }
+public void setDpsId(String dpsId) {
+        this.dpsId = dpsId;
+    }
+public String getDpId() {
+        return dpId;
+    }
+public void setDpId(String dpId) {
+        this.dpId = dpId;
+    }
+public String getStaffName() {
+        return staffName;
+    }
+public void setStaffName(String staffName) {
+        this.staffName = staffName;
+    }
+public String getRemark() {
+        return remark;
+    }
+public void setRemark(String remark) {
+        this.remark = remark;
+    }
+public String getCommunityId() {
+        return communityId;
+    }
+public void setCommunityId(String communityId) {
+        this.communityId = communityId;
+    }
+public String getStaffId() {
+        return staffId;
+    }
+public void setStaffId(String staffId) {
+        this.staffId = staffId;
+    }
+
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStatusCd() {
+        return statusCd;
+    }
+
+    public void setStatusCd(String statusCd) {
+        this.statusCd = statusCd;
+    }
+}
