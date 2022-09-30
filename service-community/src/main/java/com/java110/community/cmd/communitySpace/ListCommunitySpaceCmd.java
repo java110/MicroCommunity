fe1e0029c72c3c25ddfcc349fd72a15fe61573ae
java110 <928255095@qq.com>
2022-09-30 11:13:01 +0800

    加入场地管理功能

--- service-community/src/main/java/com/java110/community/cmd/communitySpace/ListCommunitySpaceCmd.java
+++ service-community/src/main/java/com/java110/community/cmd/communitySpace/ListCommunitySpaceCmd.java
@@ -22,6 +22,7 @@ import com.java110.core.context.ICmdDataFlowContext;
 import com.java110.core.event.cmd.Cmd;
 import com.java110.core.event.cmd.CmdEvent;
 import com.java110.core.factory.GenerateCodeFactory;
+import com.java110.doc.annotation.*;
 import com.java110.intf.community.ICommunitySpaceV1InnerServiceSMO;
 import com.java110.po.communitySpace.CommunitySpacePo;
 import com.java110.utils.exception.CmdException;
@@ -37,7 +38,41 @@ import org.springframework.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Java110CmdDoc(title = "查询场地",
+        description = "查询系统中的查询场地",
+        httpMethod = "get",
+        url = "http://{ip}:{port}/app/communitySpace.listCommunitySpace",
+        resource = "communityDoc",
+        author = "吴学文",
+        serviceCode = "communitySpace.listCommunitySpace"
+)
 
+@Java110ParamsDoc(params = {
+        @Java110ParamDoc(name = "page",type = "int",length = 11, remark = "分页页数"),
+        @Java110ParamDoc(name = "row",type = "int", length = 11, remark = "分页行数"),
+        @Java110ParamDoc(name = "communityId", length = 30, remark = "小区ID"),
+})
+
+@Java110ResponseDoc(
+        params = {
+                @Java110ParamDoc(name = "code", type = "int", length = 11, defaultValue = "0", remark = "返回编号，0 成功 其他失败"),
+                @Java110ParamDoc(name = "msg", type = "String", length = 250, defaultValue = "成功", remark = "描述"),
+                @Java110ParamDoc(name = "data", type = "Array", remark = "有效数据"),
+                @Java110ParamDoc(parentNodeName = "data",name = "communityId", type = "String", remark = "小区ID"),
+                @Java110ParamDoc(parentNodeName = "data",name = "name", type = "String", remark = "场地名称"),
+                @Java110ParamDoc(parentNodeName = "data",name = "state", type = "String", remark = "小区状态 1001 可预约状态 2002 不可以预约状态"),
+                @Java110ParamDoc(parentNodeName = "data",name = "startTime", type = "String", remark = "预约开始时间"),
+                @Java110ParamDoc(parentNodeName = "data",name = "endTime", type = "String", remark = "预约结束时间"),
+                @Java110ParamDoc(parentNodeName = "data",name = "feeMoney", type = "String", remark = "每小时费用"),
+                @Java110ParamDoc(parentNodeName = "data",name = "adminName", type = "String", remark = "管理员"),
+                @Java110ParamDoc(parentNodeName = "data",name = "tel", type = "String", remark = "联系电话"),
+        }
+)
+
+@Java110ExampleDoc(
+        reqBody="http://{ip}:{port}/app/communitySpace.listCommunitySpace?spaceId=&name=&state=&communityId=2022081539020475&page=1&row=10",
+        resBody="{\"code\":0,\"data\":[{\"adminName\":\"无需文\",\"communityId\":\"2022081539020475\",\"endTime\":\"06:10\",\"feeMoney\":\"10.00\",\"name\":\"体育场\",\"page\":-1,\"records\":0,\"row\":0,\"spaceId\":\"102022093043260007\",\"startTime\":\"05:05\",\"state\":\"1001\",\"statusCd\":\"0\",\"tel\":\"18909711443\",\"total\":0}],\"msg\":\"成功\",\"page\":0,\"records\":1,\"rows\":0,\"total\":2}"
+)
 /**
  * 类表述：查询
  * 服务编码：communitySpace.listCommunitySpace
