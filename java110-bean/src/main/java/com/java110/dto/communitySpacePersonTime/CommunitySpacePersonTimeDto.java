1fbcd8525213b65d73b7325bb1da76267c17fd03
java110 <928255095@qq.com>
2022-10-03 01:13:30 +0800

    加入预约时间

---
+++ java110-bean/src/main/java/com/java110/dto/communitySpacePersonTime/CommunitySpacePersonTimeDto.java
@@ -0,0 +1,76 @@
+package com.java110.dto.communitySpacePersonTime;
+
+import com.java110.dto.PageDto;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @ClassName FloorDto
+ * @Description 场地预约时间数据层封装
+ * @Author wuxw
+ * @Date 2019/4/24 8:52
+ * @Version 1.0
+ * add by wuxw 2019/4/24
+ **/
+public class CommunitySpacePersonTimeDto extends PageDto implements Serializable {
+
+    private String spaceId;
+private String cspId;
+private String hours;
+private String timeId;
+private String communityId;
+
+
+    private Date createTime;
+
+    private String statusCd = "0";
+
+
+    public String getSpaceId() {
+        return spaceId;
+    }
+public void setSpaceId(String spaceId) {
+        this.spaceId = spaceId;
+    }
+public String getCspId() {
+        return cspId;
+    }
+public void setCspId(String cspId) {
+        this.cspId = cspId;
+    }
+public String getHours() {
+        return hours;
+    }
+public void setHours(String hours) {
+        this.hours = hours;
+    }
+public String getTimeId() {
+        return timeId;
+    }
+public void setTimeId(String timeId) {
+        this.timeId = timeId;
+    }
+public String getCommunityId() {
+        return communityId;
+    }
+public void setCommunityId(String communityId) {
+        this.communityId = communityId;
+    }
+
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStatusCd() {
+        return statusCd;
+    }
+
+    public void setStatusCd(String statusCd) {
+        this.statusCd = statusCd;
+    }
+}
