0af871698c2432a63ab6a7cd8b7a1553753242a4
java110 <928255095@qq.com>
2022-10-03 03:05:46 +0800

    加入场馆管理

---
+++ java110-bean/src/main/java/com/java110/dto/communityVenue/CommunityVenueDto.java
@@ -0,0 +1,69 @@
+package com.java110.dto.communityVenue;
+
+import com.java110.dto.PageDto;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @ClassName FloorDto
+ * @Description 场馆数据层封装
+ * @Author wuxw
+ * @Date 2019/4/24 8:52
+ * @Version 1.0
+ * add by wuxw 2019/4/24
+ **/
+public class CommunityVenueDto extends PageDto implements Serializable {
+
+    private String venueId;
+private String name;
+private String remark;
+private String communityId;
+
+
+    private Date createTime;
+
+    private String statusCd = "0";
+
+
+    public String getVenueId() {
+        return venueId;
+    }
+public void setVenueId(String venueId) {
+        this.venueId = venueId;
+    }
+public String getName() {
+        return name;
+    }
+public void setName(String name) {
+        this.name = name;
+    }
+public String getRemark() {
+        return remark;
+    }
+public void setRemark(String remark) {
+        this.remark = remark;
+    }
+public String getCommunityId() {
+        return communityId;
+    }
+public void setCommunityId(String communityId) {
+        this.communityId = communityId;
+    }
+
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStatusCd() {
+        return statusCd;
+    }
+
+    public void setStatusCd(String statusCd) {
+        this.statusCd = statusCd;
+    }
+}
