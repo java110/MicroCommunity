75ba1e3173d8828601386a5ccf8e786db16f91a4
wuxw <928255095@qq.com>
2022-10-07 13:14:00 +0800

    删除楼栋信息

--- service-community/src/main/java/com/java110/community/cmd/floor/DeleteFloorCmd.java
+++ service-community/src/main/java/com/java110/community/cmd/floor/DeleteFloorCmd.java
@@ -6,6 +6,7 @@ import com.java110.core.annotation.Java110Transactional;
 import com.java110.core.context.ICmdDataFlowContext;
 import com.java110.core.event.cmd.Cmd;
 import com.java110.core.event.cmd.CmdEvent;
+import com.java110.doc.annotation.*;
 import com.java110.dto.UnitDto;
 import com.java110.intf.community.IFloorInnerServiceSMO;
 import com.java110.intf.community.IFloorV1InnerServiceSMO;
@@ -16,6 +17,33 @@ import com.java110.utils.util.Assert;
 import com.java110.utils.util.BeanConvertUtil;
 import com.java110.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Java110CmdDoc(title = "删除楼栋",
+        description = "用于外系统删除楼栋信息功能",
+        httpMethod = "post",
+        url = "http://{ip}:{port}/app/floor.deleteFloor",
+        resource = "communityDoc",
+        author = "吴学文",
+        serviceCode = "floor.deleteFloor"
+)
+
+@Java110ParamsDoc(params = {
+        @Java110ParamDoc(name = "communityId", length = 30, remark = "小区ID"),
+        @Java110ParamDoc(name = "floorId",length = 30, remark = "楼栋ID"),
+})
+
+@Java110ResponseDoc(
+        params = {
+                @Java110ParamDoc(name = "code", type = "int", length = 11, defaultValue = "0", remark = "返回编号，0 成功 其他失败"),
+                @Java110ParamDoc(name = "msg", type = "String", length = 250, defaultValue = "成功", remark = "描述"),
+        }
+)
+
+@Java110ExampleDoc(
+        reqBody="{\"floorId\":\"123123\",\"communityId\":\"2022081539020475\"}",
+        resBody="{'code':0,'msg':'成功'}"
+)
 
 @Java110Cmd(serviceCode = "floor.deleteFloor")
 public class DeleteFloorCmd extends Cmd {
