a4a1b356badb87d334fab7c8b56eb927df403e6c
java110 <928255095@qq.com>
2022-10-11 19:29:36 +0800

    优化商家停车劵

--- service-acct/src/main/java/com/java110/acct/cmd/parkingCoupon/ListShopParkingCouponCmd.java
+++ service-acct/src/main/java/com/java110/acct/cmd/parkingCoupon/ListShopParkingCouponCmd.java
@@ -13,16 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.java110.acct.cmd.parkingCouponShop;
+package com.java110.acct.cmd.parkingCoupon;
 
 import com.alibaba.fastjson.JSONObject;
 import com.java110.core.annotation.Java110Cmd;
-import com.java110.core.annotation.Java110Transactional;
 import com.java110.core.context.ICmdDataFlowContext;
 import com.java110.core.event.cmd.Cmd;
 import com.java110.core.event.cmd.CmdEvent;
+import com.java110.dto.parkingCouponShop.ParkingCouponShopDto;
 import com.java110.intf.acct.IParkingCouponShopV1InnerServiceSMO;
-import com.java110.po.parkingCouponShop.ParkingCouponShopPo;
 import com.java110.utils.exception.CmdException;
 import com.java110.utils.util.Assert;
 import com.java110.utils.util.BeanConvertUtil;
@@ -30,42 +29,55 @@ import com.java110.vo.ResultVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
- * 类表述：删除
- * 服务编码：parkingCouponShop.deleteParkingCouponShop
- * 请求路劲：/app/parkingCouponShop.DeleteParkingCouponShop
+ * 类表述：查询
+ * 服务编码：parkingCouponShop.listParkingCouponShop
+ * 请求路劲：/app/parkingCouponShop.ListParkingCouponShop
  * add by 吴学文 at 2022-10-10 16:43:42 mail: 928255095@qq.com
  * open source address: https://gitee.com/wuxw7/MicroCommunity
  * 官网：http://www.homecommunity.cn
  * 温馨提示：如果您对此文件进行修改 请不要删除原有作者及注释信息，请补充您的 修改的原因以及联系邮箱如下
  * // modify by 张三 at 2021-09-12 第10行在某种场景下存在某种bug 需要修复，注释10至20行 加入 20行至30行
  */
-@Java110Cmd(serviceCode = "parkingCouponShop.deleteParkingCouponShop")
-public class DeleteParkingCouponShopCmd extends Cmd {
-    private static Logger logger = LoggerFactory.getLogger(DeleteParkingCouponShopCmd.class);
+@Java110Cmd(serviceCode = "parkingCoupon.listShopParkingCoupon")
+public class ListShopParkingCouponCmd extends Cmd {
 
+    private static Logger logger = LoggerFactory.getLogger(ListShopParkingCouponCmd.class);
     @Autowired
     private IParkingCouponShopV1InnerServiceSMO parkingCouponShopV1InnerServiceSMOImpl;
 
     @Override
     public void validate(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) {
-        Assert.hasKeyAndValue(reqJson, "couponShopId", "couponShopId不能为空");
-        Assert.hasKeyAndValue(reqJson, "communityId", "communityId不能为空");
-
+        super.validatePageInfo(reqJson);
+        Assert.hasKeyAndValue(reqJson, "shopId", "未包含商铺ID");
     }
 
     @Override
-    @Java110Transactional
     public void doCmd(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) throws CmdException {
 
-        ParkingCouponShopPo parkingCouponShopPo = BeanConvertUtil.covertBean(reqJson, ParkingCouponShopPo.class);
-        int flag = parkingCouponShopV1InnerServiceSMOImpl.deleteParkingCouponShop(parkingCouponShopPo);
+        ParkingCouponShopDto parkingCouponShopDto = BeanConvertUtil.covertBean(reqJson, ParkingCouponShopDto.class);
 
-        if (flag < 1) {
-            throw new CmdException("删除数据失败");
+        int count = parkingCouponShopV1InnerServiceSMOImpl.queryParkingCouponShopsCount(parkingCouponShopDto);
+
+        List<ParkingCouponShopDto> parkingCouponShopDtos = null;
+
+        if (count > 0) {
+            parkingCouponShopDtos = parkingCouponShopV1InnerServiceSMOImpl.queryParkingCouponShops(parkingCouponShopDto);
+        } else {
+            parkingCouponShopDtos = new ArrayList<>();
         }
 
-        cmdDataFlowContext.setResponseEntity(ResultVo.success());
+        ResultVo resultVo = new ResultVo((int) Math.ceil((double) count / (double) reqJson.getInteger("row")), count, parkingCouponShopDtos);
+
+        ResponseEntity<String> responseEntity = new ResponseEntity<String>(resultVo.toString(), HttpStatus.OK);
+
+        cmdDataFlowContext.setResponseEntity(responseEntity);
     }
 }
