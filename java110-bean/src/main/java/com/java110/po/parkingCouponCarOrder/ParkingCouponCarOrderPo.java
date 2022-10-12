1447abe0015fdb2f987d78dc3931da423065a785
java110 <928255095@qq.com>
2022-10-12 19:11:55 +0800

    停车劵核销功能

---
+++ java110-bean/src/main/java/com/java110/po/parkingCouponCarOrder/ParkingCouponCarOrderPo.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2017-2020 吴学文 and java110 team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.java110.po.parkingCouponCarOrder;
+
+import java.io.Serializable;
+import java.util.Date;
+/**
+ * 类表述： Po 数据模型实体对象 基本保持与数据库模型一直 用于 增加修改删除 等时的数据载体
+ * add by 吴学文 at 2022-10-12 19:10:35 mail: 928255095@qq.com
+ * open source address: https://gitee.com/wuxw7/MicroCommunity
+ * 官网：http://www.homecommunity.cn
+ * 温馨提示：如果您对此文件进行修改 请不要删除原有作者及注释信息，请补充您的 修改的原因以及联系邮箱如下
+ * // modify by 张三 at 2021-09-12 第10行在某种场景下存在某种bug 需要修复，注释10至20行 加入 20行至30行
+ */
+public class ParkingCouponCarOrderPo implements Serializable {
+
+    private String pccId;
+private String machineId;
+private String orderId;
+private String paId;
+private String carNum;
+private String remark;
+private String statusCd = "0";
+private String communityId;
+private String carOutId;
+private String machineName;
+public String getPccId() {
+        return pccId;
+    }
+public void setPccId(String pccId) {
+        this.pccId = pccId;
+    }
+public String getMachineId() {
+        return machineId;
+    }
+public void setMachineId(String machineId) {
+        this.machineId = machineId;
+    }
+public String getOrderId() {
+        return orderId;
+    }
+public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+public String getPaId() {
+        return paId;
+    }
+public void setPaId(String paId) {
+        this.paId = paId;
+    }
+public String getCarNum() {
+        return carNum;
+    }
+public void setCarNum(String carNum) {
+        this.carNum = carNum;
+    }
+public String getRemark() {
+        return remark;
+    }
+public void setRemark(String remark) {
+        this.remark = remark;
+    }
+public String getStatusCd() {
+        return statusCd;
+    }
+public void setStatusCd(String statusCd) {
+        this.statusCd = statusCd;
+    }
+public String getCommunityId() {
+        return communityId;
+    }
+public void setCommunityId(String communityId) {
+        this.communityId = communityId;
+    }
+public String getCarOutId() {
+        return carOutId;
+    }
+public void setCarOutId(String carOutId) {
+        this.carOutId = carOutId;
+    }
+public String getMachineName() {
+        return machineName;
+    }
+public void setMachineName(String machineName) {
+        this.machineName = machineName;
+    }
+
+
+
+}
