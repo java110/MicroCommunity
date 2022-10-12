4a136d07020ae1910f3ba321fdff15a52c6dc16b
java110 <928255095@qq.com>
2022-10-12 13:56:38 +0800

    优化代码

--- java110-bean/src/main/java/com/java110/dto/parkingCouponCar/ParkingCouponCarDto.java
+++ java110-bean/src/main/java/com/java110/dto/parkingCouponCar/ParkingCouponCarDto.java
@@ -1,6 +1,7 @@
 package com.java110.dto.parkingCouponCar;
 
 import com.java110.dto.PageDto;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -15,19 +16,25 @@ import java.util.Date;
 public class ParkingCouponCarDto extends PageDto implements Serializable {
 
     private String giveWay;
-private String carNum;
-private String couponShopId;
-private String remark;
-private String couponId;
-private String pccId;
-private String typeCd;
-private String paId;
-private String startTime;
-private String shopId;
-private String state;
-private String endTime;
-private String communityId;
-private String value;
+    private String carNum;
+    private String carNumLike;
+    private String couponShopId;
+    private String remark;
+    private String couponId;
+    private String pccId;
+    private String typeCd;
+    private String typeCdName;
+    private String paId;
+    private String paName;
+    private String startTime;
+    private String shopId;
+    private String shopName;
+    private String shopNameLike;
+    private String state;
+    private String stateName;
+    private String endTime;
+    private String communityId;
+    private String value;
 
 
     private Date createTime;
@@ -38,85 +45,112 @@ private String value;
     public String getGiveWay() {
         return giveWay;
     }
-public void setGiveWay(String giveWay) {
+
+    public void setGiveWay(String giveWay) {
         this.giveWay = giveWay;
     }
-public String getCarNum() {
+
+    public String getCarNum() {
         return carNum;
     }
-public void setCarNum(String carNum) {
+
+    public void setCarNum(String carNum) {
         this.carNum = carNum;
     }
-public String getCouponShopId() {
+
+    public String getCouponShopId() {
         return couponShopId;
     }
-public void setCouponShopId(String couponShopId) {
+
+    public void setCouponShopId(String couponShopId) {
         this.couponShopId = couponShopId;
     }
-public String getRemark() {
+
+    public String getRemark() {
         return remark;
     }
-public void setRemark(String remark) {
+
+    public void setRemark(String remark) {
         this.remark = remark;
     }
-public String getCouponId() {
+
+    public String getCouponId() {
         return couponId;
     }
-public void setCouponId(String couponId) {
+
+    public void setCouponId(String couponId) {
         this.couponId = couponId;
     }
-public String getPccId() {
+
+    public String getPccId() {
         return pccId;
     }
-public void setPccId(String pccId) {
+
+    public void setPccId(String pccId) {
         this.pccId = pccId;
     }
-public String getTypeCd() {
+
+    public String getTypeCd() {
         return typeCd;
     }
-public void setTypeCd(String typeCd) {
+
+    public void setTypeCd(String typeCd) {
         this.typeCd = typeCd;
     }
-public String getPaId() {
+
+    public String getPaId() {
         return paId;
     }
-public void setPaId(String paId) {
+
+    public void setPaId(String paId) {
         this.paId = paId;
     }
-public String getStartTime() {
+
+    public String getStartTime() {
         return startTime;
     }
-public void setStartTime(String startTime) {
+
+    public void setStartTime(String startTime) {
         this.startTime = startTime;
     }
-public String getShopId() {
+
+    public String getShopId() {
         return shopId;
     }
-public void setShopId(String shopId) {
+
+    public void setShopId(String shopId) {
         this.shopId = shopId;
     }
-public String getState() {
+
+    public String getState() {
         return state;
     }
-public void setState(String state) {
+
+    public void setState(String state) {
         this.state = state;
     }
-public String getEndTime() {
+
+    public String getEndTime() {
         return endTime;
     }
-public void setEndTime(String endTime) {
+
+    public void setEndTime(String endTime) {
         this.endTime = endTime;
     }
-public String getCommunityId() {
+
+    public String getCommunityId() {
         return communityId;
     }
-public void setCommunityId(String communityId) {
+
+    public void setCommunityId(String communityId) {
         this.communityId = communityId;
     }
-public String getValue() {
+
+    public String getValue() {
         return value;
     }
-public void setValue(String value) {
+
+    public void setValue(String value) {
         this.value = value;
     }
 
@@ -135,5 +169,53 @@ public void setValue(String value) {
 
     public void setStatusCd(String statusCd) {
         this.statusCd = statusCd;
+    }
+
+    public String getTypeCdName() {
+        return typeCdName;
+    }
+
+    public void setTypeCdName(String typeCdName) {
+        this.typeCdName = typeCdName;
+    }
+
+    public String getPaName() {
+        return paName;
+    }
+
+    public void setPaName(String paName) {
+        this.paName = paName;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public String getCarNumLike() {
+        return carNumLike;
+    }
+
+    public void setCarNumLike(String carNumLike) {
+        this.carNumLike = carNumLike;
+    }
+
+    public String getShopNameLike() {
+        return shopNameLike;
+    }
+
+    public void setShopNameLike(String shopNameLike) {
+        this.shopNameLike = shopNameLike;
     }
 }
