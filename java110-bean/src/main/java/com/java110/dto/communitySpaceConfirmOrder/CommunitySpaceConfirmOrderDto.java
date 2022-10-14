0e0a3bc0c9d28dd70df3c26ce6d7f7c4d5a85335
java110 <928255095@qq.com>
2022-10-14 16:48:17 +0800

    优化预约核销订单

--- java110-bean/src/main/java/com/java110/dto/communitySpaceConfirmOrder/CommunitySpaceConfirmOrderDto.java
+++ java110-bean/src/main/java/com/java110/dto/communitySpaceConfirmOrder/CommunitySpaceConfirmOrderDto.java
@@ -1,6 +1,7 @@
 package com.java110.dto.communitySpaceConfirmOrder;
 
 import com.java110.dto.PageDto;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -15,11 +16,20 @@ import java.util.Date;
 public class CommunitySpaceConfirmOrderDto extends PageDto implements Serializable {
 
     private String spaceId;
-private String cspId;
-private String timeId;
-private String orderId;
-private String remark;
-private String communityId;
+    private String spaceName;
+    private String cspId;
+    private String timeId;
+    private String orderId;
+    private String remark;
+    private String communityId;
+
+    private String personName;
+
+    private String personTel;
+
+    private String appointmentTime;
+
+    private String hours;
 
 
     private Date createTime;
@@ -30,37 +40,48 @@ private String communityId;
     public String getSpaceId() {
         return spaceId;
     }
-public void setSpaceId(String spaceId) {
+
+    public void setSpaceId(String spaceId) {
         this.spaceId = spaceId;
     }
-public String getCspId() {
+
+    public String getCspId() {
         return cspId;
     }
-public void setCspId(String cspId) {
+
+    public void setCspId(String cspId) {
         this.cspId = cspId;
     }
-public String getTimeId() {
+
+    public String getTimeId() {
         return timeId;
     }
-public void setTimeId(String timeId) {
+
+    public void setTimeId(String timeId) {
         this.timeId = timeId;
     }
-public String getOrderId() {
+
+    public String getOrderId() {
         return orderId;
     }
-public void setOrderId(String orderId) {
+
+    public void setOrderId(String orderId) {
         this.orderId = orderId;
     }
-public String getRemark() {
+
+    public String getRemark() {
         return remark;
     }
-public void setRemark(String remark) {
+
+    public void setRemark(String remark) {
         this.remark = remark;
     }
-public String getCommunityId() {
+
+    public String getCommunityId() {
         return communityId;
     }
-public void setCommunityId(String communityId) {
+
+    public void setCommunityId(String communityId) {
         this.communityId = communityId;
     }
 
@@ -79,5 +100,46 @@ public void setCommunityId(String communityId) {
 
     public void setStatusCd(String statusCd) {
         this.statusCd = statusCd;
+    }
+
+    public String getSpaceName() {
+        return spaceName;
+    }
+
+    public void setSpaceName(String spaceName) {
+        this.spaceName = spaceName;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getPersonTel() {
+        return personTel;
+    }
+
+    public void setPersonTel(String personTel) {
+        this.personTel = personTel;
+    }
+
+
+    public String getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(String appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    public String getHours() {
+        return hours;
+    }
+
+    public void setHours(String hours) {
+        this.hours = hours;
     }
 }
