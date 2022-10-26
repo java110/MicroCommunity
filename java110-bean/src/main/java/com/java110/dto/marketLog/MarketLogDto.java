9e5ec5deac6574629cf8ffddaf90ae3a56577c67
wuxw <928255095@qq.com>
2022-10-26 23:52:19 +0800

    优化发送记录

--- java110-bean/src/main/java/com/java110/dto/marketLog/MarketLogDto.java
+++ java110-bean/src/main/java/com/java110/dto/marketLog/MarketLogDto.java
@@ -1,6 +1,7 @@
 package com.java110.dto.marketLog;
 
 import com.java110.dto.PageDto;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -15,16 +16,22 @@ import java.util.Date;
 public class MarketLogDto extends PageDto implements Serializable {
 
     private String personName;
-private String sendContent;
-private String sendWay;
-private String openId;
-private String logId;
-private String communityName;
-private String remark;
-private String ruleId;
-private String communityId;
-private String personTel;
-private String businessType;
+    private String personNameLike;
+    private String sendContent;
+    private String sendWay;
+    private String sendWayName;
+    private String openId;
+    private String logId;
+    private String communityName;
+    private String remark;
+    private String ruleId;
+    private String ruleName;
+    private String communityId;
+    private String personTel;
+    private String businessType;
+    private String businessTypeName;
+    private String startTime;
+    private String endTime;
 
 
     private Date createTime;
@@ -35,67 +42,88 @@ private String businessType;
     public String getPersonName() {
         return personName;
     }
-public void setPersonName(String personName) {
+
+    public void setPersonName(String personName) {
         this.personName = personName;
     }
-public String getSendContent() {
+
+    public String getSendContent() {
         return sendContent;
     }
-public void setSendContent(String sendContent) {
+
+    public void setSendContent(String sendContent) {
         this.sendContent = sendContent;
     }
-public String getSendWay() {
+
+    public String getSendWay() {
         return sendWay;
     }
-public void setSendWay(String sendWay) {
+
+    public void setSendWay(String sendWay) {
         this.sendWay = sendWay;
     }
-public String getOpenId() {
+
+    public String getOpenId() {
         return openId;
     }
-public void setOpenId(String openId) {
+
+    public void setOpenId(String openId) {
         this.openId = openId;
     }
-public String getLogId() {
+
+    public String getLogId() {
         return logId;
     }
-public void setLogId(String logId) {
+
+    public void setLogId(String logId) {
         this.logId = logId;
     }
-public String getCommunityName() {
+
+    public String getCommunityName() {
         return communityName;
     }
-public void setCommunityName(String communityName) {
+
+    public void setCommunityName(String communityName) {
         this.communityName = communityName;
     }
-public String getRemark() {
+
+    public String getRemark() {
         return remark;
     }
-public void setRemark(String remark) {
+
+    public void setRemark(String remark) {
         this.remark = remark;
     }
-public String getRuleId() {
+
+    public String getRuleId() {
         return ruleId;
     }
-public void setRuleId(String ruleId) {
+
+    public void setRuleId(String ruleId) {
         this.ruleId = ruleId;
     }
-public String getCommunityId() {
+
+    public String getCommunityId() {
         return communityId;
     }
-public void setCommunityId(String communityId) {
+
+    public void setCommunityId(String communityId) {
         this.communityId = communityId;
     }
-public String getPersonTel() {
+
+    public String getPersonTel() {
         return personTel;
     }
-public void setPersonTel(String personTel) {
+
+    public void setPersonTel(String personTel) {
         this.personTel = personTel;
     }
-public String getBusinessType() {
+
+    public String getBusinessType() {
         return businessType;
     }
-public void setBusinessType(String businessType) {
+
+    public void setBusinessType(String businessType) {
         this.businessType = businessType;
     }
 
@@ -114,5 +142,53 @@ public void setBusinessType(String businessType) {
 
     public void setStatusCd(String statusCd) {
         this.statusCd = statusCd;
+    }
+
+    public String getPersonNameLike() {
+        return personNameLike;
+    }
+
+    public void setPersonNameLike(String personNameLike) {
+        this.personNameLike = personNameLike;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getSendWayName() {
+        return sendWayName;
+    }
+
+    public void setSendWayName(String sendWayName) {
+        this.sendWayName = sendWayName;
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+
+    public String getBusinessTypeName() {
+        return businessTypeName;
+    }
+
+    public void setBusinessTypeName(String businessTypeName) {
+        this.businessTypeName = businessTypeName;
     }
 }
