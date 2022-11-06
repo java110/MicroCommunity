97b7b9344190f11f8ce62d11c6418d37cef36ca7
wuxw <928255095@qq.com>
2022-11-06 14:30:01 +0800

    加入 保养

---
+++ java110-bean/src/main/java/com/java110/dto/maintainanceItem/MaintainanceItemDto.java
@@ -0,0 +1,76 @@
+package com.java110.dto.maintainanceItem;
+
+import com.java110.dto.PageDto;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @ClassName FloorDto
+ * @Description 保养检查项数据层封装
+ * @Author wuxw
+ * @Date 2019/4/24 8:52
+ * @Version 1.0
+ * add by wuxw 2019/4/24
+ **/
+public class MaintainanceItemDto extends PageDto implements Serializable {
+
+    private String itemId;
+private String titleType;
+private String itemTitle;
+private String communityId;
+private String seq;
+
+
+    private Date createTime;
+
+    private String statusCd = "0";
+
+
+    public String getItemId() {
+        return itemId;
+    }
+public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+public String getTitleType() {
+        return titleType;
+    }
+public void setTitleType(String titleType) {
+        this.titleType = titleType;
+    }
+public String getItemTitle() {
+        return itemTitle;
+    }
+public void setItemTitle(String itemTitle) {
+        this.itemTitle = itemTitle;
+    }
+public String getCommunityId() {
+        return communityId;
+    }
+public void setCommunityId(String communityId) {
+        this.communityId = communityId;
+    }
+public String getSeq() {
+        return seq;
+    }
+public void setSeq(String seq) {
+        this.seq = seq;
+    }
+
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStatusCd() {
+        return statusCd;
+    }
+
+    public void setStatusCd(String statusCd) {
+        this.statusCd = statusCd;
+    }
+}
