4e2dc54f1716b14031ffad38188c23b2497c7207
wuxw <928255095@qq.com>
2022-11-10 22:48:08 +0800

    加入供应商类型 功能

---
+++ service-scm/src/main/java/com/java110/scm/cmd/supplierType/UpdateSupplierTypeCmd.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017-2020 吴学文 and java110 team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.java110.scm.cmd.supplierType;
+
+import com.alibaba.fastjson.JSONObject;
+import com.java110.core.annotation.Java110Cmd;
+import com.java110.core.annotation.Java110Transactional;
+import com.java110.core.context.ICmdDataFlowContext;
+import com.java110.core.event.cmd.Cmd;
+import com.java110.core.event.cmd.CmdEvent;
+import com.java110.core.factory.GenerateCodeFactory;
+import com.java110.intf.scm.ISupplierTypeV1InnerServiceSMO;
+import com.java110.po.supplierType.SupplierTypePo;
+import com.java110.utils.exception.CmdException;
+import com.java110.utils.util.Assert;
+import com.java110.utils.util.BeanConvertUtil;
+import com.java110.vo.ResultVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * 类表述：更新
+ * 服务编码：supplierType.updateSupplierType
+ * 请求路劲：/app/supplierType.UpdateSupplierType
+ * add by 吴学文 at 2022-11-10 22:43:40 mail: 928255095@qq.com
+ * open source address: https://gitee.com/wuxw7/MicroCommunity
+ * 官网：http://www.homecommunity.cn
+ * 温馨提示：如果您对此文件进行修改 请不要删除原有作者及注释信息，请补充您的 修改的原因以及联系邮箱如下
+ * // modify by 张三 at 2021-09-12 第10行在某种场景下存在某种bug 需要修复，注释10至20行 加入 20行至30行
+ */
+@Java110Cmd(serviceCode = "supplierType.updateSupplierType")
+public class UpdateSupplierTypeCmd extends Cmd {
+
+    private static Logger logger = LoggerFactory.getLogger(UpdateSupplierTypeCmd.class);
+
+
+    @Autowired
+    private ISupplierTypeV1InnerServiceSMO supplierTypeV1InnerServiceSMOImpl;
+
+    @Override
+    public void validate(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) {
+        Assert.hasKeyAndValue(reqJson, "stId", "stId不能为空");
+
+    }
+
+    @Override
+    @Java110Transactional
+    public void doCmd(CmdEvent event, ICmdDataFlowContext cmdDataFlowContext, JSONObject reqJson) throws CmdException {
+
+        SupplierTypePo supplierTypePo = BeanConvertUtil.covertBean(reqJson, SupplierTypePo.class);
+        int flag = supplierTypeV1InnerServiceSMOImpl.updateSupplierType(supplierTypePo);
+
+        if (flag < 1) {
+            throw new CmdException("更新数据失败");
+        }
+
+        cmdDataFlowContext.setResponseEntity(ResultVo.success());
+    }
+}
