2dbf15adad9ce75365d61674ed384c5dda9ef4d9
wuxw <928255095@qq.com>
2022-11-11 12:30:13 +0800

    测试工商银行 支付 可能会编译失败

--- service-acct/src/main/java/com/java110/acct/payment/adapt/icbc/IcbcPaymentFactoryAdapt.java
+++ service-acct/src/main/java/com/java110/acct/payment/adapt/icbc/IcbcPaymentFactoryAdapt.java
@@ -131,7 +131,9 @@ public class IcbcPaymentFactoryAdapt implements IPaymentFactoryAdapt {
             ownerAppUserDto.setAppType(appType);
             List<OwnerAppUserDto> ownerAppUserDtos = ownerAppUserInnerServiceSMOImpl.queryOwnerAppUsers(ownerAppUserDto);
 
-            Assert.listOnlyOne(ownerAppUserDtos, "未找到开放账号信息");
+            if(ownerAppUserDtos == null || ownerAppUserDtos.size() < 1){
+                throw new IllegalArgumentException("未找到开放账号信息");
+            }
             openId = ownerAppUserDtos.get(0).getOpenId();
         }
 
