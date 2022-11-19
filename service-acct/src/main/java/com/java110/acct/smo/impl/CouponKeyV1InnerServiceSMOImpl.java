636ef7b1f775c6ef9390aa73c772f25860d02bdb
wuxw <928255095@qq.com>
2022-11-19 23:32:31 +0800

    优惠券key

---
+++ service-acct/src/main/java/com/java110/acct/smo/impl/CouponKeyV1InnerServiceSMOImpl.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2017-2020 吴学文 and java110 team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.java110.acct.smo.impl;
+
+
+import com.java110.acct.dao.ICouponKeyV1ServiceDao;
+import com.java110.intf.acct.ICouponKeyV1InnerServiceSMO;
+import com.java110.dto.couponKey.CouponKeyDto;
+import com.java110.po.couponKey.CouponKeyPo;
+import com.java110.utils.util.BeanConvertUtil;
+import com.java110.core.base.smo.BaseServiceSMO;
+import com.java110.dto.user.UserDto;
+import com.java110.dto.PageDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 类表述： 服务之前调用的接口实现类，不对外提供接口能力 只用于接口建调用
+ * add by 吴学文 at 2022-11-19 23:07:21 mail: 928255095@qq.com
+ * open source address: https://gitee.com/wuxw7/MicroCommunity
+ * 官网：http://www.homecommunity.cn
+ * 温馨提示：如果您对此文件进行修改 请不要删除原有作者及注释信息，请补充您的 修改的原因以及联系邮箱如下
+ * // modify by 张三 at 2021-09-12 第10行在某种场景下存在某种bug 需要修复，注释10至20行 加入 20行至30行
+ */
+@RestController
+public class CouponKeyV1InnerServiceSMOImpl extends BaseServiceSMO implements ICouponKeyV1InnerServiceSMO {
+
+    @Autowired
+    private ICouponKeyV1ServiceDao couponKeyV1ServiceDaoImpl;
+
+
+    @Override
+    public int saveCouponKey(@RequestBody  CouponKeyPo couponKeyPo) {
+        int saveFlag = couponKeyV1ServiceDaoImpl.saveCouponKeyInfo(BeanConvertUtil.beanCovertMap(couponKeyPo));
+        return saveFlag;
+    }
+
+     @Override
+    public int updateCouponKey(@RequestBody  CouponKeyPo couponKeyPo) {
+        int saveFlag = couponKeyV1ServiceDaoImpl.updateCouponKeyInfo(BeanConvertUtil.beanCovertMap(couponKeyPo));
+        return saveFlag;
+    }
+
+     @Override
+    public int deleteCouponKey(@RequestBody  CouponKeyPo couponKeyPo) {
+       couponKeyPo.setStatusCd("1");
+       int saveFlag = couponKeyV1ServiceDaoImpl.updateCouponKeyInfo(BeanConvertUtil.beanCovertMap(couponKeyPo));
+       return saveFlag;
+    }
+
+    @Override
+    public List<CouponKeyDto> queryCouponKeys(@RequestBody  CouponKeyDto couponKeyDto) {
+
+        //校验是否传了 分页信息
+
+        int page = couponKeyDto.getPage();
+
+        if (page != PageDto.DEFAULT_PAGE) {
+            couponKeyDto.setPage((page - 1) * couponKeyDto.getRow());
+        }
+
+        List<CouponKeyDto> couponKeys = BeanConvertUtil.covertBeanList(couponKeyV1ServiceDaoImpl.getCouponKeyInfo(BeanConvertUtil.beanCovertMap(couponKeyDto)), CouponKeyDto.class);
+
+        return couponKeys;
+    }
+
+
+    @Override
+    public int queryCouponKeysCount(@RequestBody CouponKeyDto couponKeyDto) {
+        return couponKeyV1ServiceDaoImpl.queryCouponKeysCount(BeanConvertUtil.beanCovertMap(couponKeyDto));    }
+
+}
