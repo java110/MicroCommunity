03e19d706bd9bdf0d91fbfc93134e58ca192267d
wuxw <928255095@qq.com>
2022-11-21 12:17:50 +0800

    加入 用户优惠券功能

---
+++ java110-bean/src/main/java/com/java110/dto/couponPropertyUser/CouponPropertyUserDto.java
@@ -0,0 +1,125 @@
+package com.java110.dto.couponPropertyUser;
+
+import com.java110.dto.PageDto;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @ClassName FloorDto
+ * @Description 用户优惠券数据层封装
+ * @Author wuxw
+ * @Date 2019/4/24 8:52
+ * @Version 1.0
+ * add by wuxw 2019/4/24
+ **/
+public class CouponPropertyUserDto extends PageDto implements Serializable {
+
+    private String couponName;
+private String toType;
+private String validityDay;
+private String couponId;
+private String userName;
+private String userId;
+private String cppId;
+private String tel;
+private String state;
+private String stock;
+private String communityId;
+private String value;
+
+
+    private Date createTime;
+
+    private String statusCd = "0";
+
+
+    public String getCouponName() {
+        return couponName;
+    }
+public void setCouponName(String couponName) {
+        this.couponName = couponName;
+    }
+public String getToType() {
+        return toType;
+    }
+public void setToType(String toType) {
+        this.toType = toType;
+    }
+public String getValidityDay() {
+        return validityDay;
+    }
+public void setValidityDay(String validityDay) {
+        this.validityDay = validityDay;
+    }
+public String getCouponId() {
+        return couponId;
+    }
+public void setCouponId(String couponId) {
+        this.couponId = couponId;
+    }
+public String getUserName() {
+        return userName;
+    }
+public void setUserName(String userName) {
+        this.userName = userName;
+    }
+public String getUserId() {
+        return userId;
+    }
+public void setUserId(String userId) {
+        this.userId = userId;
+    }
+public String getCppId() {
+        return cppId;
+    }
+public void setCppId(String cppId) {
+        this.cppId = cppId;
+    }
+public String getTel() {
+        return tel;
+    }
+public void setTel(String tel) {
+        this.tel = tel;
+    }
+public String getState() {
+        return state;
+    }
+public void setState(String state) {
+        this.state = state;
+    }
+public String getStock() {
+        return stock;
+    }
+public void setStock(String stock) {
+        this.stock = stock;
+    }
+public String getCommunityId() {
+        return communityId;
+    }
+public void setCommunityId(String communityId) {
+        this.communityId = communityId;
+    }
+public String getValue() {
+        return value;
+    }
+public void setValue(String value) {
+        this.value = value;
+    }
+
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStatusCd() {
+        return statusCd;
+    }
+
+    public void setStatusCd(String statusCd) {
+        this.statusCd = statusCd;
+    }
+}
