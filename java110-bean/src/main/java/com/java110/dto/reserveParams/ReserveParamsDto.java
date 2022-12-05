a4dff69429d3dd530ecebf4853e4700345837052
Your Name <you@example.com>
2022-12-05 18:06:42 +0800

    加入预约参数

---
+++ java110-bean/src/main/java/com/java110/dto/reserveParams/ReserveParamsDto.java
@@ -0,0 +1,97 @@
+package com.java110.dto.reserveParams;
+
+import com.java110.dto.PageDto;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @ClassName FloorDto
+ * @Description 预约参数数据层封装
+ * @Author wuxw
+ * @Date 2019/4/24 8:52
+ * @Version 1.0
+ * add by wuxw 2019/4/24
+ **/
+public class ReserveParamsDto extends PageDto implements Serializable {
+
+    private String paramWay;
+private String paramsId;
+private String maxQuantity;
+private String name;
+private String paramWayText;
+private String startTime;
+private String communityId;
+private String hoursMaxQuantity;
+
+
+    private Date createTime;
+
+    private String statusCd = "0";
+
+
+    public String getParamWay() {
+        return paramWay;
+    }
+public void setParamWay(String paramWay) {
+        this.paramWay = paramWay;
+    }
+public String getParamsId() {
+        return paramsId;
+    }
+public void setParamsId(String paramsId) {
+        this.paramsId = paramsId;
+    }
+public String getMaxQuantity() {
+        return maxQuantity;
+    }
+public void setMaxQuantity(String maxQuantity) {
+        this.maxQuantity = maxQuantity;
+    }
+public String getName() {
+        return name;
+    }
+public void setName(String name) {
+        this.name = name;
+    }
+public String getParamWayText() {
+        return paramWayText;
+    }
+public void setParamWayText(String paramWayText) {
+        this.paramWayText = paramWayText;
+    }
+public String getStartTime() {
+        return startTime;
+    }
+public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+public String getCommunityId() {
+        return communityId;
+    }
+public void setCommunityId(String communityId) {
+        this.communityId = communityId;
+    }
+public String getHoursMaxQuantity() {
+        return hoursMaxQuantity;
+    }
+public void setHoursMaxQuantity(String hoursMaxQuantity) {
+        this.hoursMaxQuantity = hoursMaxQuantity;
+    }
+
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getStatusCd() {
+        return statusCd;
+    }
+
+    public void setStatusCd(String statusCd) {
+        this.statusCd = statusCd;
+    }
+}
