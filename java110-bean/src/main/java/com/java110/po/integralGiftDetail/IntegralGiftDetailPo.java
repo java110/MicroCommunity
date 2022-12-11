a1fa459cfdfab3a8ac6f7e5d601d08398ff16a22
Your Name <you@example.com>
2022-12-12 02:13:34 +0800

    加入积分赠送记录

---
+++ java110-bean/src/main/java/com/java110/po/integralGiftDetail/IntegralGiftDetailPo.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2017-2020 吴学文 and java110 team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.java110.po.integralGiftDetail;
+
+import java.io.Serializable;
+import java.util.Date;
+/**
+ * 类表述： Po 数据模型实体对象 基本保持与数据库模型一直 用于 增加修改删除 等时的数据载体
+ * add by 吴学文 at 2022-12-12 01:54:09 mail: 928255095@qq.com
+ * open source address: https://gitee.com/wuxw7/MicroCommunity
+ * 官网：http://www.homecommunity.cn
+ * 温馨提示：如果您对此文件进行修改 请不要删除原有作者及注释信息，请补充您的 修改的原因以及联系邮箱如下
+ * // modify by 张三 at 2021-09-12 第10行在某种场景下存在某种bug 需要修复，注释10至20行 加入 20行至30行
+ */
+public class IntegralGiftDetailPo implements Serializable {
+
+    private String configName;
+private String createUserId;
+private String quantity;
+private String configId;
+private String detailId;
+private String ruleName;
+private String tel;
+private String statusCd = "0";
+private String communityId;
+private String ruleId;
+private String userName;
+public String getConfigName() {
+        return configName;
+    }
+public void setConfigName(String configName) {
+        this.configName = configName;
+    }
+public String getCreateUserId() {
+        return createUserId;
+    }
+public void setCreateUserId(String createUserId) {
+        this.createUserId = createUserId;
+    }
+public String getQuantity() {
+        return quantity;
+    }
+public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+public String getConfigId() {
+        return configId;
+    }
+public void setConfigId(String configId) {
+        this.configId = configId;
+    }
+public String getDetailId() {
+        return detailId;
+    }
+public void setDetailId(String detailId) {
+        this.detailId = detailId;
+    }
+public String getRuleName() {
+        return ruleName;
+    }
+public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+public String getTel() {
+        return tel;
+    }
+public void setTel(String tel) {
+        this.tel = tel;
+    }
+public String getStatusCd() {
+        return statusCd;
+    }
+public void setStatusCd(String statusCd) {
+        this.statusCd = statusCd;
+    }
+public String getCommunityId() {
+        return communityId;
+    }
+public void setCommunityId(String communityId) {
+        this.communityId = communityId;
+    }
+public String getRuleId() {
+        return ruleId;
+    }
+public void setRuleId(String ruleId) {
+        this.ruleId = ruleId;
+    }
+public String getUserName() {
+        return userName;
+    }
+public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+
+}
