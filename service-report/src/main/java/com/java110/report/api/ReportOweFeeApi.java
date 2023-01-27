db457a1184e048b4bc62348413383eaa7a8d3362
Your Name <you@example.com>
2023-01-27 19:35:07 +0800

    优化代码

--- service-report/src/main/java/com/java110/report/api/ReportOweFeeApi.java
+++ service-report/src/main/java/com/java110/report/api/ReportOweFeeApi.java
@@ -103,6 +103,7 @@ public class ReportOweFeeApi {
                                                     @RequestParam(value = "roomSubType", required = false) String roomSubType,
                                                     @RequestParam(value = "roomNum", required = false) String roomNum,
                                                     @RequestParam(value = "ownerId", required = false) String ownerId,
+                                                    @RequestParam(value = "payerObjId", required = false) String payerObjId,
                                                     @RequestParam(value = "page") int page,
                                                     @RequestParam(value = "row") int row) {
         ReportOweFeeDto reportOweFeeDto = new ReportOweFeeDto();
@@ -117,6 +118,7 @@ public class ReportOweFeeApi {
         reportOweFeeDto.setRoomSubType(roomSubType);
         reportOweFeeDto.setRoomNum(roomNum);
         reportOweFeeDto.setOwnerId(ownerId);
+        reportOweFeeDto.setPayerObjId(payerObjId);
         if (!StringUtil.isEmpty(configIds)) {
             String[] tmpConfigIds = configIds.split(",");
             reportOweFeeDto.setConfigIds(tmpConfigIds);
