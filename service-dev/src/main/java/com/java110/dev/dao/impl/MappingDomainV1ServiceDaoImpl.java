b8c305c67fc1920c27bac7f2efc1d48113fbc650
java110 <928255095@qq.com>
2023-01-29 14:10:28 +0800

    add mapping domain function

---
+++ service-dev/src/main/java/com/java110/dev/dao/impl/MappingDomainV1ServiceDaoImpl.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2017-2020 吴学文 and java110 team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.java110.dev.dao.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.java110.utils.constant.ResponseConstant;
+import com.java110.utils.exception.DAOException;
+import com.java110.utils.util.DateUtil;
+import com.java110.core.base.dao.BaseServiceDao;
+import com.java110.dev.dao.IMappingDomainV1ServiceDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 类表述：
+ * add by 吴学文 at 2023-01-29 14:09:04 mail: 928255095@qq.com
+ * open source address: https://gitee.com/wuxw7/MicroCommunity
+ * 官网：http://www.homecommunity.cn
+ * 温馨提示：如果您对此文件进行修改 请不要删除原有作者及注释信息，请补充您的 修改的原因以及联系邮箱如下
+ * // modify by 张三 at 2021-09-12 第10行在某种场景下存在某种bug 需要修复，注释10至20行 加入 20行至30行
+ */
+@Service("mappingDomainV1ServiceDaoImpl")
+public class MappingDomainV1ServiceDaoImpl extends BaseServiceDao implements IMappingDomainV1ServiceDao {
+
+    private static Logger logger = LoggerFactory.getLogger(MappingDomainV1ServiceDaoImpl.class);
+
+
+
+
+
+    /**
+     * 保存配置项信息 到 instance
+     * @param info   bId 信息
+     * @throws DAOException DAO异常
+     */
+    @Override
+    public int saveMappingDomainInfo(Map info) throws DAOException {
+        logger.debug("保存 saveMappingDomainInfo 入参 info : {}",info);
+
+        int saveFlag = sqlSessionTemplate.insert("mappingDomainV1ServiceDaoImpl.saveMappingDomainInfo",info);
+
+        return saveFlag;
+    }
+
+
+    /**
+     * 查询配置项信息（instance）
+     * @param info bId 信息
+     * @return List<Map>
+     * @throws DAOException DAO异常
+     */
+    @Override
+    public List<Map> getMappingDomainInfo(Map info) throws DAOException {
+        logger.debug("查询 getMappingDomainInfo 入参 info : {}",info);
+
+        List<Map> businessMappingDomainInfos = sqlSessionTemplate.selectList("mappingDomainV1ServiceDaoImpl.getMappingDomainInfo",info);
+
+        return businessMappingDomainInfos;
+    }
+
+
+    /**
+     * 修改配置项信息
+     * @param info 修改信息
+     * @throws DAOException DAO异常
+     */
+    @Override
+    public int updateMappingDomainInfo(Map info) throws DAOException {
+        logger.debug("修改 updateMappingDomainInfo 入参 info : {}",info);
+
+        int saveFlag = sqlSessionTemplate.update("mappingDomainV1ServiceDaoImpl.updateMappingDomainInfo",info);
+
+        return saveFlag;
+    }
+
+     /**
+     * 查询配置项数量
+     * @param info 配置项信息
+     * @return 配置项数量
+     */
+    @Override
+    public int queryMappingDomainsCount(Map info) {
+        logger.debug("查询 queryMappingDomainsCount 入参 info : {}",info);
+
+        List<Map> businessMappingDomainInfos = sqlSessionTemplate.selectList("mappingDomainV1ServiceDaoImpl.queryMappingDomainsCount", info);
+        if (businessMappingDomainInfos.size() < 1) {
+            return 0;
+        }
+
+        return Integer.parseInt(businessMappingDomainInfos.get(0).get("count").toString());
+    }
+
+
+}
