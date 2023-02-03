3fbdd17668bc5b22b49d094195995214d9478c17
java110 <928255095@qq.com>
2023-02-03 17:02:47 +0800

    modify purchase

--- service-store/src/main/java/com/java110/store/cmd/resourceStore/ListResourceStoresCmd.java
+++ service-store/src/main/java/com/java110/store/cmd/resourceStore/ListResourceStoresCmd.java
@@ -80,6 +80,7 @@ public class ListResourceStoresCmd extends Cmd {
         if (count > 0) {
             resourceStores = BeanConvertUtil.covertBeanList(resourceStoreInnerServiceSMOImpl.queryResourceStores(resourceStoreDto), ApiResourceStoreDataVo.class);
             //查询总价
+            //queryResourceStoreAndResourceTotalPrice(resourceStores);
             resourceStoreDto.setPage(Integer.valueOf(reqJson.getString("page")));
             subTotalPrice = new BigDecimal(resourceStoreInnerServiceSMOImpl.queryResourceStoresTotalPrice(resourceStoreDto));
             resourceStoreDto.setPage(PageDto.DEFAULT_PAGE);
