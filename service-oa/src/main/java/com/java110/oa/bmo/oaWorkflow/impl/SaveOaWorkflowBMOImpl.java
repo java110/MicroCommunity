4bbbdcb191000d771f0448c5cdfabdcec542c162
Your Name <you@example.com>
2023-03-15 23:49:43 +0800

    优化保养转单功能

--- service-oa/src/main/java/com/java110/oa/bmo/oaWorkflow/impl/SaveOaWorkflowBMOImpl.java
+++ service-oa/src/main/java/com/java110/oa/bmo/oaWorkflow/impl/SaveOaWorkflowBMOImpl.java
@@ -6,8 +6,12 @@ import com.java110.dto.oaWorkflow.OaWorkflowDto;
 import com.java110.dto.workflow.WorkflowModelDto;
 import com.java110.intf.common.IWorkflowInnerServiceSMO;
 import com.java110.intf.oa.IOaWorkflowInnerServiceSMO;
+import com.java110.intf.oa.IOaWorkflowXmlInnerServiceSMO;
 import com.java110.oa.bmo.oaWorkflow.ISaveOaWorkflowBMO;
 import com.java110.po.oaWorkflow.OaWorkflowPo;
+import com.java110.po.oaWorkflowXml.OaWorkflowXmlPo;
+import com.java110.utils.exception.CmdException;
+import com.java110.utils.util.BpmnXml;
 import com.java110.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +25,9 @@ public class SaveOaWorkflowBMOImpl implements ISaveOaWorkflowBMO {
 
     @Autowired
     private IWorkflowInnerServiceSMO workflowInnerServiceSMOImpl;
+
+    @Autowired
+    private IOaWorkflowXmlInnerServiceSMO oaWorkflowXmlInnerServiceSMOImpl;
 
     /**
      * 添加小区信息
@@ -45,6 +52,19 @@ public class SaveOaWorkflowBMOImpl implements ISaveOaWorkflowBMO {
 
         if (flag > 0) {
             return ResultVo.createResponseEntity(ResultVo.CODE_OK, "保存成功");
+        }
+
+        //默认 流程图以防画错
+        OaWorkflowXmlPo oaWorkflowXmlPo = new OaWorkflowXmlPo();
+        oaWorkflowXmlPo.setStoreId(oaWorkflowPo.getStoreId());
+        oaWorkflowXmlPo.setFlowId(oaWorkflowPo.getFlowId());
+        oaWorkflowXmlPo.setXmlId(GenerateCodeFactory.getGeneratorId("79"));
+        oaWorkflowXmlPo.setSvgXml("");
+        oaWorkflowXmlPo.setBpmnXml(BpmnXml.getDefaultBpmnXml(oaWorkflowPo.getFlowId()));
+
+        flag = oaWorkflowXmlInnerServiceSMOImpl.saveOaWorkflowXml(oaWorkflowXmlPo);
+        if (flag < 1) {
+            throw new CmdException("保存模型数据失败");
         }
 
         return ResultVo.createResponseEntity(ResultVo.CODE_ERROR, "保存失败");
