3650137a16b449fcc89681fe17c11ac6cba7e9b2
Your Name <you@example.com>
2023-05-16 01:14:10 +0800

    优化定时任务手工测试

--- service-report/src/main/java/com/java110/report/statistics/impl/BaseDataStatisticsImpl.java
+++ service-report/src/main/java/com/java110/report/statistics/impl/BaseDataStatisticsImpl.java
@@ -4,6 +4,7 @@ import com.java110.dto.RoomDto;
 import com.java110.dto.report.QueryStatisticsDto;
 import com.java110.intf.community.IRoomV1InnerServiceSMO;
 import com.java110.report.statistics.IBaseDataStatistics;
+import com.java110.utils.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,53 +17,67 @@ public class BaseDataStatisticsImpl implements IBaseDataStatistics {
     @Autowired
     private IRoomV1InnerServiceSMO roomV1InnerServiceSMOImpl;
 
+    /**
+     * 查询全部房屋
+     *
+     * @param queryStatisticsDto
+     * @return
+     */
     @Override
     public long getRoomCount(QueryStatisticsDto queryStatisticsDto) {
 
         RoomDto roomDto = new RoomDto();
         roomDto.setCommunityId(queryStatisticsDto.getCommunityId());
         roomDto.setFloorId(queryStatisticsDto.getFloorId());
-        if(queryStatisticsDto.getObjName().contains("-")){
-            String[] objNames = queryStatisticsDto.getObjName().split("-");
-            if(objNames.length == 2){
-                roomDto.setFloorNum(objNames[0]);
-                roomDto.setUnitNum("0");
-                roomDto.setRoomNum(objNames[1]);
-            }
-            objNames = queryStatisticsDto.getObjName().split("-",3);
-            if(objNames.length == 3){
-                roomDto.setFloorNum(objNames[0]);
-                roomDto.setUnitNum(objNames[1]);
-                roomDto.setRoomNum(objNames[2]);
-            }
-        }else{
-            roomDto.setRoomNumLike(queryStatisticsDto.getObjName());
-        }
+        addRoomNumCondition(queryStatisticsDto, roomDto);
         return roomV1InnerServiceSMOImpl.queryRoomsCount(roomDto);
     }
 
+
+    /**
+     * 查询空闲房屋
+     *
+     * @param queryStatisticsDto
+     * @return
+     */
     @Override
     public long getFreeRoomCount(QueryStatisticsDto queryStatisticsDto) {
         RoomDto roomDto = new RoomDto();
         roomDto.setCommunityId(queryStatisticsDto.getCommunityId());
         roomDto.setState(RoomDto.STATE_FREE);
         roomDto.setFloorId(queryStatisticsDto.getFloorId());
-        if(queryStatisticsDto.getObjName().contains("-")){
-            String[] objNames = queryStatisticsDto.getObjName().split("-");
-            if(objNames.length == 2){
-                roomDto.setFloorNum(objNames[0]);
-                roomDto.setUnitNum("0");
-                roomDto.setRoomNum(objNames[1]);
-            }
-            objNames = queryStatisticsDto.getObjName().split("-",3);
-            if(objNames.length == 3){
-                roomDto.setFloorNum(objNames[0]);
-                roomDto.setUnitNum(objNames[1]);
-                roomDto.setRoomNum(objNames[2]);
-            }
-        }else{
-            roomDto.setRoomNumLike(queryStatisticsDto.getObjName());
-        }
+        addRoomNumCondition(queryStatisticsDto, roomDto);
         return roomV1InnerServiceSMOImpl.queryRoomsCount(roomDto);
+    }
+
+
+    /**
+     * roomNum 拆分 查询房屋信息
+     *
+     * @param queryStatisticsDto
+     * @param roomDto
+     */
+    private void addRoomNumCondition(QueryStatisticsDto queryStatisticsDto, RoomDto roomDto) {
+        if (StringUtil.isEmpty(queryStatisticsDto.getObjName())) {
+            return;
+        }
+        if (!queryStatisticsDto.getObjName().contains("-")) {
+            roomDto.setRoomNumLike(queryStatisticsDto.getObjName());
+            return;
+        }
+        String[] objNames = queryStatisticsDto.getObjName().split("-");
+        if (objNames.length == 2) {
+            roomDto.setFloorNum(objNames[0]);
+            roomDto.setUnitNum("0");
+            roomDto.setRoomNum(objNames[1]);
+            return;
+        }
+        objNames = queryStatisticsDto.getObjName().split("-", 3);
+        if (objNames.length == 3) {
+            roomDto.setFloorNum(objNames[0]);
+            roomDto.setUnitNum(objNames[1]);
+            roomDto.setRoomNum(objNames[2]);
+        }
+
     }
 }
