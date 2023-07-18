42f065eeeddb3e0e2bdeef51e1868644daae31d7
Your Name <you@example.com>
2023-07-19 02:57:38 +0800

    优化代码

--- java110-core/src/main/java/com/java110/core/cache/Java110RedisConfig.java
+++ java110-core/src/main/java/com/java110/core/cache/Java110RedisConfig.java
@@ -1,10 +1,5 @@
 package com.java110.core.cache;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,7 +13,7 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.*;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
 import org.springframework.util.StringUtils;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
