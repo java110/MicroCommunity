18ec24314c2f532b5419463e6abb1c25491b3d18
java110 <928255095@qq.com>
2023-07-20 16:13:04 +0800

    优化代码

--- service-user/src/main/java/com/java110/user/cmd/owner/SaveOwnerMemberCmd.java
+++ service-user/src/main/java/com/java110/user/cmd/owner/SaveOwnerMemberCmd.java
@@ -3,6 +3,7 @@ package com.java110.user.cmd.owner;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.java110.core.annotation.Java110Cmd;
+import com.java110.core.annotation.Java110Transactional;
 import com.java110.core.context.ICmdDataFlowContext;
 import com.java110.core.event.cmd.Cmd;
 import com.java110.core.event.cmd.CmdEvent;
@@ -104,6 +105,7 @@ public class SaveOwnerMemberCmd extends Cmd {
     }
 
     @Override
+    @Java110Transactional
     public void doCmd(CmdEvent event, ICmdDataFlowContext context, JSONObject reqJson) throws CmdException, ParseException {
 
         //todo 生成memberId
