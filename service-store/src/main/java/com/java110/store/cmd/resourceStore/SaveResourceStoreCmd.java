dd07642a35a3b103d24055be8e32d9139512021b
Your Name <you@example.com>
2023-08-26 08:52:46 +0800

    优化diamante

--- service-store/src/main/java/com/java110/store/cmd/resourceStore/SaveResourceStoreCmd.java
+++ service-store/src/main/java/com/java110/store/cmd/resourceStore/SaveResourceStoreCmd.java
@@ -277,6 +277,8 @@ public class SaveResourceStoreCmd extends Cmd {
         resourceStoreTimesPo.setResCode(resourceStorePo.getResCode());
         resourceStoreTimesPo.setStoreId(resourceStorePo.getStoreId());
         resourceStoreTimesPo.setTimesId(GenerateCodeFactory.getGeneratorId("10"));
+        resourceStoreTimesPo.setShId(resourceStorePo.getShId());
+
         resourceStoreTimesV1InnerServiceSMOImpl.saveOrUpdateResourceStoreTimes(resourceStoreTimesPo);
 
 
