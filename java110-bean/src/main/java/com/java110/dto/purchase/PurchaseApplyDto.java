aa04c9f83c8bca00405bde0e43da3f273bebc503
Your Name <you@example.com>
2023-08-27 10:31:35 +0800

    优化代码

--- java110-bean/src/main/java/com/java110/dto/purchase/PurchaseApplyDto.java
+++ java110-bean/src/main/java/com/java110/dto/purchase/PurchaseApplyDto.java
@@ -76,6 +76,12 @@ public class PurchaseApplyDto extends PageDto implements Serializable {
     private String consultPrice;
     private String timesId;
 
+    private String resName;
+
+    private String startTime;
+
+    private String endTime;
+
     public String getResOrderType() {
         return resOrderType;
     }
@@ -394,5 +400,29 @@ public class PurchaseApplyDto extends PageDto implements Serializable {
 
     public void setTimesId(String timesId) {
         this.timesId = timesId;
+    }
+
+    public String getResName() {
+        return resName;
+    }
+
+    public void setResName(String resName) {
+        this.resName = resName;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
     }
 }
