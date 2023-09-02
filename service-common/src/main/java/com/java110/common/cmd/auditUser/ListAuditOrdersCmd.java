fbe8640b13ce6b1d532d2503390c23fbf1fd3d78
Your Name <you@example.com>
2023-09-02 11:50:12 +0800

    优化物业手机版

--- service-common/src/main/java/com/java110/common/cmd/auditUser/ListAuditOrdersCmd.java
+++ service-common/src/main/java/com/java110/common/cmd/auditUser/ListAuditOrdersCmd.java
@@ -86,7 +86,7 @@ public class ListAuditOrdersCmd extends Cmd {
         if (count > 0) {
             datas = oaWorkflowUserInnerServiceSMOImpl.getDefinitionKeysUserTasks(auditUser);
             //刷新 表单数据
-            refreshFormData(datas, reqJson, storeId);
+            refreshFormData(datas, reqJson, storeId,oaWorkflowDtos.get(0).getFlowId());
         } else {
             datas = new ArrayList<>();
         }
@@ -97,11 +97,12 @@ public class ListAuditOrdersCmd extends Cmd {
         context.setResponseEntity(responseEntity);
     }
 
-    private void refreshFormData(List<JSONObject> datas, JSONObject paramIn, String storeId) {
+    private void refreshFormData(List<JSONObject> datas, JSONObject paramIn, String storeId,String flowId) {
 
         List<String> ids = new ArrayList<>();
         for (JSONObject data : datas) {
             ids.add(data.getString("id"));
+            data.put("flowId", flowId);
         }
         if (ids.size() < 1) {
             return;
