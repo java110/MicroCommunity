76eb8f1ab4c6382373653ec51c51a622ee6d8f18
Your Name <you@example.com>
2023-09-04 23:30:54 +0800

    加入静态二维码支付

---
+++ java110-bean/src/main/java/com/java110/po/payFeeQrcode/PayFeeQrcodePo.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2017-2020 吴学文 and java110 team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.java110.po.payFeeQrcode;
+
+import java.io.Serializable;
+import java.util.Date;
+/**
+ * 类表述： Po 数据模型实体对象 基本保持与数据库模型一直 用于 增加修改删除 等时的数据载体
+ * add by 吴学文 at 2023-09-04 23:24:48 mail: 928255095@qq.com
+ * open source address: https://gitee.com/wuxw7/MicroCommunity
+ * 官网：http://www.homecommunity.cn
+ * 温馨提示：如果您对此文件进行修改 请不要删除原有作者及注释信息，请补充您的 修改的原因以及联系邮箱如下
+ * // modify by 张三 at 2021-09-12 第10行在某种场景下存在某种bug 需要修复，注释10至20行 加入 20行至30行
+ */
+public class PayFeeQrcodePo implements Serializable {
+
+    private String createStaffName;
+private String queryWay;
+private String createStaffId;
+private String preFee;
+private String statusCd = "0";
+private String customFee;
+private String pfqId;
+private String qrcodeName;
+private String communityId;
+private String smsValidate;
+private String content;
+public String getCreateStaffName() {
+        return createStaffName;
+    }
+public void setCreateStaffName(String createStaffName) {
+        this.createStaffName = createStaffName;
+    }
+public String getQueryWay() {
+        return queryWay;
+    }
+public void setQueryWay(String queryWay) {
+        this.queryWay = queryWay;
+    }
+public String getCreateStaffId() {
+        return createStaffId;
+    }
+public void setCreateStaffId(String createStaffId) {
+        this.createStaffId = createStaffId;
+    }
+public String getPreFee() {
+        return preFee;
+    }
+public void setPreFee(String preFee) {
+        this.preFee = preFee;
+    }
+public String getStatusCd() {
+        return statusCd;
+    }
+public void setStatusCd(String statusCd) {
+        this.statusCd = statusCd;
+    }
+public String getCustomFee() {
+        return customFee;
+    }
+public void setCustomFee(String customFee) {
+        this.customFee = customFee;
+    }
+public String getPfqId() {
+        return pfqId;
+    }
+public void setPfqId(String pfqId) {
+        this.pfqId = pfqId;
+    }
+public String getQrcodeName() {
+        return qrcodeName;
+    }
+public void setQrcodeName(String qrcodeName) {
+        this.qrcodeName = qrcodeName;
+    }
+public String getCommunityId() {
+        return communityId;
+    }
+public void setCommunityId(String communityId) {
+        this.communityId = communityId;
+    }
+public String getSmsValidate() {
+        return smsValidate;
+    }
+public void setSmsValidate(String smsValidate) {
+        this.smsValidate = smsValidate;
+    }
+public String getContent() {
+        return content;
+    }
+public void setContent(String content) {
+        this.content = content;
+    }
+
+
+
+}
