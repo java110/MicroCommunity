f75d546655a557b7c01a21d195c1e9d295e35dd7
Your Name <you@example.com>
2023-09-08 13:16:48 +0800

    二维码支付加入 状态

--- service-fee/src/main/java/com/java110/fee/cmd/fee/PayBatchFeeCmd.java
+++ service-fee/src/main/java/com/java110/fee/cmd/fee/PayBatchFeeCmd.java
@@ -149,7 +149,7 @@ public class PayBatchFeeCmd extends Cmd {
             }
 
             if (maxEndTime != null && endTime != null && !FeeDto.FEE_FLAG_ONCE.equals(feeConfigDtos.get(0).getFeeFlag())) {
-                Date newDate = DateUtil.stepMonth(endTime, reqJson.getDouble("cycles").intValue());
+                Date newDate = DateUtil.stepMonth(endTime, paramInObj.getDouble("cycles").intValue());
                 if (newDate.getTime() > maxEndTime.getTime()) {
                     throw new IllegalArgumentException("缴费周期超过 缴费结束时间,请用按结束时间方式缴费");
                 }
