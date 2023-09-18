4fd96630a1296835c261a840a9a2131606cc55d6
java110 <928255095@qq.com>
2023-09-18 14:41:08 +0800

    优化代码

--- java110-bean/src/main/java/com/java110/po/account/AccountBankPo.java
+++ java110-bean/src/main/java/com/java110/po/account/AccountBankPo.java
@@ -6,55 +6,68 @@ import java.util.Date;
 public class AccountBankPo implements Serializable {
 
     private String personName;
-private String bankCode;
-private String bankId;
-private String bankName;
-private String statusCd = "0";
-private String shopId;
-private String personTel;
-public String getPersonName() {
+    private String bankCode;
+    private String bankId;
+    private String bankName;
+    private String statusCd = "0";
+    private String shopId;
+    private String personTel;
+
+    public String getPersonName() {
         return personName;
     }
-public void setPersonName(String personName) {
+
+    public void setPersonName(String personName) {
         this.personName = personName;
     }
-public String getBankCode() {
+
+    public String getBankCode() {
         return bankCode;
     }
-public void setBankCode(String bankCode) {
+
+    public void setBankCode(String bankCode) {
         this.bankCode = bankCode;
     }
-public String getBankId() {
+
+    public String getBankId() {
         return bankId;
     }
-public void setBankId(String bankId) {
+
+    public void setBankId(String bankId) {
         this.bankId = bankId;
     }
-public String getBankName() {
+
+    public String getBankName() {
         return bankName;
     }
-public void setBankName(String bankName) {
+
+    public void setBankName(String bankName) {
         this.bankName = bankName;
     }
-public String getStatusCd() {
+
+    public String getStatusCd() {
         return statusCd;
     }
-public void setStatusCd(String statusCd) {
+
+    public void setStatusCd(String statusCd) {
         this.statusCd = statusCd;
     }
-public String getShopId() {
+
+    public String getShopId() {
         return shopId;
     }
-public void setShopId(String shopId) {
+
+    public void setShopId(String shopId) {
         this.shopId = shopId;
     }
-public String getPersonTel() {
+
+    public String getPersonTel() {
         return personTel;
     }
-public void setPersonTel(String personTel) {
+
+    public void setPersonTel(String personTel) {
         this.personTel = personTel;
     }
-
 
 
 }
