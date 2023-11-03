7bdf7c0afbafa0d6286557049799e168d7d683f9
wuxw <928255095@qq.com>
2023-11-03 09:58:37 +0800

    优化代码

--- service-report/src/main/java/com/java110/report/cmd/admin/GetPropertyFeeSummaryCmd.java
+++ service-report/src/main/java/com/java110/report/cmd/admin/GetPropertyFeeSummaryCmd.java
@@ -148,7 +148,7 @@ public class GetPropertyFeeSummaryCmd extends Cmd {
                 }
             }
 
-            roomInfo.put("monthData", monthDatas);
+            roomInfo.put("monthData", months);
         }
     }
 }
