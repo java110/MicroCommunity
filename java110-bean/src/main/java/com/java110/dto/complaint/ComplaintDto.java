3a202a0a9d985dec4d15ac78b66fbae731bd555d
wuxw <928255095@qq.com>
2024-02-22 00:16:26 +0800

    优化代码

--- java110-bean/src/main/java/com/java110/dto/complaint/ComplaintDto.java
+++ java110-bean/src/main/java/com/java110/dto/complaint/ComplaintDto.java
@@ -27,11 +27,14 @@ public class ComplaintDto extends PageDto implements Serializable {
     private String complaintId;
     private String[] complaintIds;
     private String typeCd;
+
+    private String typeName;
     private String ownerName;
     private String context;
     private String complaintName;
     private String tel;
     private String state;
+    private String stateName;
     private String communityId;
 
     private String taskId;
@@ -263,5 +266,21 @@ public class ComplaintDto extends PageDto implements Serializable {
 
     public void setPhotos(List<PhotoVo> photos) {
         this.photos = photos;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
     }
 }
