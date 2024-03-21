fc3a727c1c0f570b0731ed0ee2c5309ced0d2eed
wuxw <928255095@qq.com>
2024-03-22 02:20:57 +0800

    优化代码

---
+++ service-user/src/main/java/com/java110/user/cmd/owner/AuthOwnerCmd.java
@@ -0,0 +1,23 @@
+package com.java110.user.cmd.owner;
+
+import com.alibaba.fastjson.JSONObject;
+import com.java110.core.annotation.Java110Cmd;
+import com.java110.core.context.ICmdDataFlowContext;
+import com.java110.core.event.cmd.Cmd;
+import com.java110.core.event.cmd.CmdEvent;
+import com.java110.utils.exception.CmdException;
+
+import java.text.ParseException;
+
+@Java110Cmd(serviceCode = "owner.authOwner")
+public class AuthOwnerCmd extends Cmd {
+    @Override
+    public void validate(CmdEvent event, ICmdDataFlowContext context, JSONObject reqJson) throws CmdException, ParseException {
+
+    }
+
+    @Override
+    public void doCmd(CmdEvent event, ICmdDataFlowContext context, JSONObject reqJson) throws CmdException, ParseException {
+
+    }
+}
